a56217b59568d293406d363f2b01857d10a1daf4
Michael Zingale <zingale@localhost.localdomain>
2013-03-18 20:05:22 -0400

    integer indices

---
+++ network_indices.f90
@@ -0,0 +1,89 @@
+module network_indices
+
+  implicit none
+
+  integer, parameter :: ihe4  = 1
+  integer, parameter :: ic12  = 2
+  integer, parameter :: io16  = 3
+  integer, parameter :: ine20 = 4
+  integer, parameter :: img24 = 5
+  integer, parameter :: isi28 = 6
+  integer, parameter :: is32  = 7
+  integer, parameter :: iar36 = 8
+  integer, parameter :: ica40 = 9
+  integer, parameter :: iti44 = 10
+  integer, parameter :: icr48 = 11
+  integer, parameter :: ife52 = 12
+  integer, parameter :: ini56 = 13
+
+  ! set the id numbers of the reaction rates
+  integer, parameter :: ir3a   = 1
+  integer, parameter :: irg3a  = 2
+  integer, parameter :: ircag  = 3
+  integer, parameter :: iroga  = 4
+  integer, parameter :: ir1212 = 5
+  integer, parameter :: ir1216 = 6
+  integer, parameter :: ir1616 = 7
+  integer, parameter :: iroag  = 8
+  integer, parameter :: irnega = 9
+  integer, parameter :: irneag = 10
+  integer, parameter :: irmgga = 11
+  integer, parameter :: irmgag = 12
+  integer, parameter :: irsiga = 13
+  integer, parameter :: irmgap = 14
+  integer, parameter :: iralpa = 15
+  integer, parameter :: iralpg = 16
+  integer, parameter :: irsigp = 17
+  integer, parameter :: irsiag = 18
+  integer, parameter :: irsga  = 19
+  integer, parameter :: irsiap = 20
+  integer, parameter :: irppa  = 21
+  integer, parameter :: irppg  = 22
+  integer, parameter :: irsgp  = 23
+  integer, parameter :: irsag  = 24
+  integer, parameter :: irarga = 25
+  integer, parameter :: irsap  = 26
+  integer, parameter :: irclpa = 27
+  integer, parameter :: irclpg = 28
+  integer, parameter :: irargp = 29
+  integer, parameter :: irarag = 30
+  integer, parameter :: ircaga = 31
+  integer, parameter :: irarap = 32
+  integer, parameter :: irkpa  = 33
+  integer, parameter :: irkpg  = 34
+  integer, parameter :: ircagp = 35
+  integer, parameter :: ircaag = 36
+  integer, parameter :: irtiga = 37
+  integer, parameter :: ircaap = 38
+  integer, parameter :: irscpa = 39
+  integer, parameter :: irscpg = 40
+  integer, parameter :: irtigp = 41
+  integer, parameter :: irtiag = 42
+  integer, parameter :: ircrga = 43
+  integer, parameter :: irtiap = 44
+  integer, parameter :: irvpa  = 45
+  integer, parameter :: irvpg  = 46
+  integer, parameter :: ircrgp = 47
+  integer, parameter :: ircrag = 48
+  integer, parameter :: irfega = 49
+  integer, parameter :: ircrap = 50
+  integer, parameter :: irmnpa = 51
+  integer, parameter :: irmnpg = 52
+  integer, parameter :: irfegp = 53
+  integer, parameter :: irfeag = 54
+  integer, parameter :: irniga = 55
+  integer, parameter :: irfeap = 56
+  integer, parameter :: ircopa = 57
+  integer, parameter :: ircopg = 58
+  integer, parameter :: irnigp = 59
+  
+  integer, parameter :: irr1   = 60
+  integer, parameter :: irs1   = 61
+  integer, parameter :: irt1   = 62
+  integer, parameter :: iru1   = 63
+  integer, parameter :: irv1   = 64
+  integer, parameter :: irw1   = 65
+  integer, parameter :: irx1   = 66
+  integer, parameter :: iry1   = 67
+
+end module network_indices
