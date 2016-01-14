2a981952b13adf186df4f94da3cb2e7ac18c5be6
Max Katz <maximilian.katz@stonybrook.edu>
2016-01-13 21:14:52 -0500

    Rewrite the rprox network using the new generic VODE interface

--- networks/rprox/actual_burner.f90
+++ networks/rprox/actual_burner.f90
@@ -1,7 +1,5 @@
 ! This module contains a version of Wallace and Woosley's (ApJS 45,389
 ! (1981)) rprox reaction network burner.
-!
-!
 
 module actual_burner_module
 
@@ -12,60 +10,59 @@ module actual_burner_module
   use eos_type_module
   use network
 
-  double precision, parameter :: T2T9 = 1.0d-9
+  integer, parameter :: nrates = 18
 
-  ! Set the number of independent variables -- this should be
-  ! temperature + the number of species which participate
-  ! in the evolution equations.
-  ! 
-  integer, parameter :: NEQ = 1 + nspec
-
-  ! Our problem is stiff, so tell ODEPACK that. 21 means stiff, jacobian 
-  ! function is supplied; 22 means stiff, figure out my jacobian through 
-  ! differencing.
-
-  integer, parameter :: MF_ANALYTIC_JAC = 21, MF_NUMERICAL_JAC = 22
-
-  ! Tolerance parameters:
-  !
-  !  itol specifies whether to use an single absolute tolerance for
-  !  all variables (1), or to pass an array of absolute tolerances, one
-  !  for each variable with a scalar relative tol (2), a scalar absolute
-  !  and array of relative tolerances (3), or arrays for both (4).
-  !  
-  !  The error is determined as e(i) = rtol*abs(y(i)) + atol, and must
-  !  be > 0.  Since we have some compositions that may be 0 initially,
-  !  we will specify both an absolute and a relative tolerance.
-  !
-  ! We will use arrays for both the absolute and relative tolerances, 
-  ! since we want to be easier on the temperature than the species.
-
-  integer, parameter :: ITOL = 4
-
-  ! We want to do a normal computation, and get the output values of y(t)
-  ! after stepping though dt.
-  
-  integer, PARAMETER :: ITASK = 1
-
-  ! We will override the maximum number of steps, so turn on the 
-  ! optional arguments flag.
-  
-  integer, parameter :: IOPT = 1
-
-  ! Declare a real work array of size 22 + 9*NEQ + 2*NEQ**2 and an
-  ! integer work array of size 30 + NEQ. These are VODE constants
-  ! that depend on the integration mode we're using -- see dvode.f.
-  
-  integer, parameter :: LRW = 22 + 9*NEQ + 2*NEQ**2
-  integer, parameter :: LIW = 30 + NEQ
-
-  ! Should we print out diagnostic output after the solve?
-  
-  logical, parameter :: verbose = .false.    
+  character (len=10), save :: reac_names(nrates)
   
 contains
 
   subroutine actual_burner_init()
+
+    use vode_data, only: temp_scale    
+    use rpar_indices
+    use network_indices
+
+    implicit none
+
+    temp_scale = 1.0d9
+
+    reac_names(irlambCNO) = "rlambdaCNO"
+    reac_names(irag15o)   = "rag15o"
+    reac_names(irr1)      = "rr1"
+    reac_names(irag16o)   = "rag16o"
+    reac_names(irpg16o)   = "rpg16o"
+    reac_names(irpg17f)   = "rpg17f"
+    reac_names(irgp17f)   = "rgp17f"
+    reac_names(irlambda2) = "rlambda2"
+    reac_names(irap14o)   = "rap14o"
+    reac_names(irs1)      = "rs1"
+    reac_names(irlambda1) = "rlambda1"
+    reac_names(ir3a)      = "r3a"
+    reac_names(irpg12c)   = "rpg12c"
+    reac_names(irwk14o)   = "wk14o"
+    reac_names(irwk17f)   = "wk17f"
+    reac_names(irwk15o)   = "wk15o"
+    reac_names(irLweak)   = "Lweak"
+    reac_names(irla2)     = "la2"
+
+    call init_rpar_indices(nrates, nspec)
+
+    ! Add some rpar indices for rate-related things
+
+    irp_rates        = get_next_rpar_index(nrates)
+    irp_drtdt        = get_next_rpar_index(nrates)
+
+    irp_dlambCNOdh1  = get_next_rpar_index(1)
+    irp_drs1dhe4     = get_next_rpar_index(1)
+    irp_drr1dh1      = get_next_rpar_index(1)
+    irp_dlambda1dhe4 = get_next_rpar_index(1)
+    irp_dlambda2dhe4 = get_next_rpar_index(1)
+
+    irp_delta1       = get_next_rpar_index(1)
+    irp_delta2       = get_next_rpar_index(1)
+
+    irp_r56eff       = get_next_rpar_index(1)
+    irp_dr56effdt    = get_next_rpar_index(1)
 
   end subroutine actual_burner_init
 
@@ -73,7 +70,9 @@ contains
 
   subroutine actual_burner(state_in, state_out, dt, time)  
 
-    use rpar_indices
+    use vode_module, only: vode_burner
+    use vode_data, only: temp_scale
+    use network_indices
 
     implicit none
 
@@ -81,87 +80,9 @@ contains
     type (eos_t),     intent(inout) :: state_out
     double precision, intent(in   ) :: dt, time    
 
-    integer :: j
-
-    double precision :: local_time
-    
-    ! Work arrays
-    
-    double precision, pointer :: y(:)
-    double precision, pointer :: atol(:), rtol(:)
-    double precision, pointer :: rwork(:)
-    integer,          pointer :: iwork(:)    
-
-    ! istate determines the state of the calculation.  A value of 1 meeans
-    ! this is the first call to the problem -- this is what we will want.
-    
-    integer :: istate
-    
-    double precision, pointer :: rpar(:)
-    integer :: ipar
-
     double precision :: T9
 
-    EXTERNAL jac, f_rhs    
-
-    ! Allocate storage for work arrays and for rpar, the
-    ! array through which we communicate with VODE from the
-    ! RHS routines.
-    
-    call bl_allocate(y, 1, NEQ)
-    call bl_allocate(atol, 1, NEQ)
-    call bl_allocate(rtol, 1, NEQ)
-    call bl_allocate(rwork, 1, LRW)
-    call bl_allocate(iwork, 1, LIW)
-    call bl_allocate(rpar, 1, n_rpar_comps)
-
-
-    ! Set the tolerances.  We will be more relaxed on the temperature
-    ! since it is only used in evaluating the rates.  
-    !
-    ! **NOTE** if you reduce these tolerances, you probably will need
-    ! to (a) decrease dT_crit, (b) increase the maximum number of 
-    ! steps allowed.
-
-    atol(1:nspec) = 1.d-11  ! mass fractions
-    atol(nspec+1) = 1.d-8   ! temperature
-
-    rtol(1:nspec) = 1.d-12  ! mass fractions
-    rtol(nspec+1) = 1.d-8   ! temperature
-
-    ! We want VODE to re-initialize each time we call it.
-    
-    istate = 1
-
-    ! Initialize work arrays to zero.       
-
-    rwork(:) = ZERO
-    iwork(:) = 0
-
-    ! Set the maximum number of steps allowed (the VODE default is 500).
-
-    iwork(6) = 15000
-
-    ! Initialize the integration time.
-
-    local_time = ZERO
-
-    ! Temperature (in units of 10**9).
-
-    T9 = state_in % T * T2T9
-
-    ! Abundances are the first nspec values and temperature (T9) is the last.
-
-    y(1:nspec) = state_in % xn(:) / aion(:)
-    y(nspec+1) = T9
-
-    ! Set the thermodynamics that are passed via rpar to the RHS routine.
-
-    rpar(irp_dens) = state_in % rho
-    rpar(irp_T9_eos) = T9
-    rpar(irp_dTcrit) = 1.0d15
-    rpar(irp_cp) = state_in % cp
-    rpar(irp_dhdX:irp_dhdX+nspec-1) = state_in % dhdX(:)
+    T9 = state_in % T / 10.0**9
 
     ! Only burn if 0.2 < T9 < 2.5 or X(H1) > 0.05.
     ! The last restriction is a kludge based on the last paragraph of WW81.
@@ -170,41 +91,10 @@ contains
 
        ! Call the integration routine.
 
-       call dvode(f_rhs, NEQ, y, time, dt, ITOL, rtol, atol, ITASK, &
-            istate, IOPT, rwork, LRW, iwork, LIW, jac, &
-                                !MF_NUMERICAL_JAC, &
-            MF_ANALYTIC_JAC,&
-            rpar, ipar)
-
-       if (istate < 0) then
-          print *, 'ERROR: integration failed in net'
-          print *, 'initial T   = ', state_in % T
-          print *, 'initial rho = ', state_in % rho
-          print *, 'iniitial X  = ', state_in % xn(:)
-          print *, 'istate = ', istate
-          print *, 'time = ', local_time
-          print *, 'dt = ', dt
-          print *, 'output state = ', y
-          call bl_error("ERROR in burner: integration failed")
-       endif
-
-       ! Store the new mass fractions.
-       state_out % xn(:) = max(smallx, min(ONE, y(1:nspec) * aion(:)))
-
-       ! Update the energy.
-       state_out % e = state_in % e + sum((state_in % xn(:) - state_out % xn(:)) * ebin(:))
-
-       if (verbose) then
-          ! print out some integration statistics, if desired
-          print *, 'integration summary: '
-          print *, 'dens: ', state_in % rho, ' temp: ', state_in % T
-          print *, 'number of steps taken: ', iwork(11)
-          print *, 'number of f evaluations: ', iwork(12)
-       endif
+       call vode_burner(state_in, state_out, dt, time)
 
     endif
     
   end subroutine actual_burner
-
 
 end module actual_burner_module
--- networks/rprox/actual_network.f90
+++ networks/rprox/actual_network.f90
@@ -8,20 +8,12 @@ module actual_network
 
   integer, parameter :: nspec = 10
   integer, parameter :: naux  = 0
-  integer, parameter :: nrat  = 18
 
   character (len=16), save :: spec_names(nspec)
   character (len= 5), save :: short_spec_names(nspec)
   character (len= 5), save :: short_aux_names(naux)
 
-  character (len=10), save :: reac_names(nrat)
-
   double precision, save :: aion(nspec), zion(nspec), ebin(nspec)
-
-  integer :: ih1, ihe4, ic12, io14, io15, io16, if17, img22, is30, ini56
-  integer :: irlambCNO, irag15o, irr1, irag16o, irpg16o, irpg17f, &
-             irgp17f, irpg22g, irlambda2, irap14o, irs1, irlambda1, ir3a, irpg12c, &
-             irwk14o, irwk17f, irwk15o, irLweak, irla2
 
   double precision, parameter :: MeV2erg = 1.60217646e-6
   double precision, parameter :: N_A = 6.0221415e23
@@ -31,142 +23,70 @@ contains
   subroutine actual_network_init
 
     use bl_constants_module
-    use rpar_indices
-
-    ! set the indices; ordering base on rprox.f
-    ic12 = 1
-    io14 = 2
-    io15 = 3
-    io16 = 4
-    if17 = 5
-    img22 = 6
-    is30 = 7
-    ini56 = 8
-    ihe4 = 9
-    ih1 = 10
-
-    irlambCNO = 1
-    irag15o = 2
-    irr1 = 3
-    irag16o = 4
-    irpg16o = 5
-    irpg17f = 6
-    irgp17f = 7
-    irlambda2 = 8
-    irap14o = 9
-    irs1 = 10
-    irlambda1 = 11
-    ir3a = 12
-    irpg12c = 13
-    irwk14o = 14
-    irwk17f = 15
-    irwk15o = 16
-    irLweak = 17
-    irla2 = 18
+    use network_indices
 
     ! set the names
-    spec_names(ic12) = "carbon-12"
-    spec_names(io14) = "oxygen-14"
-    spec_names(io15) = "oxygen-15"
-    spec_names(io16) = "oxygen-16"
-    spec_names(if17) = "flourine-17"
+    spec_names(ic12)  = "carbon-12"
+    spec_names(io14)  = "oxygen-14"
+    spec_names(io15)  = "oxygen-15"
+    spec_names(io16)  = "oxygen-16"
+    spec_names(if17)  = "flourine-17"
     spec_names(img22) = "magnesium-22"
-    spec_names(is30) = "sulfur-30"
+    spec_names(is30)  = "sulfur-30"
     spec_names(ini56) = "nickel-56"
-    spec_names(ihe4) = "helium-4"
-    spec_names(ih1) = "hydrogen-1"
+    spec_names(ihe4)  = "helium-4"
+    spec_names(ih1)   = "hydrogen-1"
 
-    short_spec_names(ic12) = "C12"
-    short_spec_names(io14) = "O14"
-    short_spec_names(io15) = "O15"
-    short_spec_names(io16) = "O16"
-    short_spec_names(if17) = "F17"
+    short_spec_names(ic12)  = "C12"
+    short_spec_names(io14)  = "O14"
+    short_spec_names(io15)  = "O15"
+    short_spec_names(io16)  = "O16"
+    short_spec_names(if17)  = "F17"
     short_spec_names(img22) = "Mg22"
-    short_spec_names(is30) = "S30"
+    short_spec_names(is30)  = "S30"
     short_spec_names(ini56) = "Ni56"
-    short_spec_names(ihe4) = "He4"
-    short_spec_names(ih1) = "H1"    
-
-    reac_names(irlambCNO) = "rlambdaCNO"
-    reac_names(irag15o) = "rag15o"
-    reac_names(irr1) = "rr1"
-    reac_names(irag16o) = "rag16o"
-    reac_names(irpg16o) = "rpg16o"
-    reac_names(irpg17f) = "rpg17f"
-    reac_names(irgp17f) = "rgp17f"
-    reac_names(irlambda2) = "rlambda2"
-    reac_names(irap14o) = "rap14o"
-    reac_names(irs1) = "rs1"
-    reac_names(irlambda1) = "rlambda1"
-    reac_names(ir3a) = "r3a"
-    reac_names(irpg12c) = "rpg12c"
-    reac_names(irwk14o) = "wk14o"
-    reac_names(irwk17f) = "wk17f"
-    reac_names(irwk15o) = "wk15o"
-    reac_names(irLweak) = "Lweak"
-    reac_names(irla2) = "la2"
+    short_spec_names(ihe4)  = "He4"
+    short_spec_names(ih1)   = "H1"    
 
     ! set the species properties
-    aion(ic12) = TWELVE
-    aion(io14) = 14.0_dp_t
-    aion(io15) = 15.0_dp_t
-    aion(io16) = 16.0_dp_t
-    aion(if17) = 17.0_dp_t
+    aion(ic12)  = TWELVE
+    aion(io14)  = 14.0_dp_t
+    aion(io15)  = 15.0_dp_t
+    aion(io16)  = 16.0_dp_t
+    aion(if17)  = 17.0_dp_t
     aion(img22) = 22.0_dp_t
-    aion(is30) = 30.0_dp_t
+    aion(is30)  = 30.0_dp_t
     aion(ini56) = 56.0_dp_t
-    aion(ihe4) = FOUR
-    aion(ih1) = ONE
+    aion(ihe4)  = FOUR
+    aion(ih1)   = ONE
 
-    zion(ic12) = SIX
-    zion(io14) = EIGHT
-    zion(io15) = EIGHT
-    zion(io16) = EIGHT
-    zion(if17) = NINE
+    zion(ic12)  = SIX
+    zion(io14)  = EIGHT
+    zion(io15)  = EIGHT
+    zion(io16)  = EIGHT
+    zion(if17)  = NINE
     zion(img22) = TWELVE
-    zion(is30) = 16.0_dp_t
+    zion(is30)  = 16.0_dp_t
     zion(ini56) = 28.0_dp_t
-    zion(ihe4) = TWO
-    zion(ih1) = ONE
+    zion(ihe4)  = TWO
+    zion(ih1)   = ONE
 
-    ! our convention is that binding energy is negative.  The
-    ! following are the binding energy (MeV).
-    ebin(ic12) =  92.16279_dp_t
-    ebin(io14) =  98.7325_dp_t
-    ebin(io15) = 111.9569_dp_t
-    ebin(io16) = 127.6207_dp_t
-    ebin(if17) = 128.2211_dp_t
+    ! Our convention is that binding energy is negative.  The
+    ! following are the binding energies in MeV.
+    ebin(ic12)  = 92.16279_dp_t
+    ebin(io14)  = 98.7325_dp_t
+    ebin(io15)  = 111.9569_dp_t
+    ebin(io16)  = 127.6207_dp_t
+    ebin(if17)  = 128.2211_dp_t
     ebin(img22) = 168.5768_dp_t
-    ebin(is30) = 243.6866_dp_t
+    ebin(is30)  = 243.6866_dp_t
     ebin(ini56) = 483.995_dp_t
-    ebin(ihe4) = 28.29599_dp_t
-    ebin(ih1) = ZERO
+    ebin(ihe4)  = 28.29599_dp_t
+    ebin(ih1)   = ZERO
 
     ! convert to erg / g by multiplying by N_A / aion and converting to erg
     ebin = -ebin * N_A * MeV2erg / aion
 
-    ! rpar is VODE's way of passing information into the RHS and                
-    ! jacobian routines.  Here we initialize some indices to make               
-    ! sense of what is stored in the rpar() array.                              
-    call init_rpar_indices(nrat, nspec)
-
   end subroutine actual_network_init
-
-  
-  function network_reaction_index(name)
-    
-    character(len=*) :: name
-    integer :: network_reaction_index, n
-
-    network_reaction_index = -1
-
-    do n = 1, nrat
-       if (name == reac_names(n)) then
-          network_reaction_index = n
-          exit
-       endif
-    enddo
-
-  end function network_reaction_index
 
 end module actual_network
