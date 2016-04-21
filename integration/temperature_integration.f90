48afe1a071461a8b13a8963fe919b155b2a1df00
Max Katz <maximilian.katz@stonybrook.edu>
2016-04-21 11:25:36 -0400

    Fix another bug in dT_crit functionality

--- integration/temperature_integration.f90
+++ integration/temperature_integration.f90
@@ -15,7 +15,7 @@ contains
     use bl_constants_module, only: ZERO, ONE
     use network, only: nspec, aion
     use burn_type_module
-    use extern_probin_module, only: do_constant_volume_burn, dT_crit
+    use extern_probin_module, only: do_constant_volume_burn, dT_crit, call_eos_in_rhs
 
     implicit none
 
@@ -37,7 +37,7 @@ contains
 
        if (do_constant_volume_burn) then
 
-          if (dT_crit < 1.0d19) then
+          if (.not. call_eos_in_rhs .and. dT_crit < 1.0d19) then
 
              cv = state % cv + (state % T - state % T_old) * state % dcvdt
 
@@ -53,7 +53,7 @@ contains
 
        else
 
-          if (dT_crit < 1.0d19) then
+          if (.not. call_eos_in_rhs .and. dT_crit < 1.0d19) then
 
              cp = state % cp + (state % T - state % T_old) * state % dcpdt
 
@@ -84,7 +84,7 @@ contains
     use bl_constants_module, only: ZERO, ONE
     use network, only: nspec, aion
     use burn_type_module
-    use extern_probin_module, only: do_constant_volume_burn, dT_crit
+    use extern_probin_module, only: do_constant_volume_burn, dT_crit, call_eos_in_rhs
 
     implicit none
 
@@ -98,7 +98,7 @@ contains
 
        if (do_constant_volume_burn) then
 
-          if (dT_crit < 1.0d19) then
+          if (.not. call_eos_in_rhs .and. dT_crit < 1.0d19) then
 
              cv = state % cv + (state % T - state % T_old) * state % dcvdt
 
@@ -120,7 +120,7 @@ contains
 
        else
 
-          if (dT_crit < 1.0d19) then
+          if (.not. call_eos_in_rhs .and. dT_crit < 1.0d19) then
 
              cp = state % cp + (state % T - state % T_old) * state % dcpdt
 
