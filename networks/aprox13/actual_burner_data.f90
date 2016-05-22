7764a153f81104289987f5052ff78236f20508a0
Max Katz <maxpkatz@gmail.com>
2016-05-22 15:08:21 -0400

    Enable a tabulated rates option for aprox13

--- networks/aprox13/actual_burner_data.f90
+++ networks/aprox13/actual_burner_data.f90
@@ -74,6 +74,21 @@ module actual_burner_data
 
   character (len=16), save :: ratenames(nrates)
 
+  ! Table interpolation data
+
+  double precision, parameter :: tab_tlo = 6.0d0, tab_thi = 10.0d0
+  integer, parameter :: tab_per_decade = 500
+  integer, parameter :: nrattab = int(tab_thi - tab_tlo) * tab_per_decade + 1
+  integer, parameter :: tab_imax = int(tab_thi - tab_tlo) * tab_per_decade + 1
+  double precision, parameter :: tab_tstp = (tab_thi - tab_tlo) / dble(tab_imax - 1)
+
+  double precision :: rattab(nrates, nrattab)
+  double precision :: drattabdt(nrates, nrattab)
+  double precision :: drattabdd(nrates, nrattab)
+  double precision :: ttab(nrattab)
+
+  !$acc declare create(rattab, drattabdt, drattabdd, ttab)
+
 contains
 
   ! Computes the instantaneous energy generation rate
