92ac02293ff18b3a9968e031e3f24ca129883b53
Max Katz <maxpkatz@gmail.com>
2016-06-04 13:52:21 -0400

    Move rate tabulation data to actual_rhs module

--- networks/aprox13/actual_burner_data.f90
+++ networks/aprox13/actual_burner_data.f90
@@ -1,22 +1,5 @@
 module actual_burner_data
 
-  use actual_network_data, only: nrates
-
   implicit none
-
-  ! Table interpolation data
-
-  double precision, parameter :: tab_tlo = 6.0d0, tab_thi = 10.0d0
-  integer, parameter :: tab_per_decade = 500
-  integer, parameter :: nrattab = int(tab_thi - tab_tlo) * tab_per_decade + 1
-  integer, parameter :: tab_imax = int(tab_thi - tab_tlo) * tab_per_decade + 1
-  double precision, parameter :: tab_tstp = (tab_thi - tab_tlo) / dble(tab_imax - 1)
-
-  double precision :: rattab(nrates, nrattab)
-  double precision :: drattabdt(nrates, nrattab)
-  double precision :: drattabdd(nrates, nrattab)
-  double precision :: ttab(nrattab)
-
-  !$acc declare create(rattab, drattabdt, drattabdd, ttab)
 
 end module actual_burner_data
--- networks/aprox13/actual_rhs.f90
+++ networks/aprox13/actual_rhs.f90
@@ -1,13 +1,28 @@
 module actual_rhs_module
 
   use network
-  use actual_burner_data
   use eos_type_module
   use burn_type_module
   use temperature_integration_module, only: temperature_rhs, temperature_jac
   use sneut_module, only: sneut5
+  use actual_network_data, only: nrates
 
   implicit none
+
+  ! Table interpolation data
+
+  double precision, parameter :: tab_tlo = 6.0d0, tab_thi = 10.0d0
+  integer, parameter :: tab_per_decade = 500
+  integer, parameter :: nrattab = int(tab_thi - tab_tlo) * tab_per_decade + 1
+  integer, parameter :: tab_imax = int(tab_thi - tab_tlo) * tab_per_decade + 1
+  double precision, parameter :: tab_tstp = (tab_thi - tab_tlo) / dble(tab_imax - 1)
+
+  double precision :: rattab(nrates, nrattab)
+  double precision :: drattabdt(nrates, nrattab)
+  double precision :: drattabdd(nrates, nrattab)
+  double precision :: ttab(nrattab)
+
+  !$acc declare create(rattab, drattabdt, drattabdd, ttab)
 
 contains
 
