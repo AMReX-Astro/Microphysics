a9944af7bac62eb25980a0df4e700d4bea8decf8
Max Katz <maxpkatz@gmail.com>
2016-06-03 22:13:41 -0400

    Insert a missing factor of 1/2 in the energy release

--- networks/ignition_simple/actual_burner_data.f90
+++ networks/ignition_simple/actual_burner_data.f90
@@ -20,9 +20,12 @@ contains
 
     ! Note that since we don't explicitly evolve Mg24
     ! in this network, we need to explicitly add its
-    ! contribution in this routine.
+    ! contribution in this routine. We can factor out
+    ! the common factor of dydt(ic12), we just need to
+    ! account for a factor of aion(ic12) / aion(img24)
+    ! for the second term to make the expression work.
 
-    enuc = dydt(ic12) * (mion(ic12) - mion(img24)) * enuc_conv2
+    enuc = dydt(ic12) * (mion(ic12) - mion(img24) / 2) * enuc_conv2
 
   end subroutine ener_gener_rate
 
