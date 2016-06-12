b9f2f1758510631ba8c97d515d1efe0be78b689a
Michael Zingale <michael.zingale@stonybrook.edu>
2016-06-12 18:15:46 -0400

    make it easier to init and finalize

--- unit_test/GPackage.mak
+++ unit_test/GPackage.mak
@@ -1,2 +1,3 @@
+f90sources += microphysics.f90
 f90sources += network.f90
 f90sources += eos.f90
---
+++ unit_test/microphysics.f90
@@ -0,0 +1,29 @@
+module microphysics_module
+
+  use network
+  use actual_rhs_module, only: actual_rhs_init
+  use actual_burner_module, only: actual_burner_init
+
+  
+  implicit none
+
+contains
+
+  subroutine microphysics_init()
+
+    call eos_init()
+
+    call network_init()
+    call actual_rhs_init()
+    call actual_burner_init()
+    
+  end subroutine init_microphysics
+
+  subroutine microphysics_finalize()
+
+    call network_finalize()
+
+  end subroutine microphysics_finalize
+
+end module microphysics_module
+    
