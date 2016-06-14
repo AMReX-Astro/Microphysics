a750555d210cb271cd81fe3d44d51f60a1852bf0
Michael Zingale <michael.zingale@stonybrook.edu>
2016-06-14 15:04:22 -0400

    add a copy of the constants for testing and allow small to eos_init()

--- unit_test/GPackage.mak
+++ unit_test/GPackage.mak
@@ -3,3 +3,4 @@ f90sources += network.f90
 f90sources += eos.f90
 f90sources += write_job_info.f90
 f90sources += build_info.f90
+f90sources += constants_cgs.f90
--- unit_test/microphysics.f90
+++ unit_test/microphysics.f90
@@ -10,9 +10,21 @@ module microphysics_module
 
 contains
 
-  subroutine microphysics_init()
+  subroutine microphysics_init(small_temp, small_dens)
 
-    call eos_init()
+    double precision, optional :: small_temp
+    double precision, optional :: small_dens
+
+
+    if (present(small_temp) .and. present(small_dens)) then
+       call eos_init(small_temp=small_temp, small_dens=small_dens)
+    else if (present(small_temp)) then
+       call eos_init(small_temp=small_temp)
+    else if (present(small_dens)) then
+       call eos_init(small_dens=small_dens)
+    else
+       call eos_init()
+    endif
 
     call network_init()
     call actual_rhs_init()
