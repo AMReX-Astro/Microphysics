8ac34c428a26eb1339228a9490bd829432a19c9c
Michael Zingale <michael.zingale@stonybrook.edu>
2016-07-21 14:21:30 -0400

    clean up and output runtime params

--- networks/triple_alpha_plus_cago/test/testburn.f90
+++ networks/triple_alpha_plus_cago/test/testburn.f90
@@ -2,16 +2,20 @@ program testburn
 
   use bl_types
   use bl_constants_module
+  use microphysics_module
   use network
   use eos_module
   use actual_burner_module
-  use microphysics_module
+  use runtime_init_module, only : runtime_pretty_print
 
   implicit none
 
   real(kind=dp_t) :: dens, temp, dt
   real(kind=dp_t), dimension(nspec) :: Xin
   type(burn_t) :: state_in, state_out 
+
+  integer :: n
+  integer :: of
 
   call microphysics_init()
 
@@ -25,6 +29,9 @@ program testburn
 
   dt = 0.001_dp_t
 
+  open(newunit=of, file="testburn-params.out", status="replace", action="write")
+  call runtime_pretty_print(of)
+  close(unit=of)
   
   print *, 'calling the burner...'
 
@@ -37,8 +44,13 @@ program testburn
 
   print *, 'done!'
 
-  print *, 'Xin:  ', state_in % xn(:)
-  print *, 'Xout: ', state_out % xn(:)
+1000 format(g25.15, 1x, g25.15)
+
+  print *, 'Xin / Xout:'
+  do n = 1, nspec
+     print 1000, state_in % xn(n), state_out % xn(n)
+  enddo
+
   print *, 'rho_Hnuc: ', dens * (state_out % e - state_in % e) / dt
 
   call microphysics_finalize()
