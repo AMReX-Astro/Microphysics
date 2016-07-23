86c4193d4c29b7e3d0cca5f26317b416a6f21988
Michael Zingale <michael.zingale@stonybrook.edu>
2016-07-23 11:36:46 -0400

    had y_init allocated with too few entries

--- integration/BS/rpar.f90
+++ integration/BS/rpar.f90
@@ -13,7 +13,7 @@ module rpar_indices
 
   integer, parameter :: irp_nspec = 1
   integer, parameter :: irp_y_init = irp_nspec + n_not_evolved
-  integer, parameter :: irp_t_sound = irp_y_init + nspec
+  integer, parameter :: irp_t_sound = irp_y_init + neqs
   integer, parameter :: irp_t0 = irp_t_sound + 1
 
   integer, parameter :: n_rpar_comps = irp_t0 + 1
