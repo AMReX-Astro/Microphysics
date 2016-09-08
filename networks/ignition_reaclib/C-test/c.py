a9d23b5024c5e0dd13dc817c9957778b87a71710
dwillcox <dwillcox@electra.iacs.stonybrook.edu>
2016-09-07 22:17:15 -0400

    Updated pyreaclib generation scripts for the new pyreaclib module system.

--- networks/ignition_reaclib/C-test/c.py
+++ networks/ignition_reaclib/C-test/c.py
@@ -1,12 +1,10 @@
 # C-burning rate module generator
 
-import reaclib
+import pyreaclib
 
 files = ["c12-ag-o16-nac2"]
 
-rc = reaclib.RateCollection(files)
-
-rc.make_network('boxlib')
+pyreaclib.make_network(files, 'boxlib')
 
 
 
