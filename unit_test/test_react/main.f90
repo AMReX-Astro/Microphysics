a457676ba4a737cf7fa5885e1e03d0f0b7852e20
dwillcox <dwillcox@electra.iacs.stonybrook.edu>
2016-10-12 12:42:38 -0400

    Fixed uninitialized state on GPU. ACC and OMP now agree to at least 1 part in 10^6

--- unit_test/test_react/main.f90
+++ unit_test/test_react/main.f90
@@ -149,7 +149,7 @@ program test_react
      !$OMP SCHEDULE(DYNAMIC,1)
 
      !$acc data copyin(temp_min, dlogT, dens_min, dlogrho, xn_zone, lo, hi, tmax) &
-     !$acc      copyout(state) 
+     !$acc      copy(state) 
 
      !$acc parallel reduction(+:n_rhs_avg) reduction(max:n_rhs_max) reduction(min:n_rhs_min)
 
