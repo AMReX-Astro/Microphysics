ffc302f2e42bd61744d240d048fee6d6343b877f
dwillcox <eugene.willcox@gmail.com>
2017-10-17 18:03:10 -0400

    (ignition_simple) If compiling with SDC, allow calling burner by passing sdc_t state variables.

--- networks/ignition_simple/actual_burner.F90
+++ networks/ignition_simple/actual_burner.F90
@@ -3,7 +3,12 @@ module actual_burner_module
   use bl_types
   use bl_constants_module
   use network
+
+#ifndef SDC
   use burn_type_module
+#else
+  use sdc_type_module
+#endif
 
   implicit none
 
@@ -29,8 +34,13 @@ contains
 
     implicit none
 
+#ifndef SDC
     type (burn_t),    intent(in   ) :: state_in
     type (burn_t),    intent(inout) :: state_out
+#else
+    type (sdc_t),    intent(in   )  :: state_in
+    type (sdc_t),    intent(inout)  :: state_out
+#endif
     double precision, intent(in   ) :: dt, time
 
     call integrator(state_in, state_out, dt, time)
