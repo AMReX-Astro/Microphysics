27af45f03219e3beef396c557654ef93a692eec2
dwillcox <eugene.willcox@gmail.com>
2017-10-17 17:50:33 -0400

    Revise sdc_type_module to allow for choice of SDC_METHOD.

--- interfaces/sdc_type.F90
+++ interfaces/sdc_type.F90
@@ -8,10 +8,15 @@ module sdc_type_module
   ! A generic structure holding data necessary to do a nuclear burn
   ! in the SDC formalism.
 
+#if (SDC_METHOD == 1)
   ! these indicies represent the order that the conserved state comes
   ! into the ODE integration from the hydro code.
   !
   ! they also represent the order of the advective sources
+  !
+  ! integrate rho*X, internal energy, total energy
+  ! carry momentum as an unevolved variable
+
   integer, parameter :: SEDEN = 1
   integer, parameter :: SEINT = 2
   integer, parameter :: SFS   = 3
@@ -22,13 +27,26 @@ module sdc_type_module
 
   integer, parameter :: SVAR  = SMZ
   integer, parameter :: SVAR_EVOLVE = SRHO - 1
+#elif (SDC_METHOD == 2)
+  ! integrate rho*X (species masses) and rho*h (enthalpy)
+  ! carry pressure for EOS calls in RHS
+
+  integer, parameter :: SSPEC = 1
+  integer, parameter :: SENTH = SSPEC + nspec
+  integer, parameter :: SVAR  = SENTH
+#endif
 
   type :: sdc_t
 
      real(dp_t) :: y(SVAR)
      real(dp_t) :: ydot_a(SVAR)
 
+#if (SDC_METHOD == 1)
      logical :: T_from_eden
+#elif (SDC_METHOD == 2)
+     ! Pressure in case we wish to use it for EOS calls
+     real(dp_t) :: p0
+#endif
 
      integer :: i
      integer :: j
@@ -38,6 +56,7 @@ module sdc_type_module
      integer :: n_jac
 
      integer :: sdc_iter
+
   end type sdc_t
 
 end module sdc_type_module
