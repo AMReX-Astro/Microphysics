c63bb09660e7fd51701c3b7bd24afbb8eacad855
dwillcox <eugene.willcox@gmail.com>
2017-10-18 11:09:19 -0400

    Update vode_type_sdc to support SDC_METHOD = 2.

--- integration/VODE/GPackage.mak
+++ integration/VODE/GPackage.mak
@@ -1,7 +1,7 @@
 ifdef SDC
   f90sources += vode_integrator_sdc.f90
   f90sources += vode_rhs_sdc.f90
-  f90sources += vode_type_sdc.f90
+  F90sources += vode_type_sdc.F90
 else
   f90sources += vode_integrator.f90
   f90sources += vode_rhs.f90
--- integration/VODE/vode_type_sdc.F90
+++ integration/VODE/vode_type_sdc.F90
@@ -4,13 +4,22 @@ module vode_type_module
   use bl_constants_module
 
   use burn_type_module, only : burn_t, net_ienuc, eos_to_burn
-  use eos_type_module, only : eos_t, eos_input_re, eos_input_rt, eos_get_small_temp, eos_get_max_temp
+  use eos_type_module, only : eos_t, eos_input_re, eos_input_rh, eos_input_rt, eos_get_small_temp, eos_get_max_temp
   use eos_module, only : eos
 
   use network, only : nspec, aion, aion_inv
 
   use rpar_indices
-  use sdc_type_module, only: SEDEN, SEINT, SFS, SRHO, SMX, SMY, SMZ, SVAR_EVOLVE, sdc_t
+
+#if (SDC_METHOD == 1)
+
+  use sdc_type_module, only: SEDEN, SEINT, SFS, SRHO, SMX, SMY, SMZ, SVAR, SVAR_EVOLVE, sdc_t
+
+#elif (SDC_METHOD == 2)
+
+  use sdc_type_module, only: SFS, SENTH, SVAR, SVAR_EVOLVE, sdc_t
+
+#endif
 
   use extern_probin_module, only: renormalize_abundances
 
@@ -18,15 +27,15 @@ module vode_type_module
 
   private
 
-  ! this should be larger than any reasonable temperature we will encounter   
-  real (kind=dp_t), parameter :: MAX_TEMP = 1.0d11          
+  ! this should be larger than any reasonable temperature we will encounter
+  real (kind=dp_t), parameter :: MAX_TEMP = 1.0d11
 
   integer, parameter :: VODE_NEQS = SVAR_EVOLVE
 
   public :: VODE_NEQS
   public :: clean_state, fill_unevolved_variables, &
-       renormalize_species, sdc_to_vode, vode_to_sdc, &
-       rhs_to_vode, jac_to_vode, vode_to_burn
+            renormalize_species, sdc_to_vode, vode_to_sdc, &
+            rhs_to_vode, jac_to_vode, vode_to_burn
 
 contains
 
@@ -39,7 +48,7 @@ contains
 
     type (eos_t) :: eos_state
 
-    ! update rho, rho*u, etc.
+    ! update rho, rho*u, etc., depending on SDC_METHOD
     call fill_unevolved_variables(time, y, rpar)
 
     ! Ensure that mass fractions always stay positive.
@@ -51,6 +60,8 @@ contains
     if (renormalize_abundances) then
        call renormalize_species(time, y, rpar)
     endif
+
+#if (SDC_METHOD == 1)
 
     ! Ensure that internal energy never goes above the maximum limit
     ! provided by the EOS. Same for the internal energy implied by the
@@ -69,6 +80,8 @@ contains
 
     y(SEDEN) = min(rpar(irp_SRHO) * max_e + ke, y(SEDEN))
 
+#endif
+
   end subroutine clean_state
 
 
@@ -76,6 +89,8 @@ contains
 
     real(dp_t), intent(in) :: time
     real(dp_t) :: y(SVAR_EVOLVE), rpar(n_rpar_comps)
+
+#if (SDC_METHOD == 1)
 
     ! we are always integrating from t = 0, so there is no offset
     ! time needed here.  The indexing of irp_ydot_a is based on
@@ -87,6 +102,15 @@ contains
     rpar(irp_SMY) = rpar(irp_u_init-1+irp_SMY) + rpar(irp_ydot_a-1+SMY) * time
     rpar(irp_SMZ) = rpar(irp_u_init-1+irp_SMZ) + rpar(irp_ydot_a-1+SMZ) * time
 
+#elif (SDC_METHOD == 2)
+
+    ! SDC_METHOD = 2 doesn't update unevolved variables
+    ! with advective source terms, but the density is
+    ! kept consistent with the partial densities.
+    rpar(irp_SRHO) = sum(y(SFS:SFS - 1 + nspec))
+
+#endif
+
   end subroutine fill_unevolved_variables
 
   subroutine renormalize_species(time, y, rpar)
@@ -96,12 +120,20 @@ contains
 
     real(dp_t) :: nspec_sum
 
-    ! update rho, rho*u, etc.
+    ! We only renormalize below for SDC_METHOD = 1 because
+    ! in SDC_METHOD = 2, we define the density as
+    ! the sum of the partial densities so nspec_sum = 1
+
+#if (SDC_METHOD == 1)
+
+    ! update rho, rho*u, etc., depending on SDC_METHOD
     call fill_unevolved_variables(time, y, rpar)
 
     nspec_sum = sum(y(SFS:SFS-1+nspec)) / rpar(irp_SRHO)
 
     y(SFS:SFS-1+nspec) = y(SFS:SFS-1+nspec) / nspec_sum
+
+#endif
 
   end subroutine renormalize_species
 
@@ -116,12 +148,14 @@ contains
 
     y(:) = sdc % y(1:SVAR_EVOLVE)
 
+    ! advective sources
+    rpar(irp_ydot_a:irp_ydot_a-1+SVAR) = sdc % ydot_a(:)
+
+#if (SDC_METHOD == 1)
+
     ! unevolved state variables
     rpar(irp_SRHO) = sdc % y(SRHO)
     rpar(irp_SMX:irp_SMZ) = sdc % y(SMX:SMZ)
-
-    ! advective sources
-    rpar(irp_ydot_a:irp_ydot_a-1+SVAR) = sdc % ydot_a(:)
 
     ! initial state for unevolved variables
     rpar(irp_u_init-1+irp_SRHO) = sdc % y(SRHO)
@@ -133,6 +167,13 @@ contains
     else
        rpar(irp_T_from_eden) = -ONE
     endif
+
+#elif (SDC_METHOD == 2)
+
+    rpar(irp_p0)   = sdc % p0
+    rpar(irp_SRHO) = sdc % rho
+
+#endif
 
   end subroutine sdc_to_vode
 
@@ -148,8 +189,17 @@ contains
     ! unevolved state variables
     call fill_unevolved_variables(time, y, rpar)
 
+#if (SDC_METHOD == 1)
+
     sdc % y(SRHO) = rpar(irp_SRHO)
     sdc % y(SMX:SMZ) = rpar(irp_SMX:irp_SMZ)
+
+#elif (SDC_METHOD == 2)
+
+    sdc % p0  = rpar(irp_p0)
+    sdc % rho = rpar(irp_SRHO)
+
+#endif
 
   end subroutine vode_to_sdc
 
@@ -173,8 +223,16 @@ contains
     ydot(SFS:SFS-1+nspec) = ydot(SFS:SFS-1+nspec) + &
          rpar(irp_SRHO) * aion(1:nspec) * burn_state % ydot(1:nspec)
 
+#if (SDC_METHOD == 1)
+
     ydot(SEINT) = ydot(SEINT) + rpar(irp_SRHO) * burn_state % ydot(net_ienuc)
     ydot(SEDEN) = ydot(SEDEN) + rpar(irp_SRHO) * burn_state % ydot(net_ienuc)
+
+#elif (SDC_METHOD == 2)
+
+    ydot(SENTH) = ydot(SENTH) + rpar(irp_SRHO) * burn_state % ydot(net_ienuc)
+
+#endif
 
   end subroutine rhs_to_vode
 
@@ -191,6 +249,8 @@ contains
 
     integer :: n
 
+#if (SDC_METHOD == 1)
+
     jac(SFS:SFS+nspec-1,SFS:SFS+nspec-1) = burn_state % jac(1:nspec,1:nspec)
     jac(SFS:SFS+nspec-1,SEDEN) = burn_state % jac(1:nspec,net_ienuc)
     jac(SFS:SFS+nspec-1,SEINT) = burn_state % jac(1:nspec,net_ienuc)
@@ -202,6 +262,16 @@ contains
     jac(SEINT,SFS:SFS+nspec-1) = burn_state % jac(net_ienuc,1:nspec)
     jac(SEINT,SEDEN) = burn_state % jac(net_ienuc,net_ienuc)
     jac(SEINT,SEINT) = burn_state % jac(net_ienuc,net_ienuc)
+
+#elif (SDC_METHOD == 2)
+
+    jac(SFS:SFS+nspec-1,SFS:SFS+nspec-1) = burn_state % jac(1:nspec,1:nspec)
+    jac(SFS:SFS+nspec-1,SENTH) = burn_state % jac(1:nspec,net_ienuc)
+
+    jac(SENTH,SFS:SFS+nspec-1) = burn_state % jac(net_ienuc,1:nspec)
+    jac(SENTH,SENTH) = burn_state % jac(net_ienuc,net_ienuc)
+
+#endif
 
     ! Scale it to match our variables. We don't need to worry about
     ! the rho dependence, since every one of the SDC variables is
@@ -217,6 +287,12 @@ contains
 
   subroutine vode_to_burn(time, y, rpar, burn_state)
 
+#if (SDC_METHOD == 2)
+
+    use probin_module, only: use_tfromp
+
+#endif
+
     type (burn_t) :: burn_state
     real(dp_t), intent(in) :: time
     real(dp_t)    :: rpar(n_rpar_comps)
@@ -224,7 +300,7 @@ contains
 
     type(eos_t) :: eos_state
 
-    real(kind=dp_t) :: rhoInv, min_temp, max_temp                               
+    real(kind=dp_t) :: rhoInv, min_temp, max_temp
 
     ! update rho, rho*u, etc.
     call fill_unevolved_variables(time, y, rpar)
@@ -234,11 +310,19 @@ contains
     eos_state % rho = rpar(irp_SRHO)
     eos_state % xn  = y(SFS:SFS+nspec-1) * rhoInv
 
+#if (SDC_METHOD == 1)
+
     if (rpar(irp_T_from_eden) > ZERO) then
        eos_state % e = (y(SEDEN) - HALF*rhoInv*sum(rpar(irp_SMX:irp_SMZ)**2)) * rhoInv
     else
        eos_state % e = y(SEINT) * rhoInv
     endif
+
+#elif (SDC_METHOD == 2)
+
+    eos_state % h = y(SENTH) * rhoInv
+
+#endif
 
     ! Give the temperature an initial guess -- use the geometric mean
     ! of the minimum and maximum temperatures.
@@ -248,15 +332,37 @@ contains
 
     eos_state % T = sqrt(min_temp * max_temp)
 
-    call eos(eos_input_re, eos_state)
-    call eos_to_burn(eos_state, burn_state)
+#if (SDC_METHOD == 1)
 
+    call eos(eos_input_re, eos_state)
+
+#elif (SDC_METHOD == 2)
+
+    if (use_tfromp) then
+
+       call bl_error("Error: SDC_METHOD = 2 requires use_tfromp = F")
+
+    else
+
+       call eos(eos_input_rh, eos_state)
+
+    endif
+
+#endif
+
+    call eos_to_burn(eos_state, burn_state)
 
     if (rpar(irp_self_heat) > ZERO) then
        burn_state % self_heat = .true.
     else
-       burn_state % self_heat = .false.       
+       burn_state % self_heat = .false.
     endif
+
+#if (SDC_METHOD == 2)
+
+    burn_state % p0 = rpar(irp_p0)
+
+#endif
 
   end subroutine vode_to_burn
 
