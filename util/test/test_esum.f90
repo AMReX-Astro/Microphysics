95252072e63b896d7a9a075012440804666b7c6d
Michael Zingale <michael.zingale@stonybrook.edu>
2017-10-20 22:29:07 -0400

    a test of 3 numbers

--- util/test/test_esum.f90
+++ util/test/test_esum.f90
@@ -6,11 +6,19 @@ program test
   implicit none
 
   real(dp_t) :: a(2)
+  real(dp_t) :: b(3)
 
   a(1) = 1.0_dp_t
   a(2) = 2.e-16_dp_t
 
+  b(1) = 1.0_dp_t
+  b(2) = 3e-16_dp_t
+  b(3) = 1.0_dp_t
+
   print *, esum(a, 2)
   print *, a(1) + a(2)
+
+  print *, esum(b, 3)
+  print *, b(1) + b(2) + b(3)
 
 end program test
