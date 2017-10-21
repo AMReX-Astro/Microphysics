c7a18415fd0adb83f75beea5e2effc068aa862dc
Michael Zingale <michael.zingale@stonybrook.edu>
2017-10-20 22:15:25 -0400

    this works

--- util/test/test_enum.f90
+++ util/test/test_enum.f90
@@ -1,3 +1,16 @@
 program test
 
+  use bl_types
+  use microphysics_math_module
+
+  implicit none
+
+  real(dp_t) :: a(2)
+
+  a(1) = 1.0_dp_t
+  a(2) = 2.e-16_dp_t
+
+  print *, esum(a, 2)
+  print *, a(1) + a(2)
+
 end program test
