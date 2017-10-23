a8178bbc97c9b10183199caf461a9578b5ea1d61
dwillcox <eugene.willcox@gmail.com>
2017-10-22 20:33:58 -0400

    Change arrays passed to f_rhs to explicit-shape arrays. Fixes PGI "failed to translate accelerator region" error.

--- integration/VODE90/vode_rhs.F90
+++ integration/VODE90/vode_rhs.F90
@@ -23,14 +23,14 @@ contains
          integrate_temperature, integrate_energy
     use vode_type_module, only: clean_state, renormalize_species, update_thermodynamics, &
                                 burn_to_vode, vode_to_burn
-    use rpar_indices, only: n_rpar_comps, irp_y_init, irp_t_sound
+    use rpar_indices, only: n_rpar_comps, irp_y_init, irp_t_sound, n_ipar_comps
 
     implicit none
 
-    integer,    intent(IN   ) :: neq, ipar(:)
-    real(dp_t), intent(INOUT) :: time, y(:)
-    real(dp_t), intent(INOUT) :: rpar(:)
-    real(dp_t), intent(INOUT) :: ydot(:)
+    integer,    intent(IN   ) :: neq, ipar(n_ipar_comps)
+    real(dp_t), intent(INOUT) :: time, y(neq)
+    real(dp_t), intent(INOUT) :: rpar(n_rpar_comps)
+    real(dp_t), intent(INOUT) :: ydot(neq)
 
     type (burn_t) :: burn_state
 
