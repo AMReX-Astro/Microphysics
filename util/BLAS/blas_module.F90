d64881281cb21d7ea687e1fde1b9b2d59e182aef
dwillcox <eugene.willcox@gmail.com>
2017-10-23 19:24:15 -0400

    Added DSCALN, an explicit-shape version of DSCAL.

--- util/BLAS/blas_module.F90
+++ util/BLAS/blas_module.F90
@@ -709,6 +709,73 @@ contains
     ! 
   end SUBROUTINE DGEMM
 
+
+#ifdef CUDA
+  attributes(device) &
+#endif    
+  SUBROUTINE DSCALN(N,DA,DX,INCX)
+
+  ! Only operates on vectors of size N
+
+    !$acc routine seq
+    !      .. Scalar Arguments ..
+    DOUBLE PRECISION DA
+    INTEGER INCX,N
+    !      ..
+    !      .. Array Arguments ..
+    DOUBLE PRECISION DX(N)
+    !      ..
+    ! 
+    !   Purpose
+    !   =======
+    ! *
+    !      scales a vector by a constant.
+    !      uses unrolled loops for increment equal to one.
+    !      jack dongarra, linpack, 3/11/78.
+    !      modified 3/93 to return if incx .le. 0.
+    !      modified 12/3/93, array(1) declarations changed to array(*)
+    ! 
+    ! 
+    !      .. Local Scalars ..
+    INTEGER I,M,MP1,NINCX
+    !      ..
+    !      .. Intrinsic Functions ..
+    INTRINSIC MOD
+    !      ..
+    IF (N.LE.0 .OR. INCX.LE.0) RETURN
+    IF (INCX.EQ.1) GO TO 20
+    ! 
+    !         code for increment not equal to 1
+    ! 
+    NINCX = N*INCX
+    DO I = 1,NINCX,INCX
+       DX(I) = DA*DX(I)
+    end do
+    RETURN
+    ! 
+    !         code for increment equal to 1
+    ! 
+    ! 
+    !         clean-up loop
+    ! 
+20  M = MOD(N,5)
+    IF (M.EQ.0) GO TO 40
+    DO I = 1,M
+       DX(I) = DA*DX(I)
+    end do
+    IF (N.LT.5) RETURN
+40  MP1 = M + 1
+    DO I = MP1,N,5
+       DX(I) = DA*DX(I)
+       DX(I+1) = DA*DX(I+1)
+       DX(I+2) = DA*DX(I+2)
+       DX(I+3) = DA*DX(I+3)
+       DX(I+4) = DA*DX(I+4)
+    end do
+    RETURN
+  END SUBROUTINE DSCALN
+
+  
 #ifdef CUDA
   attributes(device) &
 #endif    
