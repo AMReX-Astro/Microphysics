24c5d2513178efa8d05e3e01193059341da855e2
dwillcox <eugene.willcox@gmail.com>
2017-10-25 14:39:50 -0400

    Fixed some hanging references to neq and dvode_t % N from previous commit.

--- integration/VODE90/actual_integrator.F90
+++ integration/VODE90/actual_integrator.F90
@@ -225,7 +225,7 @@ contains
 
     ! Call the integration routine.
 
-    call dvode(neqs, y, local_time, local_time + dt, &
+    call dvode(y, local_time, local_time + dt, &
                ITOL, rtol, atol, ITASK, &
                istate, IOPT, rwork, iwork, MF_JAC, &
                rpar, ipar, dvode_state)
@@ -266,7 +266,7 @@ contains
 
        y(net_ienuc) = ener_offset
 
-       call dvode(neqs, y, local_time, local_time + dt, &
+       call dvode(y, local_time, local_time + dt, &
                   ITOL, rtol, atol, ITASK, &
                   istate, IOPT, rwork, iwork, MF_JAC, &
                   rpar, ipar, dvode_state)
@@ -341,7 +341,7 @@ contains
 
              y(net_ienuc) = ener_offset
 
-             call dvode(neqs, y, local_time, local_time + dt, &
+             call dvode(y, local_time, local_time + dt, &
                         ITOL, rtol, atol, ITASK, &
                         istate, IOPT, rwork, iwork, MF_JAC, &
                         rpar, ipar, dvode_state)
