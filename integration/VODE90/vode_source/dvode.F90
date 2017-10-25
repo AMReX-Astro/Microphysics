d7c391463d7ee92b8a6dc8937851f61ee68a1bc2
dwillcox <eugene.willcox@gmail.com>
2017-10-24 23:12:19 -0400

    Removed automatic arrays in dvnlsd call.

--- integration/VODE90/vode_source/dvode.F90
+++ integration/VODE90/vode_source/dvode.F90
@@ -1610,7 +1610,7 @@ contains
   
     type(dvode_t) :: vstate
     type(rwork_t) :: rwork
-    real(dp_t)    :: Y(vstate % N)
+    real(dp_t)    :: Y(VODE_NEQS)
     real(dp_t) :: RPAR(n_rpar_comps)
     integer    :: IWM(LIW), NFLAG, IPAR(n_ipar_comps)
     
