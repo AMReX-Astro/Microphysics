6d3b6331656489b018272167817b6063ae0800d9
dwillcox <eugene.willcox@gmail.com>
2017-10-25 14:32:00 -0400

    Do not carry dvode_t % N or dvode_t % NEQ. Do not pass neq to jacobian. Use VODE_NEQS throughout.

--- integration/VODE90/vode_rhs.F90
+++ integration/VODE90/vode_rhs.F90
@@ -102,7 +102,7 @@ contains
 #ifdef CUDA
   attributes(device) &
 #endif
-  subroutine jac(neq, time, y, ml, mu, pd, nrpd, rpar, ipar)
+  subroutine jac(time, y, ml, mu, pd, nrpd, rpar, ipar)
 
     !$acc routine seq
     
@@ -110,7 +110,7 @@ contains
     use bl_constants_module, only: ZERO
     use actual_rhs_module, only: actual_jac
     use burn_type_module, only: burn_t, net_ienuc, net_itemp
-    use vode_type_module, only: vode_to_burn, burn_to_vode
+    use vode_type_module, only: vode_to_burn, burn_to_vode, VODE_NEQS
     use rpar_indices, only: n_rpar_comps, irp_y_init, irp_t_sound, n_ipar_comps
     use bl_types, only: dp_t
     use extern_probin_module, only: burning_mode, burning_mode_factor, &
@@ -118,9 +118,9 @@ contains
 
     implicit none
 
-    integer   , intent(IN   ) :: neq, ml, mu, nrpd, ipar(n_ipar_comps)
-    real(dp_t), intent(INOUT) :: y(neq), rpar(n_rpar_comps), time
-    real(dp_t), intent(  OUT) :: pd(neq,neq)
+    integer   , intent(IN   ) :: ml, mu, nrpd, ipar(n_ipar_comps)
+    real(dp_t), intent(INOUT) :: y(VODE_NEQS), rpar(n_rpar_comps), time
+    real(dp_t), intent(  OUT) :: pd(VODE_NEQS,VODE_NEQS)
 
     type (burn_t) :: state
     real(dp_t) :: limit_factor, t_sound, t_enuc
