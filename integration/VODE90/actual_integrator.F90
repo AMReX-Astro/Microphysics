a6a7adc2150d954a7bd8bd2a963a151cbfebe4f8
dwillcox <eugene.willcox@gmail.com>
2017-10-27 15:46:44 -0400

    Put back 2 missing calls to dvode in `actual_integrator` I took out when revising the argument list.

--- integration/VODE90/actual_integrator.F90
+++ integration/VODE90/actual_integrator.F90
@@ -261,6 +261,9 @@ contains
 
        dvode_state % y(net_ienuc) = ener_offset
 
+       ! Call the integration routine.
+       call dvode(dvode_state, rwork, iwork, ITASK, IOPT, MF_JAC)
+
     endif
 
     ! If we still failed, print out the current state of the integration.
@@ -333,6 +336,9 @@ contains
              endif
 
              dvode_state % y(net_ienuc) = ener_offset
+
+             ! Call the integration routine.
+             call dvode(dvode_state, rwork, iwork, ITASK, IOPT, MF_JAC)
 
           enddo
 
