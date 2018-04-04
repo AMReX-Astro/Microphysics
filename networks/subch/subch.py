70adaecbb5ae846598a9efeadfc4cb43214a464a
Abigail Bishop <ms.abigailbishop@gmail.com>
2018-04-04 16:43:48 -0400

    Updated subch to include the C14(a,g)O18 reaction.

--- networks/subch/subch.py
+++ networks/subch/subch.py
@@ -3,19 +3,19 @@
 import pynucastro as pyna
 from pynucastro.networks import StarKillerNetwork
 
-library_file = "20180228default2"
+library_file = "20180319default2"
 mylibrary = pyna.rates.Library(library_file)
 
 subCh = pyna.rates.Library()
 
 all_reactants = [(("he4", "he4", "he4"), ("c12")),
-                 (("c12", "he4"), ("o16")),
-                 (("n14", "he4"), ("f18")),
-                 (("f18", "he4"), ("ne21", "p")),
-                 (("c12", "p"), ("n13")),
-                 (("n13", "he4"), ("o16", "p")),
-                 (("o16", "he4"), ("ne20"))]
-
+        (("c12", "he4"), ("o16")),
+        (("n14", "he4"), ("f18")),
+        (("f18", "he4"), ("ne21", "p")),
+        (("c12", "p"), ("n13")),
+        (("n13", "he4"), ("o16", "p")),
+        (("o16", "he4"), ("ne20")),
+        (("c14", "he4"), ("o18"))]
 
 for r, p in all_reactants:
     if not isinstance(p, tuple):
@@ -27,8 +27,3 @@ for r, p in all_reactants:
 
 net = StarKillerNetwork(libraries=[subCh])
 net.write_network()
-
-
-
-
-
