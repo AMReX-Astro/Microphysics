4aad2933b7824bc0ae2f11d76f26334ffe581c11
Andy Nonaka <AJNonaka@lbl.gov>
2018-05-16 16:39:04 -0700

    add burn_type module so this compiles

--- networks/general_null/actual_burner.f90
+++ networks/general_null/actual_burner.f90
@@ -3,32 +3,27 @@ module actual_burner_module
   use bl_types
   use bl_constants_module
   use network
-  use eos_type_module
-
-  implicit none
+  use burn_type_module
 
 contains
 
   subroutine actual_burner_init()
-
-    implicit none
 
     ! Do nothing in this burner.
 
   end subroutine actual_burner_init
 
 
-
   subroutine actual_burner(state_in, state_out, dt, time)
 
     implicit none
 
-    type (burn_t),    intent(in)    :: state_in
-    type (burn_t),    intent(inout) :: state_out
-    double precision, intent(in)    :: dt, time
+    type (burn_t), intent(in)    :: state_in
+    type (burn_t), intent(inout) :: state_out
+    double precision, intent(in) :: dt, time
 
     ! Do nothing in this burner.
-    
+
   end subroutine actual_burner
 
 end module actual_burner_module
