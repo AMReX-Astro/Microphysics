0994d5639f86334e916f3b7cbf304d1a83f98e6d
dwillcox <eugene.willcox@gmail.com>
2018-06-18 18:37:00 -0400

    Drop in test react driver from cudadevice.

--- unit_test/test_react/main.F90
+++ unit_test/test_react/main.F90
@@ -2,24 +2,29 @@
 ! call react_state() on the grid and output the results.
 
 program test_react
-
+#ifdef CUDA
+  use cudafor
+  use iso_c_binding, only: c_size_t
+#endif
+  use react_zones_module, only: pfidx_t, react_zones
+  
   use BoxLib
-  use amrex_constants_module
-  use amrex_fort_module, only : rt => amrex_real
-
+  use bl_constants_module
+  use bl_types
+  use bl_space
   use f2kcli
   use box_util_module
   use ml_layout_module
   use multifab_module
   use variables, only: init_variables, finalize_variables, plot_t
   use probin_module, only: dens_min, dens_max, &
-                           temp_min, temp_max, test_set, tmax, run_prefix, &
+                           temp_min, temp_max, test_set, run_prefix, &
                            small_temp, small_dens, do_acc
   use runtime_init_module
   use burn_type_module
   use actual_burner_module, only : actual_burner
   use microphysics_module
-  use eos_type_module, only : eos_get_small_temp, eos_get_small_dens
+  use eos_type_module, only : mintemp, mindens
   use network, only: nspec
   use util_module
   use fabio_module
@@ -31,7 +36,7 @@ program test_react
   ! Conventional fluid state multifabs
   type(multifab) , allocatable :: s(:)
 
-  real(rt) :: dx(1, MAX_SPACEDIM)
+  real(kind=dp_t) :: dx(1, MAX_SPACEDIM)
 
   logical :: pmask(MAX_SPACEDIM)
 
@@ -52,12 +57,19 @@ program test_react
 
   real(kind=dp_t), pointer :: sp(:,:,:,:)
 
-  real(kind=dp_t), allocatable :: state(:,:,:,:)
+  real(kind=dp_t), &
+#ifdef CUDA
+       managed, &
+#endif
+       allocatable :: state(:,:,:,:)
 
-  integer :: lo(MAX_SPACEDIM), hi(MAX_SPACEDIM)
+  integer, &
+#ifdef CUDA
+       managed, &
+#endif
+       allocatable :: lo(:), hi(:)
+  
   integer :: domlo(MAX_SPACEDIM), domhi(MAX_SPACEDIM)
-
-  type (burn_t) :: burn_state_in, burn_state_out
 
   real (kind=dp_t) :: dlogrho, dlogT
   real (kind=dp_t), allocatable :: xn_zone(:, :)
@@ -68,9 +80,21 @@ program test_react
 
   character (len=256) :: out_name
 
+  type(pfidx_t), &
+#ifdef CUDA
+       managed, &
+#endif
+       allocatable :: pfidx
+  
+#ifdef CUDA
+  character(len=200) :: cudaErrorMessage
+  integer :: istate
+  integer(c_size_t) :: stacksize
+  type(dim3) :: cuGrid, cuThreadBlock  
+#endif
+  
   call boxlib_initialize()
   call bl_prof_initialize(on = .true.)
-
 
   call runtime_init(.true.)
 
@@ -82,25 +106,19 @@ program test_react
 
   nlevs = mla % nlevel
   if (nlevs /= 1) then
-     call amrex_error("ERROR: only 1 level of refinement currently supported")
+     call bl_error("ERROR: only 1 level of refinement currently supported")
   endif
 
   dm = mla % dim
   if (dm /= 3) then
-     call amrex_error("ERROR: we require dm = 3")
+     call bl_error("ERROR: we require dm = 3")
   endif
 
   ! we don't care about dx -- we have no physical size
   dx(1,:) = ONE
 
   ! microphysics
-  call microphysics_init(small_temp=small_temp, small_dens=small_dens)
-
-  call eos_get_small_temp(small_temp)
-  print *, "small_temp = ", small_temp
-
-  call eos_get_small_dens(small_dens)
-  print *, "small_dens = ", small_dens
+  call microphysics_init(small_temp=mintemp, small_dens=mindens)
 
   ! we'll store everything in a multifab -- inputs and outputs
   call init_variables(pf)
@@ -135,12 +153,13 @@ program test_react
   enddo
 
   ! GPU doesn't like derived-types with bound procedures
-  itemp = pf % itemp
-  irho = pf % irho
-  ispec = pf % ispec
-  ispec_old = pf % ispec_old
-  irodot = pf % irodot
-  irho_hnuc = pf % irho_hnuc
+  allocate(pfidx)
+  pfidx % itemp = pf % itemp
+  pfidx % irho = pf % irho
+  pfidx % ispec = pf % ispec
+  pfidx % ispec_old = pf % ispec_old
+  pfidx % irodot = pf % irodot
+  pfidx % irho_hnuc = pf % irho_hnuc
 
   n = 1  ! single level assumption
 
@@ -148,6 +167,10 @@ program test_react
   n_rhs_max = -100000000
   n_rhs_min = 100000000
 
+  ! Allocate lo, hi
+  allocate(lo(MAX_SPACEDIM))
+  allocate(hi(MAX_SPACEDIM))
+  
   do i = 1, nfabs(s(n))
      sp => dataptr(s(n), i)
 
@@ -159,9 +182,9 @@ program test_react
      do kk = lo(3), hi(3)
         do jj = lo(2), hi(2)
            do ii = lo(1), hi(1)
-
+              
               state(ii, jj, kk, pf % itemp) = 10.0_dp_t**(log10(temp_min) + dble(jj)*dlogT)
-              state(ii, jj, kk, pf % irho) = 10.0_dp_t**(log10(dens_min) + dble(ii)*dlogrho)
+              state(ii, jj, kk, pf % irho)  = 10.0_dp_t**(log10(dens_min) + dble(ii)*dlogrho)
               state(ii, jj, kk, pf%ispec_old:pf%ispec_old+nspec-1) = max(xn_zone(:, kk), 1.e-10_dp_t)
 
            enddo
@@ -169,82 +192,53 @@ program test_react
      enddo
 
      ! Set up a timer for the burn.
-
      start_time = parallel_wtime()
 
-     !$OMP PARALLEL DO PRIVATE(ii,jj,kk,j) &
-     !$OMP PRIVATE(burn_state_in, burn_state_out) &
-     !$OMP REDUCTION(+:n_rhs_avg) REDUCTION(MAX:n_rhs_max) REDUCTION(MIN:n_rhs_min) &
-     !$OMP SCHEDULE(DYNAMIC,1)
+     write(*,*) 'lo = ', lo
+     write(*,*) 'hi = ', hi
+     
+#ifdef CUDA
+     ! Set up CUDA parameters
+     cuThreadBlock = dim3(2, 2, 2)
+     cuGrid = dim3(&
+          ceiling(real(hi(1)-lo(1)+1)/cuThreadBlock%x), &
+          ceiling(real(hi(2)-lo(2)+1)/cuThreadBlock%y), &
+          ceiling(real(hi(3)-lo(3)+1)/cuThreadBlock%z))
 
-     !$acc data copyin(temp_min, dlogT, dens_min, dlogrho, lo, hi, tmax) &
-     !$acc      copyin(itemp, irho, ispec, ispec_old, irodot, irho_hnuc) &
-     !$acc      copy(state(lo(1):hi(1),lo(2):hi(2),lo(3):hi(3),:)) if (do_acc == 1)
+     write(*,*) 'cuThreadBlock % x = ', cuThreadBlock % x
+     write(*,*) 'cuThreadBlock % y = ', cuThreadBlock % y
+     write(*,*) 'cuThreadBlock % z = ', cuThreadBlock % z
 
-     !$acc parallel reduction(+:n_rhs_avg) reduction(max:n_rhs_max) reduction(min:n_rhs_min) if (do_acc == 1)
+     write(*,*) 'cuGrid % x = ', cuGrid % x
+     write(*,*) 'cuGrid % y = ', cuGrid % y
+     write(*,*) 'cuGrid % z = ', cuGrid % z
 
-     !$acc loop gang vector collapse(3) &
-     !$acc private(burn_state_in, burn_state_out, ii, jj, kk, j)
+     istate = cudaDeviceSetCacheConfig(cudaFuncCachePreferL1)
+     if (istate /= 0) then
+        cudaErrorMessage = cudaGetErrorString(istate)
+        write(*,*) cudaErrorMessage
+     end if
+     
+     ! Uncomment to configure Stack Size Limit
+     ! stacksize = 64000
+     ! istate = cudaDeviceSetLimit(cudaLimitStackSize, stacksize)
+     ! write(*,*) 'limiting stack size to ', stacksize, ' with return code ', istate
 
-     do kk = lo(3), hi(3)
-        do jj = lo(2), hi(2)
-           do ii = lo(1), hi(1)
+     ! React the zones using CUDA
+     call react_zones<<<cuGrid,cuThreadBlock>>>(state, pfidx, lo, hi)
+     istate = cudaDeviceSynchronize()     
+#else
+     call react_zones(state, pfidx, lo, hi)
+#endif
+     
+     sp(:,:,:,:) = state(:,:,:,:)
 
-              burn_state_in % rho = state(ii, jj, kk, irho)
-              burn_state_in % T = state(ii, jj, kk, itemp)
-              do j = 1, nspec
-                 burn_state_in % xn(j) = state(ii, jj, kk, ispec_old + j - 1)
-              enddo
-
-              call normalize_abundances_burn(burn_state_in)
-
-              ! the integrator doesn't actually care about the initial internal
-              ! energy.
-              burn_state_in % e = ZERO
-
-              call actual_burner(burn_state_in, burn_state_out, tmax, ZERO)
-
-              do j = 1, nspec
-                 state(ii, jj, kk, ispec + j - 1) = burn_state_out % xn(j)
-              enddo
-
-              do j=1, nspec
-                 ! an explicit loop is needed here to keep the GPU happy
-                 state(ii, jj, kk, irodot + j - 1) = &
-                      (burn_state_out % xn(j) - burn_state_in % xn(j)) / tmax
-              enddo
-
-              state(ii, jj, kk, irho_hnuc) = &
-                   state(ii, jj, kk, irho) * (burn_state_out % e - burn_state_in % e) / tmax
-
-              n_rhs_avg = n_rhs_avg + burn_state_out % n_rhs
-              n_rhs_min = min(n_rhs_min, burn_state_out % n_rhs)
-              n_rhs_max = max(n_rhs_max, burn_state_out % n_rhs)
-
-           enddo
-        enddo
-     enddo
-     !$acc end parallel
-     !$acc end data
-
-     !$OMP END PARALLEL DO
-
-     ! End the timer and print the results.
-
+     ! End the timer and print the results.     
      end_time = parallel_wtime()
 
      print *, "Execution time: ", end_time - start_time
-
-     sp(:,:,:,:) = state(:,:,:,:)
+     
   enddo
-
-  ! note: integer division
-  n_rhs_avg = n_rhs_avg/(nT*nrho*nX)
-
-  print *, "RHS stats:"
-  print *, "  min: ", n_rhs_min
-  print *, "  avg: ", n_rhs_avg
-  print *, "  max: ", n_rhs_max
 
   ! output
   out_name = trim(run_prefix) // "test_react." // trim(integrator_dir)
@@ -253,7 +247,7 @@ program test_react
 
   call write_job_info(out_name, mla%mba)
 
-
+  write(*,*) 'Cleanup...'
   ! if you (or a subroutine) built it, destroy it!
   do n = 1,nlevs
     call destroy(s(n))
@@ -263,12 +257,12 @@ program test_react
 
   call finalize_variables(pf)
 
+  deallocate(pfidx)
   deallocate(state)
   deallocate(s)
   deallocate(xn_zone)
 
   call runtime_close()
-
 
   call microphysics_finalize()
 
@@ -278,5 +272,5 @@ program test_react
   call boxlib_finalize()
 
   call send_success_return_code()
-
+  
 end program test_react
