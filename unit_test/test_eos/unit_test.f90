24ca102835584c2b024c1eaa174546bd69661e0b
Michael Zingale <michael.zingale@stonybrook.edu>
2018-07-05 14:20:40 -0400

    almost there...

--- unit_test/test_eos/unit_test.f90
+++ unit_test/test_eos/unit_test.f90
@@ -1,6 +1,8 @@
 subroutine init_unit_test(name, namlen) bind(C, name="init_unit_test")
 
   use amrex_fort_module, only: rt => amrex_real
+  use extern_probin_module
+  use microphysics_module
 
   implicit none
 
@@ -8,5 +10,7 @@ subroutine init_unit_test(name, namlen) bind(C, name="init_unit_test")
   integer, intent(in) :: name(namlen)
 
   call runtime_init(name, namlen)
+
+  call microphysics_init(small_temp, small_dens)
 
 end subroutine init_unit_test
