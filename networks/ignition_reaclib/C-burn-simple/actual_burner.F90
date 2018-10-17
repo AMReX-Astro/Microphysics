174bad6c8b6b419acbe79ca649871d36461a98b4
Donald E. Willcox <dewillcox@lbl.gov>
2018-10-16 18:03:03 -0700

    pynucastro -- Updated C-burn-simple network

--- networks/ignition_reaclib/C-burn-simple/actual_burner.F90
+++ networks/ignition_reaclib/C-burn-simple/actual_burner.F90
@@ -1,9 +1,6 @@
 module actual_burner_module
 
-  use amrex_constants_module
-  use amrex_fort_module, only : rt => amrex_real
   use network
-  use burn_type_module
 
   implicit none
 
@@ -11,9 +8,9 @@ contains
 
   subroutine actual_burner_init()
 
-    use integrator_module, only: integrator_init
     use reaclib_rates, only: init_reaclib, net_screening_init
     use table_rates, only: init_tabular
+    use integrator_module, only: integrator_init
 
     implicit none
 
@@ -24,6 +21,7 @@ contains
     call net_screening_init()
     
   end subroutine actual_burner_init
+
 
   subroutine actual_burner_finalize
     use reaclib_rates, only: term_reaclib, net_screening_finalize
@@ -36,20 +34,23 @@ contains
     call net_screening_finalize()
   end subroutine actual_burner_finalize
 
+
   subroutine actual_burner(state_in, state_out, dt, time)
 
     !$acc routine seq
-    !$gpu
 
+    use amrex_fort_module, only: rt => amrex_real
     use integrator_module, only: integrator
+    use burn_type_module, only: burn_t
 
     implicit none
 
     type (burn_t),    intent(in   ) :: state_in
     type (burn_t),    intent(inout) :: state_out
-    double precision, intent(in   ) :: dt, time
+    real(kind=rt),    intent(in   ) :: dt, time
 
-    ! Calling integrator ...
+    !$gpu
+
     call integrator(state_in, state_out, dt, time)
   end subroutine actual_burner
 
