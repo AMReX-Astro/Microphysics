5979b1430d01958f3a292e53b651045e004a3744
Michael Zingale <michael.zingale@stonybrook.edu>
2018-10-25 19:00:07 -0400

    enable reverse rates

--- networks/ECSN/ecsn.py
+++ networks/ECSN/ecsn.py
@@ -8,10 +8,10 @@ mylibrary = pyna.rates.Library(library_file)
 
 all_nuclei = ["p", "he4",
               "ne20", "o20", "f20",
-              "mg24", "o16",
+              "mg24", "al27", "o16",
               "si28", "s32", "p31"]
 
-ecsn_library = mylibrary.linking_nuclei(all_nuclei, with_reverse=False)
+ecsn_library = mylibrary.linking_nuclei(all_nuclei, with_reverse=True)
 
 print(ecsn_library)
 
