7acdf011c6a14e754f7f072e68aaee1dd6510936
sailoridy <richardidy@gmail.com>
2019-03-11 23:01:32 -0400

    ECSN

---
+++ networks/network_test/actual_network.F90
@@ -0,0 +1,222 @@
+module actual_network
+
+  use physical_constants, only: ERG_PER_MeV
+  use amrex_fort_module, only: rt => amrex_real
+
+  implicit none
+
+  public
+
+  real(rt), parameter :: avo = 6.0221417930d23
+  real(rt), parameter :: c_light = 2.99792458d10
+  real(rt), parameter :: enuc_conv2 = -avo*c_light*c_light
+
+  real(rt), parameter :: ev2erg  = 1.60217648740d-12
+  real(rt), parameter :: mev2erg = ev2erg*1.0d6
+  real(rt), parameter :: mev2gr  = mev2erg/c_light**2
+
+  real(rt), parameter :: mass_neutron  = 1.67492721184d-24
+  real(rt), parameter :: mass_proton   = 1.67262163783d-24
+  real(rt), parameter :: mass_electron = 9.10938215450d-28
+
+  integer, parameter :: nrates = 1
+  integer, parameter :: num_rate_groups = 4
+
+  ! Evolution and auxiliary
+  integer, parameter :: nspec_evolve = 2
+  integer, parameter :: naux  = 0
+
+  ! Number of nuclear species in the network
+  integer, parameter :: nspec = 2
+
+  ! Number of reaclib rates
+  integer, parameter :: nrat_reaclib = 0
+  integer, parameter :: number_reaclib_sets = 0
+
+  ! Number of tabular rates
+  integer, parameter :: nrat_tabular = 1
+
+  ! Binding Energies Per Nucleon (MeV)
+  real(rt) :: ebind_per_nucleon(nspec)
+
+  ! aion: Nucleon mass number A
+  ! zion: Nucleon atomic number Z
+  ! nion: Nucleon neutron number N
+  ! bion: Binding Energies (ergs)
+
+  ! Nuclides
+  integer, parameter :: jf20   = 1
+  integer, parameter :: jne20   = 2
+
+  ! Reactions
+  integer, parameter :: k_ne20__f20   = 1
+
+  ! reactvec indices
+  integer, parameter :: i_rate        = 1
+  integer, parameter :: i_drate_dt    = 2
+  integer, parameter :: i_scor        = 3
+  integer, parameter :: i_dscor_dt    = 4
+  integer, parameter :: i_dqweak      = 5
+  integer, parameter :: i_epart       = 6
+
+  character (len=16), save :: spec_names(nspec)
+  character (len= 5), save :: short_spec_names(nspec)
+  character (len= 5), save :: short_aux_names(naux)
+
+  real(rt), allocatable, save :: aion(:), zion(:), bion(:)
+  real(rt), allocatable, save :: nion(:), mion(:), wion(:)
+
+#ifdef AMREX_USE_CUDA
+  attributes(managed) :: aion, zion, bion, nion, mion, wion
+#endif
+
+  !$acc declare create(aion, zion, bion, nion, mion, wion)
+
+#ifdef REACT_SPARSE_JACOBIAN
+  ! Shape of Jacobian in Compressed Sparse Row format
+  integer, parameter   :: NETWORK_SPARSE_JAC_NNZ = 12
+  integer, allocatable :: csr_jac_col_index(:), csr_jac_row_count(:)
+
+#ifdef AMREX_USE_CUDA
+  attributes(managed) :: csr_jac_col_index, csr_jac_row_count
+#endif
+#endif
+
+contains
+
+  subroutine actual_network_init()
+
+    implicit none
+
+    integer :: i
+
+    ! Allocate ion info arrays
+    allocate(aion(nspec))
+    allocate(zion(nspec))
+    allocate(bion(nspec))
+    allocate(nion(nspec))
+    allocate(mion(nspec))
+    allocate(wion(nspec))
+
+    spec_names(jf20)   = "fluorine-20"
+    spec_names(jne20)   = "neon-20"
+
+    short_spec_names(jf20)   = "f20"
+    short_spec_names(jne20)   = "ne20"
+
+    ebind_per_nucleon(jf20)   = 7.72013400000000d+00
+    ebind_per_nucleon(jne20)   = 8.03224000000000d+00
+
+    aion(jf20)   = 2.00000000000000d+01
+    aion(jne20)   = 2.00000000000000d+01
+
+    zion(jf20)   = 9.00000000000000d+00
+    zion(jne20)   = 1.00000000000000d+01
+
+    nion(jf20)   = 1.10000000000000d+01
+    nion(jne20)   = 1.00000000000000d+01
+
+    do i = 1, nspec
+       bion(i) = ebind_per_nucleon(i) * aion(i) * ERG_PER_MeV
+    end do
+
+    ! Set the mass
+    mion(:) = nion(:) * mass_neutron + zion(:) * (mass_proton + mass_electron) &
+         - bion(:)/(c_light**2)
+
+    ! Molar mass
+    wion(:) = avo * mion(:)
+
+    ! Common approximation
+    !wion(:) = aion(:)
+
+    !$acc update device(aion, zion, bion, nion, mion, wion)
+
+#ifdef REACT_SPARSE_JACOBIAN
+    ! Set CSR format metadata for Jacobian
+    allocate(csr_jac_col_index(NETWORK_SPARSE_JAC_NNZ))
+    allocate(csr_jac_row_count(nspec_evolve + 3)) ! neq + 1
+
+    csr_jac_col_index = [ &
+      1, &
+      2, &
+      3, &
+      2, &
+      3, &
+      1, &
+      2, &
+      3, &
+      1, &
+      2, &
+      3, &
+      4  ]
+
+    csr_jac_row_count = [ &
+      1, &
+      4, &
+      6, &
+      9, &
+      13  ]
+#endif
+
+  end subroutine actual_network_init
+
+
+  subroutine actual_network_finalize()
+    ! Deallocate storage arrays
+
+    if (allocated(aion)) then
+       deallocate(aion)
+    endif
+
+    if (allocated(zion)) then
+       deallocate(zion)
+    endif
+
+    if (allocated(bion)) then
+       deallocate(bion)
+    endif
+
+    if (allocated(nion)) then
+       deallocate(nion)
+    endif
+
+    if (allocated(mion)) then
+       deallocate(mion)
+    endif
+
+    if (allocated(wion)) then
+       deallocate(wion)
+    endif
+
+#ifdef REACT_SPARSE_JACOBIAN
+    if (allocated(csr_jac_col_index)) then
+       deallocate(csr_jac_col_index)
+    endif
+
+    if (allocated(csr_jac_row_count)) then
+       deallocate(csr_jac_row_count)
+    endif
+#endif
+
+  end subroutine actual_network_finalize
+
+
+  subroutine ener_gener_rate(dydt, enuc)
+    ! Computes the instantaneous energy generation rate
+
+    !$acc routine seq
+
+    implicit none
+
+    real(rt) :: dydt(nspec), enuc
+
+    !$gpu
+
+    ! This is basically e = m c**2
+
+    enuc = sum(dydt(:) * mion(:)) * enuc_conv2
+
+  end subroutine ener_gener_rate
+
+end module actual_network
