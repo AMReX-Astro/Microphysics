1eb558380bc4becc73adb6dca4e1cb37ca17630a
Donald E. Willcox <eugene.willcox@gmail.com>
2019-04-03 16:35:25 -0700

    Use the python 3 version, f2py3, in dof2py.sh

--- python_library/dof2py.sh
+++ python_library/dof2py.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 cd tmp_build_dir/o/3d.gnu.EXE
-f2py -c -m _StarKillerMicrophysics *.o f90wrap_*.f90 -lstdc++
+f2py3 -c -m _StarKillerMicrophysics *.o f90wrap_*.f90 -lstdc++
 cd ../../..
 mv tmp_build_dir/o/3d.gnu.EXE/*.so .
