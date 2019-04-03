a9a80614536b77d547c8e39107b94000007fcad1
Donald E. Willcox <eugene.willcox@gmail.com>
2019-04-03 16:45:02 -0700

    Add the StarKiller python package

---
+++ python_library/StarKiller/initialization/initialization.py
@@ -0,0 +1,5 @@
+import StarKillerMicrophysics as SKM
+
+def starkiller_initialize(probin_filename):
+    skinit = SKM.Starkiller_Initialization_Module()
+    skinit.starkiller_initialize(probin_filename)
