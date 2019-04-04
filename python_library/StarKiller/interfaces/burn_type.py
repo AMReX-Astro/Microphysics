f196a5368f84770817cf00946461b369e19d19ab
Donald E. Willcox <eugene.willcox@gmail.com>
2019-04-04 02:02:04 -0700

    Make BurnType a non-static class.

--- python_library/StarKiller/interfaces/burn_type.py
+++ python_library/StarKiller/interfaces/burn_type.py
@@ -1,27 +1,25 @@
 import StarKillerMicrophysics as SKM
 from StarKiller.interfaces import EosType
 
-BurnTypeModule = SKM.Burn_Type_Module()
-
 class BurnType(object):
-    neqs = BurnTypeModule.neqs
-    
     def __init__(self):
-        self.state = BurnTypeModule.burn_t()
+        self.BurnTypeModule = SKM.Burn_Type_Module()
+        self.neqs = self.BurnTypeModule.neqs
+        self.state = self.BurnTypeModule.burn_t()
 
     def to_eos_type(self):
         # Return an EosType object from this BurnType
         eos_state = EosType()
-        BurnTypeModule.burn_to_eos(self.state, eos_state.state)
+        self.BurnTypeModule.burn_to_eos(self.state, eos_state.state)
         return eos_state
         
     def from_eos_type(self, eos_state):
         # Given an EosType object, set this BurnType data
-        BurnTypeModule.eos_to_burn(eos_state.state, self.state)
+        self.BurnTypeModule.eos_to_burn(eos_state.state, self.state)
 
     def copy(self):
         # Return a deep copy of this object
         state_copy = BurnType()
-        BurnTypeModule.copy_burn_t(state_copy.state, self.state)
+        self.BurnTypeModule.copy_burn_t(state_copy.state, self.state)
         return state_copy
 
