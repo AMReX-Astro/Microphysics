9beed57adfb6ef3e44dba4ee89e016629c5614ad
Donald E. Willcox <eugene.willcox@gmail.com>
2019-04-04 00:34:05 -0700

    Use nonstatic class for Eos and Network, as Network needs initialization from the python library.

--- python_library/StarKiller/network/network.py
+++ python_library/StarKiller/network/network.py
@@ -2,27 +2,26 @@ import StarKillerMicrophysics as SKM
 from StarKiller.interfaces import BurnType, EosType
 from StarKiller.eos import Eos
 
-NetworkModule = SKM.Network()
-ActualNetworkModule = SKM.Actual_Network()
-RHSModule = SKM.actual_rhs_module
-
 class Network(object):
-    nspec = ActualNetworkModule.nspec
-    nspec_evolve = ActualNetworkModule.nspec_evolve
+    def __init__(self):
+        self.NetworkModule = SKM.Network()
+        self.ActualNetworkModule = SKM.Actual_Network()
+        self.RHSModule = SKM.actual_rhs_module
 
-    # These are python zero based indexes
-    net_itemp = nspec_evolve
-    net_ienuc = nspec_evolve + 1
+        self.nspec = self.ActualNetworkModule.nspec
+        self.nspec_evolve = self.ActualNetworkModule.nspec_evolve
+
+        # These are python zero based indexes
+        self.net_itemp = self.nspec_evolve
+        self.net_ienuc = self.nspec_evolve + 1
     
-    short_species_names = [NetworkModule.get_network_short_species_name(i+1).decode("ASCII").strip().lower() for i in range(nspec)]
-    species_names = [NetworkModule.get_network_species_name(i+1).decode("ASCII").strip().lower() for i in range(nspec)]
+        self.short_species_names = [self.NetworkModule.get_network_short_species_name(i+1).decode("ASCII").strip().lower() for i in range(self.nspec)]
+        self.species_names = [self.NetworkModule.get_network_species_name(i+1).decode("ASCII").strip().lower() for i in range(self.nspec)]
 
-    @staticmethod
-    def rhs(burn_state):
+    def rhs(self, burn_state):
         Eos.evaluate(EosType.eos_input_rt, burn_state)
-        RHSModule.actual_rhs(burn_state.state)
+        self.RHSModule.actual_rhs(burn_state.state)
 
-    @staticmethod
-    def jacobian(burn_state):
+    def jacobian(self, burn_state):
         Eos.evaluate(EosType.eos_input_rt, burn_state)
-        RHSModule.actual_jac(burn_state.state)
+        self.RHSModule.actual_jac(burn_state.state)
