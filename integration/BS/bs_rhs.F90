1d0e8cf0a72efa7b9a6a7792069f93458b73ca00
Michael Zingale <michael.zingale@stonybrook.edu>
2019-07-15 18:52:35 -0400

    remove unused variables

--- integration/BS/bs_rhs.F90
+++ integration/BS/bs_rhs.F90
@@ -18,13 +18,11 @@ contains
     use extern_probin_module, only: integrate_temperature, integrate_energy, react_boost
     use bs_type_module, only: bs_t, clean_state, renormalize_species, update_thermodynamics, &
                               burn_to_bs, bs_to_burn
-    use rpar_indices, only: irp_y_init, irp_t_sound
+    use rpar_indices, only: irp_y_init
 
     implicit none
 
     type (bs_t) :: bs
-
-    real(rt) :: limit_factor, t_sound, t_enuc
 
     ! We are integrating a system of
     !
