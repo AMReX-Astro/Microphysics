84e18266ffe3d771cf0592fa576eb29a225d9ea8
Donald E. Willcox <dewillcox@lbl.gov>
2019-09-17 18:02:35 -0700

    Create a RHS wrapper for the integrators to call.

---
+++ integration/integrator_rhs.F90
@@ -0,0 +1,48 @@
+module integrator_rhs_module
+
+  implicit none
+
+  public
+
+contains
+
+  subroutine integrator_rhs(state)
+
+    use actual_rhs_module, only: actual_rhs
+    use burn_type_module, only: burn_t
+
+#ifdef NONAKA_PLOT
+    use nonaka_plot_module, only: nonaka_rhs
+#endif
+
+    implicit none
+
+    type(burn_t), intent(inout) :: state
+
+    !$gpu
+
+    call actual_rhs(state)
+
+#ifdef NONAKA_PLOT
+    call nonaka_rhs(state)
+#endif
+
+  end subroutine integrator_rhs
+
+
+  subroutine integrator_jac(state)
+
+    use actual_rhs_module, only: actual_jac
+    use burn_type_module, only: burn_t
+
+    implicit none
+
+    type(burn_t), intent(inout) :: state
+
+    !$gpu
+
+    call actual_jac(state)
+
+  end subroutine integrator_jac
+
+end module integrator_rhs_module
