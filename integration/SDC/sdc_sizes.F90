e4972cbcf5357e921afe2e8660a1e4f3bf5e1255
Michael Zingale <michael.zingale@stonybrook.edu>
2019-09-28 18:23:27 -0400

    add missing files

---
+++ integration/SDC/sdc_sizes.F90
@@ -0,0 +1,20 @@
+module sdc_sizes_module
+
+  use amrex_fort_module, only : rt => amrex_real
+
+  implicit none
+
+  ! these are parameters for the SDC 4th order Radau method
+  integer, parameter :: SDC_NODES = 4
+  integer, parameter :: SDC_MAX_ITERATIONS = 4
+
+  real(rt), parameter :: dt_sdc(SDC_NODES) = [0.0d0, (4.0d0 - sqrt(6.0d0))/10.0d0, &
+                                   (4.0d0 + sqrt(6.0d0))/10.0d0, 1.0d0]
+  real(rt), parameter :: node_weights(SDC_NODES) = [0.0d0, (16.0d0 - sqrt(6.0d0))/36.0d0, &
+                                        (16.0d0 + sqrt(6.0d0))/36.0d0, 1.0d0/9.0d0]
+
+  integer, parameter :: NEWTON_SUCCESS = 0
+  integer, parameter :: SINGULAR_MATRIX = -1
+  integer, parameter :: CONVERGENCE_FAILURE = -2
+
+end module sdc_sizes_module
