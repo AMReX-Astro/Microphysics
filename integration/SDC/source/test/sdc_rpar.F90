579b8b9a2686b323cc3110cf54a4c6b6020f4057
Michael Zingale <michael.zingale@stonybrook.edu>
2019-09-28 20:05:02 -0400

    some reorg to mimic cuVODE

---
+++ integration/SDC/source/test/sdc_rpar.F90
@@ -0,0 +1,12 @@
+! rpar is a set of real quantities that we use to convert auxiliary data back
+! and forth between the rhs and jacobian routines and the calling scope for dvode.
+!
+! This module is a stub for the user to extend, use this to store indices.
+!
+module sdc_rpar_indices
+
+  implicit none
+
+  integer, parameter :: n_rpar_comps = 0
+
+end module sdc_rpar_indices
