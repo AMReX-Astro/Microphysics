30e528b8d591bdc7864503535a654ae17cc60a35
Michael Zingale <michael.zingale@stonybrook.edu>
2019-11-18 17:01:13 -0500

    fix compilation

--- integration/VODE90/vode_integrator_true_sdc.F90
+++ integration/VODE90/vode_integrator_true_sdc.F90
@@ -16,7 +16,9 @@ contains
   end subroutine vode_integrator_init
 
 
-  subroutine vode_integrator(state_in, state_out, dt, time)
+  subroutine vode_integrator(state_in, state_out, dt, time, status)
+
+    use integration_data
 
     implicit none
 
@@ -25,6 +27,7 @@ contains
     type (burn_t), intent(in   ) :: state_in
     type (burn_t), intent(inout) :: state_out
     real(rt),    intent(in   ) :: dt, time
+    type (integration_status_t), intent(inout) :: status
 
   end subroutine vode_integrator
 
