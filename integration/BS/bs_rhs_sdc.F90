280d140d8c7d3db6402bfe18f0711edb1c3577f0
Alice Harpole <aliceharpole@gmail.com>
2019-12-06 16:28:47 -0500

    fix compilation of BS

--- integration/BS/bs_rhs_sdc.F90
+++ integration/BS/bs_rhs_sdc.F90
@@ -14,6 +14,7 @@ contains
     use network_rhs_module, only: network_rhs
     use bs_type_module, only: bs_t, clean_state, rhs_to_bs, bs_to_burn
     use bs_rpar_indices, only: irp_t0
+    use microphysics_type_module, only: ZERO
 
     implicit none
 
