e56445773a5385259fc9e98b7756f76305cbb9aa
Donald E. Willcox <eugene.willcox@gmail.com>
2019-12-11 14:06:10 -0800

    Fix helmholtz initialization for python package. If not found, attempt to symlink it from MICROPHYSICS_HOME.

--- python_library/StarKiller/StarKiller/initialization/initialization.py
+++ python_library/StarKiller/StarKiller/initialization/initialization.py
@@ -2,7 +2,6 @@ import StarKillerMicrophysics as SKM
 from StarKiller.eos import Eos
 
 def starkiller_initialize(probin_filename):
-    eos = Eos()
-    with eos._initialize_safe():
+    with Eos():
         skinit = SKM.Starkiller_Initialization_Module()
         skinit.starkiller_initialize(probin_filename)
