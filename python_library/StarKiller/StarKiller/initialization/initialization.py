b3d8f1fb90a26f76fd634ab4b00857d4570e80c6
Donald E. Willcox <eugene.willcox@gmail.com>
2019-12-11 14:41:50 -0800

    Change initialization safety checking a bit, print EOS, network name after initialization.

--- python_library/StarKiller/StarKiller/initialization/initialization.py
+++ python_library/StarKiller/StarKiller/initialization/initialization.py
@@ -1,7 +1,26 @@
 import StarKillerMicrophysics as SKM
 from StarKiller.eos import Eos
+from StarKiller.network import Network
 
 def starkiller_initialize(probin_filename):
-    with Eos():
-        skinit = SKM.Starkiller_Initialization_Module()
-        skinit.starkiller_initialize(probin_filename)
+    try:
+        Eos._initialize_safe()
+    except:
+        print("EOS cannot be initialized.")
+        raise
+
+    try:
+        Network._initialize_safe()
+    except:
+        print("Network cannot be initialized.")
+        raise
+
+    skinit = SKM.Starkiller_Initialization_Module()
+    skinit.starkiller_initialize(probin_filename)
+
+    eos = Eos()
+    net = Network()
+
+    print("\nInitialized StarKiller with ...")
+    print("- EOS:     {}".format(eos.name))
+    print("- Network: {}".format(net.name))
