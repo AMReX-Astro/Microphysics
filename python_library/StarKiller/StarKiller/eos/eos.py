29e289e1928e587367c53274de8eaac6b6cf25a9
Donald E. Willcox <eugene.willcox@gmail.com>
2019-12-11 11:52:25 -0800

    StarKiller package fails gracefully if using the Helmholtz EOS and the table is not found.

--- python_library/StarKiller/StarKiller/eos/eos.py
+++ python_library/StarKiller/StarKiller/eos/eos.py
@@ -1,9 +1,24 @@
 import StarKillerMicrophysics as SKM
 from StarKiller.interfaces import EosType
+import os.path
 
 class Eos(object):
     def __init__(self):
         self.EosModule = SKM.Eos_Module()
+        self.name = self.EosModule.get_eos_name().decode("ASCII").strip().lower()
 
     def evaluate(self, input_mode, input_state, use_raw_inputs=False):
         self.EosModule.eos(input_mode, input_state.state, use_raw_inputs)
+
+    def _initialize_safe(self):
+        if (self.name == "helmholtz"):
+            # check to see if the Helmholtz table is in the
+            # current working directory
+            try:
+                assert(os.path.isfile("helm_table.dat"))
+            except AssertionError:
+                print("helm_table.dat file or symlink is missing from the working directory")
+                raise
+            except:
+                raise
+        return True
