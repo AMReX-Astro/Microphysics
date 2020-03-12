eafa2bcef81e34af6ec239d838b32a1a7ed808c6
Michael Zingale <michael.zingale@stonybrook.edu>
2020-03-12 03:39:54 -0400

    Place the Helmholtz globals in a namespace (#271)
    Place the Helmholtz global variables in a helmholtz namespace; this helps avoid shadowing.
    
    Co-authored-by: Max Katz <maxpkatz@gmail.com>

--- EOS/helmholtz/actual_eos_data.cpp
+++ EOS/helmholtz/actual_eos_data.cpp
@@ -1,46 +1,46 @@
 #include <actual_eos_data.H>
 
-AMREX_GPU_MANAGED bool do_coulomb;
-AMREX_GPU_MANAGED bool input_is_constant;
+AMREX_GPU_MANAGED bool helmholtz::do_coulomb;
+AMREX_GPU_MANAGED bool helmholtz::input_is_constant;
 
-AMREX_GPU_MANAGED int itmax;
-AMREX_GPU_MANAGED int jtmax;
+AMREX_GPU_MANAGED int helmholtz::itmax;
+AMREX_GPU_MANAGED int helmholtz::jtmax;
 
-AMREX_GPU_MANAGED amrex::Real d[imax];
-AMREX_GPU_MANAGED amrex::Real t[jmax];
+AMREX_GPU_MANAGED amrex::Real helmholtz::d[imax];
+AMREX_GPU_MANAGED amrex::Real helmholtz::t[jmax];
 
-AMREX_GPU_MANAGED amrex::Real tlo;
-AMREX_GPU_MANAGED amrex::Real thi;
-AMREX_GPU_MANAGED amrex::Real tstp;
-AMREX_GPU_MANAGED amrex::Real tstpi;
+AMREX_GPU_MANAGED amrex::Real helmholtz::tlo;
+AMREX_GPU_MANAGED amrex::Real helmholtz::thi;
+AMREX_GPU_MANAGED amrex::Real helmholtz::tstp;
+AMREX_GPU_MANAGED amrex::Real helmholtz::tstpi;
 
-AMREX_GPU_MANAGED amrex::Real dlo;
-AMREX_GPU_MANAGED amrex::Real dhi;
-AMREX_GPU_MANAGED amrex::Real dstp;
-AMREX_GPU_MANAGED amrex::Real dstpi;
+AMREX_GPU_MANAGED amrex::Real helmholtz::dlo;
+AMREX_GPU_MANAGED amrex::Real helmholtz::dhi;
+AMREX_GPU_MANAGED amrex::Real helmholtz::dstp;
+AMREX_GPU_MANAGED amrex::Real helmholtz::dstpi;
 
-AMREX_GPU_MANAGED amrex::Real ttol;
-AMREX_GPU_MANAGED amrex::Real dtol;
+AMREX_GPU_MANAGED amrex::Real helmholtz::ttol;
+AMREX_GPU_MANAGED amrex::Real helmholtz::dtol;
 
 // for the helmholtz free energy tables
-AMREX_GPU_MANAGED amrex::Real f[jmax][imax][9];
+AMREX_GPU_MANAGED amrex::Real helmholtz::f[jmax][imax][9];
 
 // for the pressure derivative with density tables
-AMREX_GPU_MANAGED amrex::Real dpdf[jmax][imax][4];
+AMREX_GPU_MANAGED amrex::Real helmholtz::dpdf[jmax][imax][4];
 
 // for chemical potential tables
-AMREX_GPU_MANAGED amrex::Real ef[jmax][imax][4];
+AMREX_GPU_MANAGED amrex::Real helmholtz::ef[jmax][imax][4];
 
 // for the number density tables
-AMREX_GPU_MANAGED amrex::Real xf[jmax][imax][4];
+AMREX_GPU_MANAGED amrex::Real helmholtz::xf[jmax][imax][4];
 
 // for storing the differences
-AMREX_GPU_MANAGED amrex::Real dt_sav[jmax];
-AMREX_GPU_MANAGED amrex::Real dt2_sav[jmax];
-AMREX_GPU_MANAGED amrex::Real dti_sav[jmax];
-AMREX_GPU_MANAGED amrex::Real dt2i_sav[jmax];
+AMREX_GPU_MANAGED amrex::Real helmholtz::dt_sav[jmax];
+AMREX_GPU_MANAGED amrex::Real helmholtz::dt2_sav[jmax];
+AMREX_GPU_MANAGED amrex::Real helmholtz::dti_sav[jmax];
+AMREX_GPU_MANAGED amrex::Real helmholtz::dt2i_sav[jmax];
 
-AMREX_GPU_MANAGED amrex::Real dd_sav[imax];
-AMREX_GPU_MANAGED amrex::Real dd2_sav[imax];
-AMREX_GPU_MANAGED amrex::Real ddi_sav[imax];
-AMREX_GPU_MANAGED amrex::Real dd2i_sav[imax];
+AMREX_GPU_MANAGED amrex::Real helmholtz::dd_sav[imax];
+AMREX_GPU_MANAGED amrex::Real helmholtz::dd2_sav[imax];
+AMREX_GPU_MANAGED amrex::Real helmholtz::ddi_sav[imax];
+AMREX_GPU_MANAGED amrex::Real helmholtz::dd2i_sav[imax];
