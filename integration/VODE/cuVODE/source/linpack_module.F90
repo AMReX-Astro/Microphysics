d116b5c19fff3434477eb19bb260b7116688b448
Max Katz <maxpkatz@gmail.com>
2020-03-18 09:48:34 -0700

    Remove transpose version of dgesl

--- integration/VODE/cuVODE/source/linpack_module.F90
+++ integration/VODE/cuVODE/source/linpack_module.F90
@@ -16,118 +16,39 @@ contains
 
     integer, parameter :: lda = VODE_NEQS
     integer, parameter :: n = VODE_NEQS
-    integer, parameter :: job = 0
 
     integer, intent(in) :: ipvt(n)
     real(rt)        , intent(in) :: a(lda,n)
     real(rt)        , intent(inout) :: b(n)
 
-    ! 
-    !      dgesl solves the real(rt)         system
-    !      a * x = b  or  trans(a) * x = b
-    !      using the factors computed by dgeco or dgefa.
-    ! 
-    !      on entry
-    ! 
-    !         a       real(rt)        (lda, n)
-    !                 the output from dgeco or dgefa.
-    ! 
-    !         lda     integer
-    !                 the leading dimension of the array  a .
-    ! 
-    !         n       integer
-    !                 the order of the matrix  a .
-    ! 
-    !         ipvt    integer(n)
-    !                 the pivot array from dgeco or dgefa.
-    ! 
-    !         b       real(rt)        (n)
-    !                 the right hand side array.
-    ! 
-    !         job     integer
-    !                 = 0         to solve  a*x = b ,
-    !                 = nonzero   to solve  trans(a)*x = b  where
-    !                             trans(a)  is the transpose.
-    ! 
-    !      on return
-    ! 
-    !         b       the solution array  x .
-    ! 
-    !      error condition
-    ! 
-    !         a division by zero will occur if the input factor contains a
-    !         zero on the diagonal.  technically this indicates singularity
-    !         but it is often caused by improper arguments or improper
-    !         setting of lda .  it will not occur if the subroutines are
-    !         called correctly and if dgeco has set rcond .gt. 0.0
-    !         or dgefa has set info .eq. 0 .
-    ! 
-    !      to compute  inverse(a) * c  where  c  is a matrix
-    !      with  p  columns
-    !            call dgeco(a,lda,n,ipvt,rcond,z)
-    !            if (rcond is too small) go to ...
-    !            do 10 j = 1, p
-    !               call dgesl(a,lda,n,ipvt,c(1,j),0)
-    !         10 continue
-    ! 
-    !      linpack. this version dated 08/14/78 .
-    !      cleve moler, university of new mexico, argonne national lab.
-    ! 
-    !      internal variables
-
-    real(rt)         t
-    integer k,kb,l,nm1
+    real(rt) :: t
+    integer  :: k, kb, l, nm1
 
     !$gpu
 
     nm1 = n - 1
-    if (job == 0) then
-       ! job = 0, solve a * x = b
-       ! first solve l * y = b
-       if (nm1 >= 1) then
-          do k = 1, nm1
-             l = ipvt(k)
-             t = b(l)
-             if (l /= k) then
-                b(l) = b(k)
-                b(k) = t
-             end if
-             b(k+1:n) = b(k+1:n) + t * a(k+1:n,k)
-          end do
-       end if
 
-       ! now solve u * x = y
-       do kb = 1, n
-          k = n + 1 - kb
-          b(k) = b(k)/a(k,k)
-          t = -b(k)
-          b(1:k-1) = b(1:k-1) + t * a(1:k-1,k)
-       enddo
-
-    else
-
-       ! job = nonzero, solve  trans(a) * x = b
-       ! first solve trans(u) * y = b
-       do k = 1, n
-          t = sum(a(1:k-1,k) * b(1:k-1))
-          b(k) = (b(k) - t)/a(k,k)
+    ! solve a * x = b
+    ! first solve l * y = b
+    if (nm1 >= 1) then
+       do k = 1, nm1
+          l = ipvt(k)
+          t = b(l)
+          if (l /= k) then
+             b(l) = b(k)
+             b(k) = t
+          end if
+          b(k+1:n) = b(k+1:n) + t * a(k+1:n,k)
        end do
-
-       ! now solve trans(l) * x = y
-       if (nm1 >= 1) then
-          do kb = 1, nm1
-             k = n - kb
-             b(k) = b(k) + sum(a(k+1:n,k) * b(k+1:n))
-             l = ipvt(k)
-             if (l /= k) then
-                t = b(l)
-                b(l) = b(k)
-                b(k) = t
-             end if
-          end do
-       end if
-
     end if
+
+    ! now solve u * x = y
+    do kb = 1, n
+       k = n + 1 - kb
+       b(k) = b(k)/a(k,k)
+       t = -b(k)
+       b(1:k-1) = b(1:k-1) + t * a(1:k-1,k)
+    end do
 
   end subroutine dgesl
 
