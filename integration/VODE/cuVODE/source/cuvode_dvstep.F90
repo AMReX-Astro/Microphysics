a97e103bd58ce95870ed672cbd3cd0538b04fa54
Max Katz <maxpkatz@gmail.com>
2020-03-18 23:03:45 -0700

    Merge Nordsieck functions into dvstep module

--- integration/VODE/cuVODE/source/cuvode_dvstep.F90
+++ integration/VODE/cuVODE/source/cuvode_dvstep.F90
@@ -6,11 +6,68 @@ module cuvode_dvstep_module
   use cuvode_dvset_module
   use cuvode_dvjust_module
   use cuvode_dvnlsd_module
-  use cuvode_nordsieck_module
 
   implicit none
 
 contains
+
+#if defined(AMREX_USE_CUDA) && !defined(AMREX_USE_GPU_PRAGMA)
+  attributes(device) &
+#endif
+  subroutine advance_nordsieck(vstate)
+
+    ! Effectively multiplies the Nordsieck history
+    ! array by the Pascal triangle matrix.
+
+    implicit none
+
+    ! Declare arguments
+    type(dvode_t), intent(inout) :: vstate
+
+    ! Declare local variables
+    integer :: k, j, i
+
+    !$gpu
+
+    do k = vstate % NQ, 1, -1
+       do j = k, vstate % NQ
+          do i = 1, VODE_NEQS
+             vstate % YH(i, j) = vstate % YH(i, j) + vstate % YH(i, j+1)
+          end do
+       end do
+    end do
+
+  end subroutine advance_nordsieck
+
+
+#if defined(AMREX_USE_CUDA) && !defined(AMREX_USE_GPU_PRAGMA)
+  attributes(device) &
+#endif
+  subroutine retract_nordsieck(vstate)
+
+    ! Undoes the Pascal triangle matrix multiplication
+    ! implemented in subroutine advance_nordsieck.
+
+    implicit none
+
+    ! Declare arguments
+    type(dvode_t), intent(inout) :: vstate
+
+    ! Declare local variables
+    integer :: k, j, i
+
+    !$gpu
+
+    do k = vstate % NQ, 1, -1
+       do j = k, vstate % NQ
+          do i = 1, VODE_NEQS
+             vstate % YH(i, j) = vstate % YH(i, j) - vstate % YH(i, j+1)
+          end do
+       end do
+    end do
+
+  end subroutine retract_nordsieck
+
 
 #if defined(AMREX_USE_CUDA) && !defined(AMREX_USE_GPU_PRAGMA)
   attributes(device) &
