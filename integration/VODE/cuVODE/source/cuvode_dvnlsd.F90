6af02ee839966f25eca9c1e72b029c3d85858963
Max Katz <maxpkatz@gmail.com>
2020-03-20 00:56:53 -0700

    Non-linear solver cleanup

--- integration/VODE/cuVODE/source/cuvode_dvnlsd.F90
+++ integration/VODE/cuVODE/source/cuvode_dvnlsd.F90
@@ -42,7 +42,7 @@ contains
 
     ! Declare local variables
     real(rt) :: CSCALE, DCON, DEL, DELP
-    integer    :: I, IERPJ, M
+    integer  :: I, IERPJ, M
 
     ! Parameter declarations
     real(rt), parameter :: CCMAX = 0.3e0_rt
@@ -57,22 +57,32 @@ contains
 
     ! -----------------------------------------------------------------------
     !  On the first step, on a change of method order, or after a
-    !  nonlinear convergence failure with NFLAG = -2, set IPUP = jacobian
+    !  nonlinear convergence failure with NFLAG = -2, set IPUP = 1
     !  to force a Jacobian update.
     ! -----------------------------------------------------------------------
-    IF (vstate % JSTART .EQ. 0) vstate % NSLP = 0
-    IF (NFLAG .EQ. 0) vstate % ICF = 0
-    IF (NFLAG .EQ. -2) vstate % IPUP = vstate % jacobian
-    IF (vstate % JSTART .EQ. 0) vstate % IPUP = vstate % jacobian
+    if (vstate % JSTART == 0) then
+       vstate % NSLP = 0
+    end if
+    if (NFLAG == 0) then
+       vstate % ICF = 0
+    end if
+    if (NFLAG == -2) then
+       vstate % IPUP = 1
+    end if
+    if (vstate % JSTART == 0) then
+       vstate % IPUP = 1
+    end if
 
     ! -----------------------------------------------------------------------
     !  RC is the ratio of new to old values of the coefficient H/EL(2)=h/l1.
-    !  When RC differs from 1 by more than CCMAX, IPUP is set to jacobian
+    !  When RC differs from 1 by more than CCMAX, IPUP is set to 1
     !  to force DVJAC to be called, if a Jacobian is involved.
     !  In any case, DVJAC is called at least every MSBP steps.
     ! -----------------------------------------------------------------------
-    vstate % DRC = ABS(vstate % RC-1.0_rt)
-    IF (vstate % DRC .GT. CCMAX .OR. vstate % NST .GE. vstate % NSLP+MSBP) vstate % IPUP = vstate % jacobian
+    vstate % DRC = abs(vstate % RC - 1.0_rt)
+    if (vstate % DRC > CCMAX .or. vstate % NST >= vstate % NSLP+MSBP) then
+       vstate % IPUP = 1
+    end if
 
     ! -----------------------------------------------------------------------
     !  Up to MAXCOR corrector iterations are taken.  A convergence test is
@@ -89,35 +99,37 @@ contains
        DELP = 0.0_rt
 
        vstate % Y(1:VODE_NEQS) = vstate % yh(1:VODE_NEQS,1)
-       CALL f_rhs (vstate % TN, vstate, vstate % savf)
+       call f_rhs(vstate % TN, vstate, vstate % savf)
        vstate % NFE = vstate % NFE + 1
 
-       IF (vstate % IPUP > 0) then
+       if (vstate % IPUP == 1) then
           ! -----------------------------------------------------------------------
           !  If indicated, the matrix P = I - h*rl1*J is reevaluated and
           !  preprocessed before starting the corrector iteration.  IPUP is set
           !  to 0 as an indicator that this has been done.
           ! -----------------------------------------------------------------------
-          CALL DVJAC (pivot, IERPJ, vstate)
+          call dvjac(pivot, IERPJ, vstate)
           vstate % IPUP = 0
           vstate % RC = 1.0_rt
           vstate % DRC = 0.0_rt
           vstate % CRATE = 1.0_rt
           vstate % NSLP = vstate % NST
+
           ! If matrix is singular, take error return to force cut in step size. --
-          IF (IERPJ .NE. 0) then
+          if (IERPJ /= 0) then
              NFLAG = -1
              vstate % ICF = 2
-             vstate % IPUP = vstate % jacobian
-             RETURN
-          end IF
+             vstate % IPUP = 1
+             return
+          end if
 
-       end IF
+       end if
 
-       do I = 1,VODE_NEQS
+       do I = 1, VODE_NEQS
           vstate % acor(I) = 0.0_rt
        end do
-       ! This is a looping point for the corrector iteration. -----------------
+
+       ! This is a looping point for the corrector iteration.
 
        do while (.true.)
 
@@ -128,25 +140,25 @@ contains
           !  2/(1+RC) to account for changes in h*rl1 since the last DVJAC call.
           ! -----------------------------------------------------------------------
 
-          do I = 1,VODE_NEQS
-             vstate % Y(I) = (vstate % RL1*vstate % H) * vstate % SAVF(I) - &
-                  (vstate % RL1 * vstate % YH(I,2) + vstate % ACOR(I))
+          do I = 1, VODE_NEQS
+             vstate % Y(I) = (vstate % RL1 * vstate % H) * vstate % SAVF(I) - &
+                             (vstate % RL1 * vstate % YH(I,2) + vstate % ACOR(I))
           end do
 
           call dgesl(vstate % jac, pivot, vstate % Y(:))
 
           vstate % NNI = vstate % NNI + 1
 
-          IF (vstate % RC .NE. 1.0_rt) THEN
-             CSCALE = 2.0_rt/(1.0_rt + vstate % RC)
+          if (vstate % RC /= 1.0_rt) then
+             CSCALE = 2.0_rt / (1.0_rt + vstate % RC)
              vstate % Y(:) = vstate % Y(:) * CSCALE
-          ENDIF
+          end if
 
 #ifdef CLEAN_INTEGRATOR_CORRECTION
           ! Clean the correction to Y. Use vstate % Y as scratch space.
 
           ! Find the corrected Y: Yc = Y_previous + Y_delta
-          do I = 1,VODE_NEQS
+          do I = 1, VODE_NEQS
              vstate % Y(I) = vstate % Y(I) + (vstate % YH(I,1) + vstate % ACOR(I))
           end do
 
@@ -162,7 +174,7 @@ contains
           DEL = sqrt(sum((vstate % Y * vstate % EWT)**2) / VODE_NEQS)
           vstate % acor(:) = vstate % acor(:) + vstate % Y(:)
 
-          do I = 1,VODE_NEQS
+          do I = 1, VODE_NEQS
              vstate % Y(I) = vstate % YH(I,1) + vstate % ACOR(I)
           end do
 
@@ -171,25 +183,25 @@ contains
           !  rate constant is stored in CRATE, and this is used in the test.
           ! -----------------------------------------------------------------------
 
-          IF (M .NE. 0) vstate % CRATE = MAX(CRDOWN*vstate % CRATE,DEL/DELP)
-          DCON = DEL*MIN(1.0_rt,vstate % CRATE)/vstate % TQ(4)
-          IF (DCON .LE. 1.0_rt) then
-             ! we converted, exit the outer loop
+          if (M /= 0) vstate % CRATE = max(CRDOWN * vstate % CRATE, DEL / DELP)
+          DCON = DEL * minn(1.0_rt, vstate % CRATE) / vstate % TQ(4)
+          if (DCON <= 1.0_rt) then
+             ! we converged, exit the outer loop
              converged = .true.
              exit
-          end IF
+          end if
 
           M = M + 1
-          IF (M .EQ. MAXCOR) then
+          if (M == MAXCOR) then
              ! exit the inner correction iteration
              exit
-          end IF
-          IF (M .GE. 2 .AND. DEL .GT. RDIV*DELP) then
+          end if
+          if (M >= 2 .and. DEL > RDIV*DELP) then
              ! exit the inner correction iteration
              exit
           end if
           DELP = DEL
-          CALL f_rhs (vstate % TN, vstate, vstate % SAVF)
+          call f_rhs(vstate % TN, vstate, vstate % SAVF)
           vstate % NFE = vstate % NFE + 1
        end do
 
@@ -197,24 +209,28 @@ contains
           exit
        end if
 
-       IF (vstate % JCUR .EQ. 1) then
+       if (vstate % JCUR == 1) then
           NFLAG = -1
           vstate % ICF = 2
-          vstate % IPUP = vstate % jacobian
-          RETURN
-       end IF
+          vstate % IPUP = 1
+          return
+       end if
 
        vstate % ICF = 1
-       vstate % IPUP = vstate % jacobian
+       vstate % IPUP = 1
 
     end do
 
-    ! Return for successful step. ------------------------------------------
+    ! Return for successful step.
     NFLAG = 0
     vstate % JCUR = 0
     vstate % ICF = 0
-    IF (M .EQ. 0) vstate % ACNRM = DEL
-    IF (M .GT. 0) vstate % ACNRM = sqrt(sum((vstate % ACOR * vstate % EWT)**2) / VODE_NEQS)
+    if (M == 0) then
+       vstate % ACNRM = DEL
+    end if
+    if (M > 0) then
+       vstate % ACNRM = sqrt(sum((vstate % ACOR * vstate % EWT)**2) / VODE_NEQS)
+    end if
 
   end subroutine dvnlsd
 
