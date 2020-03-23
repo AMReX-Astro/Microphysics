c8f50b08cf6c74b5aa0277af0e305ac164a218c3
Alice Harpole <aliceharpole@gmail.com>
2020-03-23 14:26:25 -0400

    C++ aprox_rates compiles

--- rates/aprox_rates_data.cpp
+++ rates/aprox_rates_data.cpp
@@ -1,5 +1,7 @@
 #include <aprox_rates_data.H>
 
+using namespace amrex;
+
 amrex::GpuArray<amrex::Real,6> rv = {6.0_rt, 7.0_rt, 8.0_rt, 9.0_rt, 10.0_rt, 11.0_rt};
 amrex::GpuArray<amrex::Real,14> tv = {1.0_rt,2.0_rt,3.0_rt,4.0_rt,5.0_rt,6.0_rt,7.0_rt,8.0_rt,9.0_rt,10.0_rt,11.0_rt,12.0_rt,13.0_rt,14.0_rt};
 amrex::Vector<amrex::Vector<amrex::GpuArray<amrex::Real,6> > > datn;
