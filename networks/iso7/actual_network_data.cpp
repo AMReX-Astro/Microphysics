13aac8273356c494fb254ba8ff88154c69641916
Donald E. Willcox <eugene.willcox@gmail.com>
2020-03-26 16:48:53 -0700

    Species::NSpec -> Species::NumSpec

--- networks/iso7/actual_network_data.cpp
+++ networks/iso7/actual_network_data.cpp
@@ -2,9 +2,9 @@
 
 namespace iso7
 {
-    AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,NSpec> bion;
-    AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,NSpec> mion;
-    AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,NSpec> wion;
+    AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,NumSpec> bion;
+    AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,NumSpec> mion;
+    AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,NumSpec> wion;
 }
 
 namespace Rates
