2035adba601eea1ac6270e5aebee26f54fc54925
Michael Zingale <michael.zingale@stonybrook.edu>
2020-03-26 15:18:06 -0400

    is_valid_input for Fortran EOSes

--- EOS/multigamma/actual_eos.F90
+++ EOS/multigamma/actual_eos.F90
@@ -64,6 +64,22 @@ contains
 
 
 
+  subroutine is_input_valid(input, valid)
+    implicit none
+    integer, intent(in) :: input
+    logical, intent(out) :: valid
+
+    !$gpu
+
+    valid = .true.
+
+    if (input == eos_input_ps .or. &
+        input == eos_input_th) then
+       valid = .false.
+    end if
+  end subroutine is_input_valid
+
+
   subroutine actual_eos(input, state)
 
     use fundamental_constants_module, only: k_B, n_A, hbar
