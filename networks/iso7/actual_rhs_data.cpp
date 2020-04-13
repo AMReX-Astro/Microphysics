71da81c73a01d6a47d492bd8373de51972597e90
Donald E. Willcox <eugene.willcox@gmail.com>
2020-04-13 15:29:55 -0700

    move initialization into C++ for iso7

--- networks/iso7/actual_rhs_data.cpp
+++ networks/iso7/actual_rhs_data.cpp
@@ -6,3 +6,18 @@ namespace RateTable
     AMREX_GPU_MANAGED Array2D<Real, 1, Rates::NumRates, 1, nrattab> drattabdt;
     AMREX_GPU_MANAGED Array1D<Real, 1, nrattab> ttab;
 }
+
+void actual_rhs_init()
+{
+    rates_init();
+
+    screening_init();
+
+    set_up_screening_factors();
+
+    if (use_tables)
+    {
+        amrex::Print() << "\nInitializing iso7 rate table\n";
+        set_iso7rat();
+    }
+}
