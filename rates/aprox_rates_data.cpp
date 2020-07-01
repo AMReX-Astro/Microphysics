bca4e98a473541462434831ce41fc75f72caab49
Max Katz <maxpkatz@gmail.com>
2020-07-01 12:13:39 -0700

    Fix indexing errors in Langanke C++ implementation (#322)
    I think the Langanke implementation in C++ wasn't quite right due to possible off-by-one indexing errors in the arrays as they were converted from 1-indexing to 0-indexing. Rather than sort it out, I just replaced GpuArrays with Array1D and used 1-indexing everywhere, and this now matches the Fortran output for aprox21 for the rates that depend on it (e.g. d(Cr56)/dt).

--- rates/aprox_rates_data.cpp
+++ rates/aprox_rates_data.cpp
@@ -2,15 +2,15 @@
 
 using namespace amrex;
 
-AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,6> rv = {6.0_rt, 7.0_rt, 8.0_rt, 9.0_rt, 10.0_rt, 11.0_rt};
-AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,14> tv = {1.0_rt,2.0_rt,3.0_rt,4.0_rt,5.0_rt,6.0_rt,7.0_rt,8.0_rt,9.0_rt,10.0_rt,11.0_rt,12.0_rt,13.0_rt,14.0_rt};
-AMREX_GPU_MANAGED amrex::Array3D<amrex::Real,0,1,0,5,0,13> datn;
-AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,4> rfdm;
-AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,4> rfd0;
-AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,4> rfd1;
-AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,4> rfd2;
-AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,12> tfdm;
-AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,12> tfd0;
-AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,12> tfd1;
-AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real,12> tfd2;
+AMREX_GPU_MANAGED amrex::Array1D<amrex::Real,1,6> rv = {6.0_rt, 7.0_rt, 8.0_rt, 9.0_rt, 10.0_rt, 11.0_rt};
+AMREX_GPU_MANAGED amrex::Array1D<amrex::Real,1,14> tv = {1.0_rt,2.0_rt,3.0_rt,4.0_rt,5.0_rt,6.0_rt,7.0_rt,8.0_rt,9.0_rt,10.0_rt,11.0_rt,12.0_rt,13.0_rt,14.0_rt};
+AMREX_GPU_MANAGED amrex::Array3D<amrex::Real,1,2,1,6,1,14> datn;
+AMREX_GPU_MANAGED amrex::Array1D<amrex::Real,1,4> rfdm;
+AMREX_GPU_MANAGED amrex::Array1D<amrex::Real,1,4> rfd0;
+AMREX_GPU_MANAGED amrex::Array1D<amrex::Real,1,4> rfd1;
+AMREX_GPU_MANAGED amrex::Array1D<amrex::Real,1,4> rfd2;
+AMREX_GPU_MANAGED amrex::Array1D<amrex::Real,1,12> tfdm;
+AMREX_GPU_MANAGED amrex::Array1D<amrex::Real,1,12> tfd0;
+AMREX_GPU_MANAGED amrex::Array1D<amrex::Real,1,12> tfd1;
+AMREX_GPU_MANAGED amrex::Array1D<amrex::Real,1,12> tfd2;
 
