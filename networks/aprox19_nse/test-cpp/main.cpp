da32792100c4bf987866b54ba9b7eec99cba750c
Michael Zingale <michael.zingale@stonybrook.edu>
2020-08-03 11:03:40 -0400

    a simple test driver for the table

---
+++ networks/aprox19_nse/test-cpp/main.cpp
@@ -0,0 +1,56 @@
+#include <new>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+
+#ifndef WIN32
+#include <unistd.h>
+#endif
+
+#include <AMReX_CArena.H>
+#include <AMReX_REAL.H>
+#include <AMReX_Utility.H>
+#include <AMReX_IntVect.H>
+#include <AMReX_Box.H>
+#include <AMReX_ParmParse.H>
+#include <AMReX_ParallelDescriptor.H>
+
+#include <time.h>
+
+
+
+extern "C"
+{
+   void do_nse();
+   void do_initialization();
+}
+
+
+std::string inputs_name = "";
+
+int
+main (int   argc,
+      char* argv[])
+{
+
+    //
+    // Make sure to catch new failures.
+    //
+    amrex::Initialize(argc,argv);
+
+    // save the inputs file name for later
+    if (argc > 1) {
+      if (!strchr(argv[1], '=')) {
+        inputs_name = argv[1];
+      }
+    }
+
+    do_initialization();
+
+    do_nse();
+
+    amrex::Finalize();
+
+    return 0;
+}
---
+++ networks/aprox19_nse/test-cpp/testnse.f90
@@ -0,0 +1,27 @@
+subroutine do_nse() bind (C)
+
+  ! this routine simply calls the table interpolation with a fixed
+  ! input, so we can compare the results with those we get from the
+  ! entire Microphysics machinery.
+
+  use nse_module
+
+  use amrex_fort_module, only : rt => amrex_real
+
+  implicit none
+
+  real(rt), parameter :: t9 = 1.0_rt
+  real(rt), parameter :: rho = 1.e9_rt
+  real(rt), parameter :: ye = 0.46
+
+  real(rt) :: abar, dq, dyedt, X(nspec)
+
+  call nse_interp(t9, rho, ye, abar, dq, dyedt, X)
+
+  print *, "      t9    ", "        rho    ", "       ye     ", &
+       "     abar     ", "      be/a    ", "      dyedt  "
+
+  write (*, 41) t9, rho, ye, abar, dq, dyedt, X(:)
+41 format (1pe12.3, 5e14.5, 19e14.5)
+
+end subroutine do_nse
