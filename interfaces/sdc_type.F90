0c82be21e30d74f820fc7497a2df6780819f3f74
Michael Zingale <michael.zingale@stonybrook.edu>
2020-10-12 13:32:13 -0400

    add print methods

--- interfaces/sdc_type.F90
+++ interfaces/sdc_type.F90
@@ -68,4 +68,15 @@ module sdc_type_module
      logical :: success
   end type sdc_t
 
+contains
+
+  subroutine print_sdc_t(sdc_state)
+    implicit none
+    type(sdc_t), intent(in) :: sdc_state
+
+    print *, "y = ", sdc_state % y(:)
+    print *, "ydot_a = ", sdc_state % ydot_a(:)
+
+  end subroutine print_sdc_t
+
 end module sdc_type_module
