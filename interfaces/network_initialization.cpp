ccd82d1e962b79e15d19fd77575b69e536550553
Max Katz <maxpkatz@gmail.com>
2020-11-01 05:33:25 -0800

    Guard against pulling in actual_network (#417)
    We pull in this file if USE_CXX_REACTIONS = TRUE, but if that is set without USE_REACT = TRUE, we will attempt to pull in actual_network.H, which only exists for some networks.

--- interfaces/network_initialization.cpp
+++ interfaces/network_initialization.cpp
@@ -1,12 +1,12 @@
-#include <actual_network.H>
 #ifdef REACTIONS
+#include <actual_network.H>
 #include <actual_rhs.H>
 #endif
 
 void network_init()
 {
-    actual_network_init();
 #ifdef REACTIONS
+    actual_network_init();
     actual_rhs_init();
 #endif
 }
