403a17bef19da09f48e74384eb704097d8bf7468
Michael Zingale <michael.zingale@stonybrook.edu>
2020-11-16 12:51:16 -0500

    remove the string names from the plot_t object

--- unit_test/test_conductivity/variables.cpp
+++ unit_test/test_conductivity/variables.cpp
@@ -14,19 +14,24 @@ plot_t init_variables() {
   p.iconductivity = p.next_index(1);
   p.ispec = p.next_index(NumSpec);
 
-  p.names.resize(p.n_plot_comps);
-
-  p.names[p.irho] = "density";
-  p.names[p.itemp] = "temperature";
-  p.names[p.ih] = "specific_enthalpy";
-  p.names[p.ie] = "specific_energy";
-  p.names[p.ip] = "pressure";
-  p.names[p.iconductivity] = "conductivity";
-  p.names[p.is] = "specific_entropy";
-  for (int n = 0; n < NumSpec; n++) {
-    p.names[p.ispec + n] = "X_" + spec_names_cxx[n];
-  }
 
   return p;
 }
 
+
+void get_varnames(const plot_t p, amrex::Vector<std::string>& names) {
+
+  names.resize(p.n_plot_comps);
+
+  names[p.irho] = "density";
+  names[p.itemp] = "temperature";
+  names[p.ih] = "specific_enthalpy";
+  names[p.ie] = "specific_energy";
+  names[p.ip] = "pressure";
+  names[p.iconductivity] = "conductivity";
+  names[p.is] = "specific_entropy";
+  for (int n = 0; n < NumSpec; n++) {
+    names[p.ispec + n] = "X_" + spec_names_cxx[n];
+  }
+
+}
