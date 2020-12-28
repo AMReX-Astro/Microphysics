275e8b4e74a2a1772d9d92e40c0c91dc2b4d7e21
Michael Zingale <michael.zingale@stonybrook.edu>
2020-12-28 17:23:53 -0500

    remove support for F90 simplified SDC with VODE We are transitioning to C++ in the application codes that use SDC, so the F90 simplified SDC interface is unused and will not get new features.

--- integration/VODE/cuvode_parameters.F90
+++ integration/VODE/cuvode_parameters.F90
@@ -1,17 +1,12 @@
 module cuvode_parameters_module
 
   use burn_type_module, only: neqs
-#ifdef SIMPLIFIED_SDC
-  use sdc_type_module, only : SVAR_EVOLVE
-#endif
   use network, only : nspec
 
   implicit none
 
 #ifdef TRUE_SDC
   integer, parameter :: VODE_NEQS = nspec + 2
-#elif SIMPLIFIED_SDC
-  integer, parameter :: VODE_NEQS = SVAR_EVOLVE
 #else
   integer, parameter :: VODE_NEQS = neqs
 #endif
