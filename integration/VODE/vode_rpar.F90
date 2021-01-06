b62fb17cad952c85ca2b2a542fd6c17c3461e76f
Michael Zingale <michael.zingale@stonybrook.edu>
2021-01-06 08:52:48 -0500

    remove support for F90 simplified SDC with VODE (#492)
    We are transitioning to C++ in the application codes that use SDC,
    so the F90 simplified SDC interface is unused and will not get new
    features.

--- integration/VODE/vode_rpar.F90
+++ integration/VODE/vode_rpar.F90
@@ -2,14 +2,9 @@
 ! and forth between the Microphysics data format and the specific data format
 ! used by the integrators.
 
-! SDC TODO: we need to store the advective sources here.  Also some bits
-! are not needed in the SDC implementation (like the cv and cp stuff)
-
 module vode_rpar_indices
 
-#ifdef SIMPLIFIED_SDC
-  use sdc_type_module, only: SVAR, SVAR_EVOLVE
-#elif TRUE_SDC
+#ifdef TRUE_SDC
   use actual_network, only: nspec
 #else
   use actual_network, only: nspec, naux
@@ -19,68 +14,7 @@ module vode_rpar_indices
   use amrex_fort_module, only : rt => amrex_real
   implicit none
 
-
-#ifdef SIMPLIFIED_SDC
-
-#if defined(SDC_EVOLVE_ENERGY)
-
-  ! Note: we require these components to be first, to allow for offset
-  ! indexing with irp_ydot_a and irp_u_init
-  integer, parameter :: irp_SRHO = 1
-  integer, parameter :: irp_SMX  = 2
-  integer, parameter :: irp_SMY  = 3
-  integer, parameter :: irp_SMZ  = 4
-
-  ! these are the SVAR advective terms (including those that we do
-  ! not explicitly evolve).  Note: these need to be indexed using
-  ! the indicies defined in sdc_type_module
-  integer, parameter :: irp_ydot_a = 5
-
-  ! these are the SVAR - SVAR_EVOLVE initial values of the unevolved
-  ! components of the conserved state.  Note: these need to be indexed
-  ! using the components defined above (irp_SRHO, irp_SMX, ...)
-  integer, parameter :: irp_u_init = irp_ydot_a + SVAR
-
-  integer, parameter :: irp_T_from_eden = irp_u_init + SVAR - SVAR_EVOLVE
-  integer, parameter :: irp_self_heat = irp_T_from_eden + 1
-
-#elif defined(SDC_EVOLVE_ENTHALPY)
-
-  ! Note: we require these components to be first, to allow for offset
-  ! indexing with irp_ydot_a
-  integer, parameter :: irp_SRHO = 1
-
-  ! These are the SVAR advective terms.  Note: these need to be
-  ! indexed using the indices defined in sdc_type_module
-  integer, parameter :: irp_ydot_a = 2
-
-  ! This is the pressure component, carried in case we
-  ! wish to call the EOS using pressure as an input
-  integer, parameter :: irp_p0 = irp_ydot_a + SVAR
-
-  ! These are various bookkeeping parameters
-  integer, parameter :: irp_self_heat = irp_p0 + 1
-
-#endif
-
-  integer, parameter :: irp_t0 = irp_self_heat + 1
-
-#ifdef NONAKA_PLOT
-
-  integer, parameter :: irp_i = irp_t0 + 1
-  integer, parameter :: irp_j = irp_t0 + 2
-  integer, parameter :: irp_k = irp_t0 + 3
-  integer, parameter :: irp_iter = irp_k + 1
-
-  integer, parameter :: n_rpar_comps = irp_iter
-
-#else
-
-  integer, parameter :: n_rpar_comps = irp_t0
-
-#endif
-
-#elif TRUE_SDC
+#ifdef TRUE_SDC
   ! f_source is function we are zeroing.  There are nspec + 2
   ! components (density and energy), since those are the unknowns for
   ! the nonlinear system
