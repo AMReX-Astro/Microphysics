ca7fed4ba1cdb658486f7e9fd65f26fee9aebb22
Michael Zingale <michael.zingale@stonybrook.edu>
2021-08-12 20:14:17 -0400

    add a single zone SDC burn test this will help investigate SDC failures

---
+++ unit_test/burn_cell_sdc/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+#include <AMReX_ParmParse.H>
+#include <AMReX_MultiFab.H>
+using namespace amrex;
+
+#include <extern_parameters.H>
+#include <eos.H>
+#include <network.H>
+#include <burn_cell.H>
+#include <unit_test.H>
+
+int main(int argc, char *argv[]) {
+
+  amrex::Initialize(argc, argv);
+
+  std::cout << "starting the single zone burn..." << std::endl;
+
+  ParmParse ppa("amr");
+
+  std::string probin_file = "probin";
+
+  ppa.query("probin_file", probin_file);
+
+  std::cout << "probin = " << probin_file << std::endl;
+
+  const int probin_file_length = probin_file.length();
+  Vector<int> probin_file_name(probin_file_length);
+
+  for (int i = 0; i < probin_file_length; i++)
+    probin_file_name[i] = probin_file[i];
+
+  init_unit_test(probin_file_name.dataPtr(), &probin_file_length);
+
+  // C++ EOS initialization (must be done after Fortran eos_init and init_extern_parameters)
+  eos_init(small_temp, small_dens);
+
+  // C++ Network, RHS, screening, rates initialization
+  network_init();
+
+  burn_cell_c();
+
+  amrex::Finalize();
+}
