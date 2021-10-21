c0b5064c2bce3aaeb6b4a152e354a7175200ced1
Michael Zingale <michael.zingale@stonybrook.edu>
2021-10-21 14:09:05 -0400

    add a pynucastro generated 3-alpha net we'll use this as the basis for converting to C++

---
+++ networks/triple_alpha_pynucastro_fortran/actual_network.F90
@@ -0,0 +1,162 @@
+module actual_network
+
+  use network_properties
+  use physical_constants, only: ERG_PER_MeV
+  use amrex_fort_module, only: rt => amrex_real
+
+  implicit none
+
+  public
+
+  character (len=32), parameter :: network_name = "pynucastro"
+
+  real(rt), parameter :: avo = 6.0221417930e23_rt
+  real(rt), parameter :: c_light = 2.99792458e10_rt
+  real(rt), parameter :: enuc_conv2 = -avo*c_light*c_light
+
+  real(rt), parameter :: ev2erg  = 1.60217648740e-12_rt
+  real(rt), parameter :: mev2erg = ev2erg * 1.0e6_rt
+  real(rt), parameter :: mev2gr  = mev2erg / c_light**2
+
+  real(rt), parameter :: mass_neutron  = 1.67492721184e-24_rt
+  real(rt), parameter :: mass_proton   = 1.67262163783e-24_rt
+  real(rt), parameter :: mass_electron = 9.10938215450e-28_rt
+
+  integer, parameter :: nrates = 2
+
+
+  ! For each rate, we need: rate, drate/dT, screening, dscreening/dT
+  integer, parameter :: num_rate_groups = 4
+
+  ! Number of reaclib rates
+  integer, parameter :: nrat_reaclib = 2
+  integer, parameter :: number_reaclib_sets = 5
+
+  ! Number of tabular rates
+  integer, parameter :: nrat_tabular = 0
+
+  ! Binding Energies Per Nucleon (MeV)
+  real(rt) :: ebind_per_nucleon(nspec)
+
+  ! bion: Binding Energies (ergs)
+
+  ! Nuclides
+  integer, parameter :: jhe4   = 1
+  integer, parameter :: jc12   = 2
+  integer, parameter :: jo16   = 3
+
+  ! Reactions
+  integer, parameter :: k_he4_he4_he4__c12   = 1
+  integer, parameter :: k_he4_c12__o16   = 2
+
+  real(rt), allocatable, save :: bion(:), mion(:)
+
+#ifdef REACT_SPARSE_JACOBIAN
+  ! Shape of Jacobian in Compressed Sparse Row format
+  integer, parameter   :: NETWORK_SPARSE_JAC_NNZ = 19
+  integer, allocatable :: csr_jac_col_index(:), csr_jac_row_count(:)
+#endif
+
+contains
+
+  subroutine actual_network_init()
+
+    implicit none
+
+    integer :: i
+
+    call network_properties_init()
+
+    ! Allocate ion info arrays
+    allocate(bion(nspec))
+    allocate(mion(nspec))
+
+    ebind_per_nucleon(jhe4)   = 7.07391500000000e+00_rt
+    ebind_per_nucleon(jc12)   = 7.68014400000000e+00_rt
+    ebind_per_nucleon(jo16)   = 7.97620600000000e+00_rt
+
+    do i = 1, nspec
+       bion(i) = ebind_per_nucleon(i) * aion(i) * ERG_PER_MeV
+    end do
+
+    ! Set the mass
+    mion(:) = nion(:) * mass_neutron + zion(:) * (mass_proton + mass_electron) &
+         - bion(:)/(c_light**2)
+
+
+#ifdef REACT_SPARSE_JACOBIAN
+    ! Set CSR format metadata for Jacobian
+    allocate(csr_jac_col_index(NETWORK_SPARSE_JAC_NNZ))
+    allocate(csr_jac_row_count(nspec + 3)) ! neq + 1
+
+    csr_jac_col_index = [ &
+      1, &
+      2, &
+      4, &
+      1, &
+      2, &
+      4, &
+      1, &
+      2, &
+      3, &
+      4, &
+      1, &
+      2, &
+      3, &
+      4, &
+      1, &
+      2, &
+      3, &
+      4, &
+      5  ]
+
+    csr_jac_row_count = [ &
+      1, &
+      4, &
+      7, &
+      11, &
+      15, &
+      20  ]
+#endif
+
+  end subroutine actual_network_init
+
+
+  subroutine actual_network_finalize()
+    ! Deallocate storage arrays
+
+    if (allocated(bion)) then
+       deallocate(bion)
+    endif
+
+    if (allocated(mion)) then
+       deallocate(mion)
+    endif
+
+#ifdef REACT_SPARSE_JACOBIAN
+    if (allocated(csr_jac_col_index)) then
+       deallocate(csr_jac_col_index)
+    endif
+
+    if (allocated(csr_jac_row_count)) then
+       deallocate(csr_jac_row_count)
+    endif
+#endif
+
+  end subroutine actual_network_finalize
+
+
+  subroutine ener_gener_rate(dydt, enuc)
+    ! Computes the instantaneous energy generation rate
+
+    implicit none
+
+    real(rt) :: dydt(nspec), enuc
+
+    ! This is basically e = m c**2
+
+    enuc = sum(dydt(:) * mion(:)) * enuc_conv2
+
+  end subroutine ener_gener_rate
+
+end module actual_network
