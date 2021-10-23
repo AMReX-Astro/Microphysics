bb9dc81c4fdde01d32ef841b5cdfb05eadffbbb1
Max Katz <maxpkatz@gmail.com>
2021-10-23 15:25:04 -0700

    Move rate table upstream

--- networks/iso7/actual_rhs_data.cpp
+++ networks/iso7/actual_rhs_data.cpp
@@ -1,12 +1,5 @@
 #include <actual_rhs.H>
 
-namespace RateTable
-{
-    AMREX_GPU_MANAGED Array3D<Real, 1, Rates::NumRatesFR, 1, 2, 1, nrattab> rattab;
-    AMREX_GPU_MANAGED Array3D<Real, 1, Rates::NumRatesFR, 1, 2, 1, nrattab> drattabdt;
-    AMREX_GPU_MANAGED Array1D<Real, 1, nrattab> ttab;
-}
-
 void actual_rhs_init()
 {
     rates_init();
