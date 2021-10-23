c224e93bce75ed5fcfad0231dd4a2933c0a7c092
Max Katz <maxpkatz@gmail.com>
2021-10-23 16:39:17 -0700

    Upstream rhs_init

--- interfaces/network_initialization.cpp
+++ interfaces/network_initialization.cpp
@@ -9,8 +9,13 @@ void network_init()
 {
 #ifdef REACTIONS
 #ifdef NETWORK_HAS_CXX_IMPLEMENTATION
+#ifdef NEW_NETWORK_IMPLEMENTATION
+    actual_network_init();
+    RHS::rhs_init();
+#else
     actual_network_init();
     actual_rhs_init();
+#endif
 #endif
 #endif
 }
