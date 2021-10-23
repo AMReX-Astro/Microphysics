411a7823185ce55b42c1fde4b5f890d0a40ad48b
Max Katz <maxpkatz@gmail.com>
2021-10-23 15:39:58 -0700

    Remove old-style rates

--- interfaces/rhs_utilities.cpp
+++ interfaces/rhs_utilities.cpp
@@ -1,5 +1,5 @@
 #include <rhs_utilities.H>
 
-AMREX_GPU_MANAGED Array3D<Real, 1, Rates::NumRatesFR, 1, 2, 1, RHS::nrattab> RHS::rattab;
-AMREX_GPU_MANAGED Array3D<Real, 1, Rates::NumRatesFR, 1, 2, 1, RHS::nrattab> RHS::drattabdt;
+AMREX_GPU_MANAGED Array3D<Real, 1, Rates::NumRates, 1, 2, 1, RHS::nrattab> RHS::rattab;
+AMREX_GPU_MANAGED Array3D<Real, 1, Rates::NumRates, 1, 2, 1, RHS::nrattab> RHS::drattabdt;
 AMREX_GPU_MANAGED Array1D<Real, 1, RHS::nrattab> RHS::ttab;
