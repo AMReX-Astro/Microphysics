66498dfbbabd12e45cb6000973172dca578f5030
Michael Zingale <michael.zingale@stonybrook.edu>
2022-01-03 09:30:22 -0500

    remove some Fortran unit_test stuff (#860)
    also eliminate some probins

--- unit_test/eos_cell/main.cpp
+++ unit_test/eos_cell/main.cpp
@@ -10,7 +10,7 @@ using namespace amrex;
 #include <eos.H>
 #include <network.H>
 #include <eos_cell.H>
-#include <unit_test_F.H>
+#include <unit_test.H>
 
 int main(int argc, char *argv[]) {
 
@@ -20,19 +20,7 @@ int main(int argc, char *argv[]) {
 
   ParmParse ppa("amr");
 
-  std::string probin_file = "probin";
-
-  ppa.query("probin_file", probin_file);
-
-  std::cout << "probin = " << probin_file << std::endl;
-
-  const int probin_file_length = probin_file.length();
-  Vector<int> probin_file_name(probin_file_length);
-
-  for (int i = 0; i < probin_file_length; i++)
-    probin_file_name[i] = probin_file[i];
-
-  init_unit_test(probin_file_name.dataPtr(), &probin_file_length);
+  init_unit_test();
 
   // C++ EOS initialization (must be done after Fortran eos_init and init_extern_parameters)
   eos_init(small_temp, small_dens);
--- unit_test/test_rhs/main.cpp
+++ unit_test/test_rhs/main.cpp
@@ -18,7 +18,6 @@ using namespace amrex;
 #include <AMReX_buildInfo.H>
 #include <variables.H>
 #include <unit_test.H>
-#include <unit_test_F.H>
 #include <react_util.H>
 
 int main (int argc, char* argv[])
@@ -101,17 +100,7 @@ void main_main ()
 
     ParmParse ppa("amr");
 
-    std::string probin_file = "probin";
-
-    ppa.query("probin_file", probin_file);
-
-    const int probin_file_length = probin_file.length();
-    Vector<int> probin_file_name(probin_file_length);
-
-    for (int i = 0; i < probin_file_length; i++)
-      probin_file_name[i] = probin_file[i];
-
-    init_unit_test(probin_file_name.dataPtr(), &probin_file_length);
+    init_unit_test();
 
     // C++ EOS initialization (must be done after Fortran eos_init and init_extern_parameters)
     eos_init(small_temp, small_dens);
