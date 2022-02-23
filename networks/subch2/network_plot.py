bd3e6cb43c8fc3a3b421c52225c1bada73ca5235
Michael Zingale <michael.zingale@stonybrook.edu>
2022-02-23 12:44:04 -0500

    add some more nuclei to better represent C12+C12, C12+O16, and O16+O16

--- networks/subch2/network_plot.py
+++ networks/subch2/network_plot.py
@@ -1,19 +1,14 @@
 # C-burning with A=23 URCA rate module generator
 
 import pynucastro as pyna
+import subch2
 
 library_file = "20180319default2"
-mylibrary = pyna.rates.Library(library_file)
+reaclib_library = pyna.rates.Library(library_file)
 
-all_reactants = ["p",
-        "he4", "c12", "o16", "ne20", "mg24", "si28", "s32",
-        "ar36", "ca40", "ti44", "cr48", "fe52", "ni56",
-        "al27", "p31", "cl35", "k39", "sc43", "v47", "mn51", "co55",
-        "c14", "n13", "n14", "o18", "f18", "ne21" ]
+subch_library = reaclib_library.linking_nuclei(subch2.all_reactants)
 
-subCh = mylibrary.linking_nuclei(all_reactants)
-
-rc = pyna.RateCollection(libraries=[subCh])
+rc = pyna.RateCollection(libraries=[subch_library])
 
 comp = pyna.Composition(rc.get_nuclei())
 comp.set_all(0.1)
--- networks/subch2/subch2.py
+++ networks/subch2/subch2.py
@@ -4,17 +4,26 @@ import pynucastro as pyna
 from pynucastro.networks import StarKillerCxxNetwork
 
 library_file = "20180319default2"
-mylibrary = pyna.rates.Library(library_file)
 
-subCh = pyna.rates.Library()
+all_reactants = ["n", "p",
+                 "he4", "c12", "o16", "ne20", "mg24", "si28", "s32",
+                 "ar36", "ca40", "ti44", "cr48", "fe52", "ni56",
+                 "al27", "p31", "cl35", "k39", "sc43", "v47", "mn51", "co55",
+                 "c14", "n13", "n14", "o18", "f18", "ne21",
+                 "mg23", "na23", "si27", "s31"]
 
-all_reactants = ["p",
-        "he4", "c12", "o16", "ne20", "mg24", "si28", "s32",
-        "ar36", "ca40", "ti44", "cr48", "fe52", "ni56",
-        "al27", "p31", "cl35", "k39", "sc43", "v47", "mn51", "co55",
-        "c14", "n13", "n14", "o18", "f18", "ne21" ]
+def doit():
+    reaclib_library = pyna.rates.Library(library_file)
 
-subCh = mylibrary.linking_nuclei(all_reactants)
 
-net = StarKillerCxxNetwork(libraries=[subCh])
-net.write_network()
+    subch_library = reaclib_library.linking_nuclei(all_reactants)
+
+    # generate a report about any missing rates that we might want to include
+
+    subch_library.validate(reaclib_library)
+
+    net = StarKillerCxxNetwork(libraries=[subch_library])
+    net.write_network()
+
+if __name__ == "__main__":
+    doit()
