58fe094a62a981470584f5db8ea267a387196d15
Michael Zingale <michael.zingale@stonybrook.edu>
2022-03-23 19:33:55 -0400

    regenerate with the screening fix in pynucastro

--- networks/subch2/subch2.py
+++ networks/subch2/subch2.py
@@ -26,45 +26,11 @@ def get_subch2_library(validate=False):
     return subch_library, no_neutron_library
 
 
-
-
 def doit():
 
     subch_library, no_neutron_library = get_subch2_library()
 
     n_rates = [r for _, r in (subch_library - no_neutron_library)._rates.items()]
-
-    print("original set of n rates: ")
-    for r in n_rates:
-        print(r)
-
-    print(" ")
-
-    print(subch_library)
-
-    r1 = [r for r in n_rates if r.__repr__() == "ne21 + he4 --> n + mg24"][0]
-    r2 = [r for r in n_rates if r.__repr__() == "ne21 --> n + ne20"][0]
-    r3 = [r for r in n_rates if r.__repr__() == "ne21 + n --> he4 + o18"][0]
-
-    remove_rates = [r1, r2, r3]
-
-    # debugging
-    #remove_rates = []
-
-    #for i, r in enumerate(sorted(n_rates)):
-    #    #if i <= (len(n_rates)-1)//2: doesn't run
-    #    #if i > len(n_rates)//2:  # runs well
-    #    #if i > 10:  # doesn't run
-    #    if i <= 10 or i > len(n_rates)//2: # works well
-    #    #if i <= 15 or i > len(n_rates)//2:  # doesn't run
-    #    #if i <= 10 or i > len(n_rates)//2 - 4: # also crashes!
-    #        continue
-    #    remove_rates.append(r)
-
-
-    for r in remove_rates:
-        print("removing: ", r)
-        subch_library.remove_rate(r)
 
     net = StarKillerCxxNetwork(libraries=[subch_library], symmetric_screening=True)
     net.write_network()
