ca2eb0f3df82625fb0bb4e651b67b5f7bab180f2
Michael Zingale <michael.zingale@stonybrook.edu>
2022-03-24 13:10:59 -0400

    this seems to work

---
+++ interfaces/react_data.cpp
@@ -0,0 +1,7 @@
+#include <react_data.H>
+
+// Declare extern variables
+
+bool ReactData::initialized;
+AMREX_GPU_MANAGED amrex::Real ReactData::mintemp;
+AMREX_GPU_MANAGED amrex::Real ReactData::maxtemp;
