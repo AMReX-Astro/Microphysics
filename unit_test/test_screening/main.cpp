66461d36ce2dc0fdd32e08616bdb12be7266ff1b
Michael Zingale <michael.zingale@stonybrook.edu>
2022-07-04 17:32:09 -0400

    remove .cxx

--- unit_test/test_screening/main.cpp
+++ unit_test/test_screening/main.cpp
@@ -153,7 +153,7 @@ void main_main ()
     ParallelDescriptor::ReduceRealMax(stop_time, IOProc);
 
 
-    std::string name = "test_screening." + screen_name + ".cxx";
+    std::string name = "test_screening." + screen_name;
 
     // Write a plotfile
     WriteSingleLevelPlotfile(name, state, names, geom, time, 0);
