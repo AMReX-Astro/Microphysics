983989dd73989833732b77867cc23be763045546
Zhi <zhichen0428@gmail.com>
2022-09-30 17:30:47 -0400

    update progress

--- nse/nse_index.cpp
+++ nse/nse_index.cpp
@@ -7,3 +7,4 @@ AMREX_GPU_MANAGED int NSE_INDEX::p_index {-1};
 AMREX_GPU_MANAGED int NSE_INDEX::h1_index {-1};
 AMREX_GPU_MANAGED int NSE_INDEX::n_index {-1};
 AMREX_GPU_MANAGED int NSE_INDEX::he4_index {-1};
+AMREX_GPU_MANAGED amrex::Array1D<int, 1, Rates::NumRates, 1, 7> rate_indices;
