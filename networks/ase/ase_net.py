61b3d61065ede5c03306f7559b3ff0e045b18ee9
Zhi <zhichen0428@gmail.com>
2022-11-04 11:45:04 -0400

    temp save

--- networks/ase/ase_net.py
+++ networks/ase/ase_net.py
@@ -8,16 +8,17 @@ from pynucastro.networks import StarKillerCxxNetwork
 reaclib_lib = pyna.ReacLibLibrary()
 
 nucs = ["p", "n", "he4",
-        # "c12", "n13", "n14",
-        # "o16", "f18", "ne20",
-        # "ne21", "na22", "na23",
-        # "mg24", "al27" , "si28",
-        # "p31", "s32", "ar36",
-        # "ca40", "ti44", "cr48",
-        # "fe52", "ni56"]
-        
-        "si28", "si29",
-        "si30", "p31", "s32"]
+        "c12", "n13", "n14",
+        "o16", "f18", "ne20",
+        "ne21", "na22", "na23",
+        "mg24", "al27" , "si28",
+        "p31", "s32", "ar36",
+        "ca40", "ti44", "cr48",
+        "fe52", "ni56"]
+
+# nucs = ["p", "n", "he4",
+#         "si28", "si29",
+#         "si30", "p31", "s32"]
 
 ase = reaclib_lib.linking_nuclei(nucs)
 
