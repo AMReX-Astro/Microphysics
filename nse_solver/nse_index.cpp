6536c6e8f396f567a7e6b6d8fa13cbfd7b048d81
Zhi <zhichen0428@gmail.com>
2022-11-11 23:32:27 -0500

    approx rate bug fix

--- nse_solver/nse_index.cpp
+++ nse_solver/nse_index.cpp
@@ -7,4 +7,4 @@ AMREX_GPU_MANAGED int NSE_INDEX::p_index {-1};
 AMREX_GPU_MANAGED int NSE_INDEX::h1_index {-1};
 AMREX_GPU_MANAGED int NSE_INDEX::n_index {-1};
 AMREX_GPU_MANAGED int NSE_INDEX::he4_index {-1};
-AMREX_GPU_MANAGED amrex::Array2D<int, 1, Rates::NumRates, 1, 7> NSE_INDEX::rate_indices;
+AMREX_GPU_MANAGED amrex::Array2D<int, 1, Rates::NumRates, 1, 6> NSE_INDEX::rate_indices;
