1626b42c14110904c2c5171e1ad6aab6a5d0744c
Michael Zingale <michael.zingale@stonybrook.edu>
2022-11-30 21:03:59 -0500

    style

--- networks/subch_approx/subch_approx.py
+++ networks/subch_approx/subch_approx.py
@@ -39,7 +39,8 @@ def doit():
     r1 = subch.get_rate_by_name("c12(p,g)n13")
     r2 = subch.get_rate_by_name("n13(a,p)o16")
 
-    net = AmrexAstroCxxNetwork(libraries=[subch], symmetric_screening=True, disable_rate_params=[r1, r2])
+    net = AmrexAstroCxxNetwork(libraries=[subch], symmetric_screening=True,
+                               disable_rate_params=[r1, r2])
     net.make_ap_pg_approx(intermediate_nuclei=["cl35", "k39", "sc43", "v47", "mn51", "co55"])
     net.remove_nuclei(["cl35", "k39", "sc43", "v47", "mn51", "co55"])
 
