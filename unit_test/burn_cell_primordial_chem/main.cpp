dc4a7e216c061efdbaced041a495f5b2d7e6dd2d
Piyush Sharda <34922596+psharda@users.noreply.github.com>
2022-12-22 01:15:05 +1100

    Added burn_cell test for primordial chem (#1064)
    This PR will add the burn_cell test for primordial chemistry. The test is identical to the one used in other astrochem codes. It basically sets up a one-zone model of a collapsing primordial molecular cloud. We initialize the abundances of each chemical specie (H, H+, H2, D, HD, etc.) at some initial density. We then increase the density in steps of 0.1*freefall_time, and the output at each step is the number densities of all chemical species and the gas temperature. The model stops when the densities are > 3e-6 g/cm^3 or the number of steps > 1000, or the maximum time exceeds the given value.

---
+++ unit_test/burn_cell_primordial_chem/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+#include <AMReX_ParmParse.H>
+#include <AMReX_MultiFab.H>
+using namespace amrex;
+
+#include <extern_parameters.H>
+#include <eos.H>
+#include <network.H>
+#include <burn_cell.H>
+#include <unit_test.H>
+
+int main(int argc, char *argv[]) {
+
+  amrex::Initialize(argc, argv);
+
+  std::cout << "starting the single zone burn..." << std::endl;
+
+  ParmParse ppa("amr");
+
+  init_unit_test();
+
+  // C++ EOS initialization (must be done after Fortran eos_init and init_extern_parameters)
+  eos_init(small_temp, small_dens);
+
+  // C++ Network, RHS, screening, rates initialization
+  network_init();
+
+  burn_cell_c();
+
+  amrex::Finalize();
+}
