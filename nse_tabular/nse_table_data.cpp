991fa315ba909362fa8da5e50696e48ce5e00cd9
Max Katz <maxpkatz@gmail.com>
2023-07-17 08:12:18 -0400

    Move tabular NSE implementation to its own directory (#1281)
    this still only works with aprox19

--- nse_tabular/nse_table_data.cpp
+++ nse_tabular/nse_table_data.cpp
@@ -1,25 +1,12 @@
-#include <AMReX_Vector.H>
-#include <actual_network.H>
-#ifdef NSE_TABLE
-#include <nse_table.H>
-#endif
+#include <nse_table_data.H>
 
 #ifdef NSE_TABLE
-namespace table
+namespace nse_table
 {
-
   AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, npts> abartab;
   AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, npts> ebtab;
   AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, npts> wratetab;
 
   AMREX_GPU_MANAGED amrex::Array2D<amrex::Real, 1, NumSpec, 1, npts> massfractab;
-
 }
 #endif
-
-void actual_network_init()
-{
-#ifdef NSE_TABLE
-    init_nse();
-#endif
-}
