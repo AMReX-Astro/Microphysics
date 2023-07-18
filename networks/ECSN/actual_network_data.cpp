21e85bcd22c59b754756058fd921262408da29ec
Alexander Smith <72695548+aisclark91@users.noreply.github.com>
2023-07-18 07:56:06 -0400

    New Ci-Benchmark for the burn_cell test (#1277)
    This updates ECSN with the latest pynucastro, which changes the rate tables to be log10.  Due to differences in the units conversion factors and the number of decimals between the existing tabular rates and the ones of the pynucastro PR #550, the existing benchmark will differ.

--- networks/ECSN/actual_network_data.cpp
+++ networks/ECSN/actual_network_data.cpp
@@ -7,6 +7,33 @@ namespace network
     AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, NumSpec> mion;
 }
 
+#ifdef NSE_NET
+namespace NSE_INDEX
+{
+    AMREX_GPU_MANAGED amrex::Array2D<int, 1, Rates::NumRates, 1, 7, Order::C> rate_indices {
+        -1, -1, 5, -1, 1, 2, -1,
+        -1, 1, 2, -1, -1, 5, 1,
+        -1, 1, 5, -1, -1, 6, -1,
+        -1, 1, 6, -1, -1, 8, -1,
+        -1, 0, 7, -1, -1, 8, -1,
+        -1, 1, 7, -1, -1, 9, -1,
+        -1, 1, 8, -1, -1, 10, -1,
+        -1, 0, 9, -1, -1, 10, -1,
+        -1, 2, 2, -1, 0, 9, -1,
+        -1, 2, 2, -1, 1, 8, -1,
+        -1, 1, 6, -1, 0, 7, -1,
+        -1, 0, 7, -1, 1, 6, 11,
+        -1, 1, 8, -1, 0, 9, -1,
+        -1, 0, 9, -1, 1, 8, 13,
+        -1, -1, 4, -1, -1, 3, -1,
+        -1, -1, 5, -1, -1, 4, -1,
+        -1, -1, 3, -1, -1, 4, 15,
+        -1, -1, 4, -1, -1, 5, 16
+    };
+    AMREX_GPU_MANAGED bool initialized = false;
+}
+#endif
+
 void actual_network_init()
 {
     using namespace Species;
