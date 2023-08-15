4fff7afd7c7a2a33f1ac332f0e87a870c57fa4f4
Max Katz <maxpkatz@gmail.com>
2023-08-14 22:06:19 -0400

    Remove obsolete NETWORK_PROPERTIES file

--- .github/workflows/find_changed_files.py
+++ .github/workflows/find_changed_files.py
@@ -35,10 +35,10 @@ def find_files(SHAs=None):
     # see which directories contain changed files
     changed_networks = set()
     for f in files:
-        # check for the NETWORK_PROPERTIES file in each parent directory
+        # check for the Make.package file in each parent directory
         parts = f.split('/')
         while parts:
-            if os.path.exists(os.path.join(*parts, 'NETWORK_PROPERTIES')):
+            if os.path.exists(os.path.join(*parts, 'Make.package')):
                 # remove networks/
                 changed_networks.add(os.path.join(*parts[1:]))
                 break
