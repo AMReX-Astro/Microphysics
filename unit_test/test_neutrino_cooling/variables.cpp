611cfb43cc9cb183f3482929342547277948fd5d
Michael Zingale <michael.zingale@stonybrook.edu>
2023-09-11 14:20:30 -0400

    add a neutrino cooling unit test

---
+++ unit_test/test_neutrino_cooling/variables.cpp
@@ -0,0 +1,30 @@
+#include <variables.H>
+#include <network.H>
+
+plot_t init_variables() {
+
+  plot_t p;
+
+  p.irho = p.next_index(1);
+  p.itemp = p.next_index(1);
+  p.ispec = p.next_index(NumSpec);
+  p.isneut = p.next_index(1);
+
+  return p;
+}
+
+
+void get_varnames(const plot_t& p, amrex::Vector<std::string>& names) {
+
+  names.resize(p.n_plot_comps);
+
+  names[p.irho] = "density";
+  names[p.itemp] = "temperature";
+  for (int n = 0; n < NumSpec; n++) {
+    names[p.ispec + n] = "X_" + spec_names_cxx[n];
+  }
+
+  names[p.isneut] = "sneut";
+}
+
+
