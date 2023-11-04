e1ff5fc681b07365c5131bf5712e42f1ea878cb8
Michael Zingale <michael.zingale@stonybrook.edu>
2023-11-04 13:58:44 -0400

    add d<B/A>/dt

--- nse_tabular/nse_table_data.cpp
+++ nse_tabular/nse_table_data.cpp
@@ -7,6 +7,7 @@ namespace nse_table
     AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, npts> beatab;
     AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, npts> dyedttab;
     AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, npts> dabardttab;
+    AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, npts> dbeadttab;
     AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, npts> enutab;
 
   AMREX_GPU_MANAGED amrex::Array2D<amrex::Real, 1, NumSpec, 1, npts> massfractab;
