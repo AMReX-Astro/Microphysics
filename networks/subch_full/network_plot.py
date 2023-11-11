9aaf3992dba16c6663ddca30a7bcbb3f12c08384
Michael Zingale <michael.zingale@stonybrook.edu>
2023-11-11 07:59:27 -0500

    update network pynucastro scripts to new RateCollection plot interface

--- networks/subch_full/network_plot.py
+++ networks/subch_full/network_plot.py
@@ -12,6 +12,9 @@ comp.set_all(0.1)
 comp.set_nuc("he4", 0.95)
 comp.normalize()
 
-rc.plot(outfile="subch2.pdf", rho=1.e6, T=1.e9, comp=comp, hide_xalpha=True,
+rho = 1.e6
+T = 1.e9
+
+rc.plot(rho, T, comp, outfile="subch2.pdf", hide_xalpha=True,
         size=(1500, 650), node_size=500, node_font_size=11, node_color="#337dff", node_shape="s",
         Z_range=(1,29), rotated=True, curved_edges=True)
