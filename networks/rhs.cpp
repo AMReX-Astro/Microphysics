ef3da3a73671c92e0527976936f036e3686963ec
Michael Zingale <michael.zingale@stonybrook.edu>
2024-02-05 12:52:23 -0500

    don't include <rhs.H> unless we are a new network type

--- networks/rhs.cpp
+++ networks/rhs.cpp
@@ -1,6 +1,5 @@
-#include <rhs.H>
-
 #ifdef NEW_NETWORK_IMPLEMENTATION
+#include <rhs.H>
 
 AMREX_GPU_MANAGED Array3D<Real, 1, Rates::NumRates, 1, 2, 1, RHS::nrattab> RHS::rattab;
 AMREX_GPU_MANAGED Array3D<Real, 1, Rates::NumRates, 1, 2, 1, RHS::nrattab> RHS::drattabdt;
