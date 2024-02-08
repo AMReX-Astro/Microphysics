b5d1a91a99bcbd68bc3b864392fe103f61b76e3a
Michael Zingale <michael.zingale@stonybrook.edu>
2024-02-08 15:10:37 -0500

    start moving away from "using namespace amrex" (#1465)

--- networks/rhs.cpp
+++ networks/rhs.cpp
@@ -2,8 +2,8 @@
 
 #ifdef NEW_NETWORK_IMPLEMENTATION
 
-AMREX_GPU_MANAGED Array3D<Real, 1, Rates::NumRates, 1, 2, 1, RHS::nrattab> RHS::rattab;
-AMREX_GPU_MANAGED Array3D<Real, 1, Rates::NumRates, 1, 2, 1, RHS::nrattab> RHS::drattabdt;
-AMREX_GPU_MANAGED Array1D<Real, 1, RHS::nrattab> RHS::ttab;
+AMREX_GPU_MANAGED Array3D<amrex::Real, 1, Rates::NumRates, 1, 2, 1, RHS::nrattab> RHS::rattab;
+AMREX_GPU_MANAGED Array3D<amrex::Real, 1, Rates::NumRates, 1, 2, 1, RHS::nrattab> RHS::drattabdt;
+AMREX_GPU_MANAGED Array1D<amrex::Real, 1, RHS::nrattab> RHS::ttab;
 
 #endif
