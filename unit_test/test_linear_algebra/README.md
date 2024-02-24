5b6214680c8a9dce4c53031dd4a183534d6e6bc2
Michael Zingale <michael.zingale@stonybrook.edu>
2024-02-24 17:59:44 -0500

    create a linear algebra unit test this will test just the rhs.H routines for solving a linear system

---
+++ unit_test/test_linear_algebra/README.md
@@ -0,0 +1,5 @@
+# `test_linear_algebra`
+
+This test creates a simple diagonally-dominant matrix, A, and multiples by
+a test x to get a righthand side vector, b.  It then uses the linear algebra
+routines in `rhs.H` to solve Ax = b and compares to the original x.
---
+++ unit_test/test_linear_algebra/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include <extern_parameters.H>
+#include <eos.H>
+#include <network.H>
+#include <unit_test.H>
+#include <test_linear_algebra.H>
+
+using namespace unit_test_rp;
+
+int main(int argc, char *argv[]) {
+
+  amrex::Initialize(argc, argv);
+
+  std::cout << "starting the single zone burn..." << std::endl;
+
+  init_unit_test();
+
+  // C++ EOS initialization (must be done after Fortran eos_init and init_extern_parameters)
+  eos_init(small_temp, small_dens);
+
+  // C++ Network, RHS, screening, rates initialization
+  network_init();
+
+  linear_algebra();
+
+  amrex::Finalize();
+}
