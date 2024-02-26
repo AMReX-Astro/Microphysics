e273732e78d52b09ac03f5a074a34d2f5b3bc6f1
Michael Zingale <michael.zingale@stonybrook.edu>
2024-02-26 15:21:51 -0500

    create a linear algebra unit test (#1493)
    this will test the rhs.H routines for solving a linear system as well as
    those in linpack.H.

---
+++ unit_test/test_linear_algebra/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include <extern_parameters.H>
+#include <eos.H>
+#include <network.H>
+#include <unit_test.H>
+#include <test_linear_algebra.H>
+
+using namespace unit_test_rp;
+
+int main(int argc, char *argv[]) {
+
+  amrex::Initialize(argc, argv);
+
+  init_unit_test();
+
+  // C++ EOS initialization (must be done after Fortran eos_init and init_extern_parameters)
+  eos_init(small_temp, small_dens);
+
+  // C++ Network, RHS, screening, rates initialization
+  network_init();
+
+  linear_algebra();
+
+  amrex::Finalize();
+}
