e8366977a419ea3a298daef86fff7b594f82428f
Zhi <zhichen0428@gmail.com>
2024-06-18 12:25:58 -0400

    update

--- util/approx_math/test_fast_atan/main.cpp
+++ util/approx_math/test_fast_atan/main.cpp
@@ -8,31 +8,19 @@ int main() {
 
     test_fast_atan_accuracy(0.0_rt);
     test_fast_atan_accuracy(0.01_rt);
-    test_fast_atan_accuracy(-0.01_rt);
+    test_fast_atan_accuracy(0.05_rt);
     test_fast_atan_accuracy(0.1_rt);
-    test_fast_atan_accuracy(-0.1_rt);
     test_fast_atan_accuracy(0.5_rt);
-    test_fast_atan_accuracy(-0.5_rt);
     test_fast_atan_accuracy(1.0_rt);
-    test_fast_atan_accuracy(-1.0_rt);
-    test_fast_atan_accuracy(10.0_rt);
-    test_fast_atan_accuracy(-10.0_rt);
-    test_fast_atan_accuracy(100.0_rt);
-    test_fast_atan_accuracy(-100.0_rt);
-    test_fast_atan_accuracy(-1000.0_rt);
-    test_fast_atan_accuracy(-1000.0_rt);
 
-    // Large positive and negative values
+    // Inf and NaN case
     test_fast_atan_accuracy(std::numeric_limits<amrex::Real>::infinity());
-    test_fast_atan_accuracy(-std::numeric_limits<amrex::Real>::infinity());
-
-    // NaN value
     test_fast_atan_accuracy(std::numeric_limits<amrex::Real>::quiet_NaN());
 
     std::cout << "Accuracy tests passed!" << std::endl;
 
     // Now performanc test
-    int iters = 2000;
-    amrex::Real test_value = 4146.3232_rt;
+    int iters = 3000;
+    amrex::Real test_value = 0.667_rt;
     test_fast_atan_speed(iters, test_value);
 }
