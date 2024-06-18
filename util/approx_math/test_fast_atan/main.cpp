07d7786350844600490cb9de9d549cd21e918466
Zhi <zhichen0428@gmail.com>
2024-06-18 14:39:18 -0400

    some update

--- util/approx_math/test_fast_atan/main.cpp
+++ util/approx_math/test_fast_atan/main.cpp
@@ -12,19 +12,34 @@ int main() {
     test_fast_atan_accuracy(0.1_rt);
     test_fast_atan_accuracy(0.5_rt);
     test_fast_atan_accuracy(1.0_rt);
+    test_fast_atan_accuracy(5.0_rt);
+    test_fast_atan_accuracy(8.0_rt);
+    test_fast_atan_accuracy(10.0_rt);
+    test_fast_atan_accuracy(23.0_rt);
     test_fast_atan_accuracy(100.0_rt);
-    test_fast_atan_accuracy(200.0_rt);
     test_fast_atan_accuracy(500.0_rt);
     test_fast_atan_accuracy(3000.0_rt);
 
+    test_fast_atan_accuracy(-0.01_rt);
+    test_fast_atan_accuracy(-0.05_rt);
+    test_fast_atan_accuracy(-0.1_rt);
+    test_fast_atan_accuracy(-0.5_rt);
+    test_fast_atan_accuracy(-1.0_rt);
+    test_fast_atan_accuracy(-5.0_rt);
+    test_fast_atan_accuracy(-8.0_rt);
+    test_fast_atan_accuracy(-10.0_rt);
+    test_fast_atan_accuracy(-23.0_rt);
+    test_fast_atan_accuracy(-100.0_rt);
+    test_fast_atan_accuracy(-500.0_rt);
+    test_fast_atan_accuracy(-3000.0_rt);
+
     // Inf and NaN case
     test_fast_atan_accuracy(std::numeric_limits<amrex::Real>::infinity());
-    test_fast_atan_accuracy(std::numeric_limits<amrex::Real>::quiet_NaN());
 
     std::cout << "Accuracy tests passed!" << std::endl;
 
     // Now performanc test
-    int iters = 3000;
-    amrex::Real test_value = 0.667_rt;
+    int iters = 300;
+    amrex::Real test_value = 0.43_rt;
     test_fast_atan_speed(iters, test_value);
 }
