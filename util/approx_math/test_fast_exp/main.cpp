b1be4692dcc2884b38af231766e619453a876d89
Zhi <zhichen0428@gmail.com>
2024-06-20 19:45:14 -0400

    update unit test

--- util/approx_math/test_fast_exp/main.cpp
+++ util/approx_math/test_fast_exp/main.cpp
@@ -7,7 +7,9 @@ int main() {
     // Test values including edge cases and some typical values
 
     test_fast_exp_accuracy(0.0_rt);
+    test_fast_exp_accuracy(0.001_rt);
     test_fast_exp_accuracy(0.01_rt);
+    test_fast_exp_accuracy(0.05_rt);
     test_fast_exp_accuracy(0.08_rt);
     test_fast_exp_accuracy(0.1_rt);
     test_fast_exp_accuracy(0.5_rt);
@@ -15,23 +17,31 @@ int main() {
     test_fast_exp_accuracy(5.0_rt);
     test_fast_exp_accuracy(8.0_rt);
     test_fast_exp_accuracy(10.0_rt);
+    test_fast_exp_accuracy(15.0_rt);
     test_fast_exp_accuracy(25.0_rt);
-    // test_fast_exp_accuracy(100.0_rt);
-    // test_fast_exp_accuracy(500.0_rt);
-    // test_fast_exp_accuracy(5.0e8_rt);
+    test_fast_exp_accuracy(100.0_rt);
+    test_fast_exp_accuracy(200.0_rt);
+    test_fast_exp_accuracy(300.0_rt);
 
+    test_fast_exp_accuracy(-0.001_rt);
     test_fast_exp_accuracy(-0.01_rt);
-    test_fast_exp_accuracy(-0.08_rt);
+    test_fast_exp_accuracy(-0.05_rt);
+    test_fast_exp_accuracy(-0.08_rt);;
     test_fast_exp_accuracy(-0.1_rt);
     test_fast_exp_accuracy(-0.5_rt);
     test_fast_exp_accuracy(-1.0_rt);
     test_fast_exp_accuracy(-5.0_rt);
     test_fast_exp_accuracy(-8.0_rt);
+    test_fast_exp_accuracy(-9.0_rt);
     test_fast_exp_accuracy(-10.0_rt);
+    test_fast_exp_accuracy(-11.0_rt);
+    test_fast_exp_accuracy(-12.0_rt);
+    test_fast_exp_accuracy(-13.0_rt);
+    test_fast_exp_accuracy(-14.0_rt);
+    test_fast_exp_accuracy(-15.0_rt);
     test_fast_exp_accuracy(-25.0_rt);
-    // test_fast_exp_accuracy(-100.0_rt);
-    // test_fast_exp_accuracy(-500.0_rt);
-    // test_fast_exp_accuracy(-5.0e8_rt);
+    test_fast_exp_accuracy(-100.0_rt);
+
 
     // Inf Case
 
@@ -42,21 +52,21 @@ int main() {
 
     // Now performance test
 
-    // int iters = 5;
-    // amrex::Real test_value = 160.0_rt;
-    // test_fast_exp_speed(100, iters, test_value);
+    int iters = 5;
+    amrex::Real test_value = 160.0_rt;
+    test_fast_exp_speed(100, iters, test_value);
 
-    // iters = 10;
-    // test_fast_exp_speed(100, iters, test_value);
+    iters = 10;
+    test_fast_exp_speed(100, iters, test_value);
 
-    // iters = 20;
-    // test_fast_exp_speed(100, iters, test_value);
+    iters = 20;
+    test_fast_exp_speed(100, iters, test_value);
 
-    // iters = 30;
-    // test_fast_exp_speed(100, iters, test_value);
+    iters = 30;
+    test_fast_exp_speed(100, iters, test_value);
 
-    // iters = 50;
-    // test_fast_exp_speed(100, iters, test_value);
+    iters = 50;
+    test_fast_exp_speed(100, iters, test_value);
 
     // iters = 70;
     // test_fast_exp_speed(100, iters, test_value);
