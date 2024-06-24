c5475c00acb91a6e8577e99212d7efa7ff661c39
Zhi <zhichen0428@gmail.com>
2024-06-24 10:46:56 -0400

    add float version of ekmett exp

--- util/approx_math/test_fast_exp/main.cpp
+++ util/approx_math/test_fast_exp/main.cpp
@@ -9,7 +9,6 @@ int main() {
     test_fast_exp_accuracy(0.0_rt);
     test_fast_exp_accuracy(0.001_rt);
     test_fast_exp_accuracy(0.01_rt);
-    test_fast_exp_accuracy(0.05_rt);
     test_fast_exp_accuracy(0.1_rt);
     test_fast_exp_accuracy(0.5_rt);
     test_fast_exp_accuracy(1.0_rt);
@@ -20,8 +19,7 @@ int main() {
     test_fast_exp_accuracy(500.0_rt);
 
     test_fast_exp_accuracy(-0.001_rt);
-    test_fast_exp_accuracy(-0.01_rt);
-    test_fast_exp_accuracy(-0.05_rt);
+    test_fast_exp_accuracy(-0.01_rt);;
     test_fast_exp_accuracy(-0.1_rt);
     test_fast_exp_accuracy(-0.5_rt);
     test_fast_exp_accuracy(-1.0_rt);
@@ -30,6 +28,30 @@ int main() {
     test_fast_exp_accuracy(-50.0_rt);
     test_fast_exp_accuracy(-100.0_rt);
     test_fast_exp_accuracy(-500.0_rt);
+
+
+    test_fast_exp_accuracy(0.0F);
+    test_fast_exp_accuracy(0.001F);
+    test_fast_exp_accuracy(0.01F);
+    test_fast_exp_accuracy(0.1F);
+    test_fast_exp_accuracy(0.5F);
+    test_fast_exp_accuracy(1.0F);
+    test_fast_exp_accuracy(5.0F);
+    test_fast_exp_accuracy(10.0F);
+    test_fast_exp_accuracy(50.0F);
+    test_fast_exp_accuracy(100.0F);
+    test_fast_exp_accuracy(500.0F);
+
+    test_fast_exp_accuracy(-0.001F);
+    test_fast_exp_accuracy(-0.01F);
+    test_fast_exp_accuracy(-0.1F);
+    test_fast_exp_accuracy(-0.5F);
+    test_fast_exp_accuracy(-1.0F);
+    test_fast_exp_accuracy(-5.0F);
+    test_fast_exp_accuracy(-10.0F);;
+    test_fast_exp_accuracy(-50.0F);
+    test_fast_exp_accuracy(-100.0F);
+    test_fast_exp_accuracy(-500.0F);
 
     std::cout << "Accuracy tests passed!" << std::endl;
 
