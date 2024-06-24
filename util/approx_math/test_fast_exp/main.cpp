90726224850a62eb43c27d0cacbc6f938a351d98
Zhi <zhichen0428@gmail.com>
2024-06-24 11:36:26 -0400

    update test

--- util/approx_math/test_fast_exp/main.cpp
+++ util/approx_math/test_fast_exp/main.cpp
@@ -14,44 +14,26 @@ int main() {
     test_fast_exp_accuracy(1.0_rt);
     test_fast_exp_accuracy(5.0_rt);
     test_fast_exp_accuracy(10.0_rt);
+    test_fast_exp_accuracy(15.0_rt);
+    test_fast_exp_accuracy(20.0_rt);
+    test_fast_exp_accuracy(30.0_rt);
     test_fast_exp_accuracy(50.0_rt);
     test_fast_exp_accuracy(100.0_rt);
     test_fast_exp_accuracy(500.0_rt);
 
     test_fast_exp_accuracy(-0.001_rt);
-    test_fast_exp_accuracy(-0.01_rt);;
+    test_fast_exp_accuracy(-0.01_rt);
     test_fast_exp_accuracy(-0.1_rt);
     test_fast_exp_accuracy(-0.5_rt);
     test_fast_exp_accuracy(-1.0_rt);
     test_fast_exp_accuracy(-5.0_rt);
-    test_fast_exp_accuracy(-10.0_rt);;
+    test_fast_exp_accuracy(-10.0_rt);
+    test_fast_exp_accuracy(-15.0_rt);
+    test_fast_exp_accuracy(-20.0_rt);
+    test_fast_exp_accuracy(-30.0_rt);
     test_fast_exp_accuracy(-50.0_rt);
     test_fast_exp_accuracy(-100.0_rt);
     test_fast_exp_accuracy(-500.0_rt);
-
-
-    test_fast_exp_accuracy(0.0F);
-    test_fast_exp_accuracy(0.001F);
-    test_fast_exp_accuracy(0.01F);
-    test_fast_exp_accuracy(0.1F);
-    test_fast_exp_accuracy(0.5F);
-    test_fast_exp_accuracy(1.0F);
-    test_fast_exp_accuracy(5.0F);
-    test_fast_exp_accuracy(10.0F);
-    test_fast_exp_accuracy(50.0F);
-    test_fast_exp_accuracy(100.0F);
-    test_fast_exp_accuracy(500.0F);
-
-    test_fast_exp_accuracy(-0.001F);
-    test_fast_exp_accuracy(-0.01F);
-    test_fast_exp_accuracy(-0.1F);
-    test_fast_exp_accuracy(-0.5F);
-    test_fast_exp_accuracy(-1.0F);
-    test_fast_exp_accuracy(-5.0F);
-    test_fast_exp_accuracy(-10.0F);;
-    test_fast_exp_accuracy(-50.0F);
-    test_fast_exp_accuracy(-100.0F);
-    test_fast_exp_accuracy(-500.0F);
 
     std::cout << "Accuracy tests passed!" << std::endl;
 
