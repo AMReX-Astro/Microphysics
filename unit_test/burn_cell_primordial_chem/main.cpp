154dac739ee023a50a409c498456e458da6e01db
Ben Wibking <ben@wibking.com>
2024-06-23 21:33:10 -0400

    return exit code and check for input file

--- unit_test/burn_cell_primordial_chem/main.cpp
+++ unit_test/burn_cell_primordial_chem/main.cpp
@@ -15,20 +15,32 @@ using namespace amrex;
 int main(int argc, char *argv[]) {
 
   amrex::Initialize(argc, argv);
+  int success = 0;
 
-  std::cout << "starting the single zone burn..." << std::endl;
+  {
+    // check that correct input file is provided
+    ParmParse const pp("unit_test");
+    std::string const run_prefix = "burn_cell_primordial_chem_";
+    std::string input_run_prefix;
+    pp.query("run_prefix", input_run_prefix);
+    AMREX_ALWAYS_ASSERT_WITH_MESSAGE(run_prefix == input_run_prefix, "input file is missing or incorrect!");
+    
+    std::cout << "starting the single zone burn..." << std::endl;
 
-  ParmParse ppa("amr");
+    ParmParse ppa("amr");
 
-  init_unit_test();
+    init_unit_test();
 
-  // C++ EOS initialization (must be done after Fortran eos_init and init_extern_parameters)
-  eos_init(unit_test_rp::small_temp, unit_test_rp::small_dens);
+    // C++ EOS initialization (must be done after Fortran eos_init and init_extern_parameters)
+    eos_init(unit_test_rp::small_temp, unit_test_rp::small_dens);
 
-  // C++ Network, RHS, screening, rates initialization
-  network_init();
+    // C++ Network, RHS, screening, rates initialization
+    network_init();
 
-  burn_cell_c();
-
+    success = burn_cell_c();
+  }
+  
   amrex::Finalize();
+
+  return (!success);
 }
