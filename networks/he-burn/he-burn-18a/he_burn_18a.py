23185e29731c96ced04c61b2b0df9aaedce7892e
Michael Zingale <michael.zingale@stonybrook.edu>
2024-12-09 15:53:06 -0500

    more work

--- networks/he-burn/he-burn-18a/he_burn_18a.py
+++ networks/he-burn/he-burn-18a/he_burn_18a.py
@@ -4,84 +4,17 @@
 import pynucastro as pyna
 from pynucastro.networks import AmrexAstroCxxNetwork
 
+import he_burn_core
+
+
 DO_DERIVED_RATES = True
-
-
-def get_library():
-
-    reaclib_lib = pyna.ReacLibLibrary()
-
-    all_reactants = ["p",
-                     "he4", "c12", "o16", "ne20", "mg24", "si28", "s32",
-                     "ar36", "ca40", "ti44", "cr48", "fe52", "ni56",
-                     "al27", "p31", "cl35", "k39", "sc43", "v47", "mn51", "co55",
-                     "n13", "na23"]
-
-    subch = reaclib_lib.linking_nuclei(all_reactants)
-
-    # in this list, we have the reactants, the actual reactants,
-    # and modified products that we will use instead
-    other_rates = [("c12(c12,n)mg23", "mg24"),
-                   ("o16(o16,n)s31", "s32"),
-                   ("o16(c12,n)si27", "si28")]
-
-    for r, mp in other_rates:
-        _r = reaclib_lib.get_rate_by_name(r)
-        _r.modify_products(mp)
-        subch += pyna.Library(rates=[_r])
-
-    # finally, the aprox nets don't include the reverse rates for
-    # C12+C12, C12+O16, and O16+O16, so remove those
-
-    for r in subch.get_rates():
-        if sorted(r.products) in [[pyna.Nucleus("c12"), pyna.Nucleus("c12")],
-                                  [pyna.Nucleus("c12"), pyna.Nucleus("o16")],
-                                  [pyna.Nucleus("o16"), pyna.Nucleus("o16")]]:
-            subch.remove_rate(r)
-
-    # C12+Ne20 and reverse
-    # (a,g) links between Na23 and Al27
-    # (a,g) links between Al27 and P31
-
-    rates_to_remove = ["p31(p,c12)ne20",
-                       "si28(a,c12)ne20",
-                       "ne20(c12,p)p31",
-                       "ne20(c12,a)si28",
-                       "na23(a,g)al27",
-                       "al27(g,a)na23",
-                       "al27(a,g)p31",
-                       "p31(g,a)al27"]
-
-    for r in rates_to_remove:
-        print("removing: ", r)
-        _r = subch.get_rate_by_name(r)
-        subch.remove_rate(_r)
-
-    if DO_DERIVED_RATES:
-        rates_to_derive = []
-        for r in subch.get_rates():
-            if r.reverse:
-                # this rate was computed using detailed balance, regardless
-                # of whether Q < 0 or not.  We want to remove it and then
-                # recompute it
-                rates_to_derive.append(r)
-
-        # now for each of those derived rates, look to see if the pair exists
-
-        for r in rates_to_derive:
-            fr = subch.get_rate_by_nuclei(r.products, r.reactants)
-            if fr:
-                print(f"modifying {r} from {fr}")
-                subch.remove_rate(r)
-                d = pyna.DerivedRate(rate=fr, compute_Q=False, use_pf=True)
-                subch.add_rate(d)
-
-    return subch
 
 
 def doit():
 
-    subch = get_library()
+    subch = he_burn_core.get_core_library(include_n14_sequence=False,
+                                          include_zn=False,
+                                          do_detailed_balance=DO_DERIVED_RATES)
 
     # these are the rates that we are going to allow to be optionally
     # zeroed
