94307412e23773ec4f7538dbe32c1de083d71117
Michael Zingale <michael.zingale@stonybrook.edu>
2025-05-29 14:13:55 -0400

    update to use he_burn_core

--- networks/he-burn/cno-he-burn-33a/cno_he_burn_33a.py
+++ networks/he-burn/cno-he-burn-33a/cno_he_burn_33a.py
@@ -6,30 +6,30 @@ from pynucastro.networks import AmrexAstroCxxNetwork
 import he_burn_core
 
 
-DO_DERIVED_RATES = False
+DO_DERIVED_RATES = True
 
 
 def doit():
 
-    extra_reactants = ["c13", "n15",
+    extra_reactants = ["c13", "n14", "n15",
                        "o14", "o15", "o17", "o18",
-                       "f17", "f19",
+                       "f17", "f18", "f19",
                        "ne18", "ne19",
-                       "na22", "na23",
+                       "ne21", "na22", "na23",
                        "mg22"]
 
-    subch = he_burn_core.get_core_library(include_n14_sequence=True,
+    subch = he_burn_core.get_core_library(include_n14_approx=False,
                                           include_zn=False,
                                           extra_nuclei=extra_reactants,
                                           do_detailed_balance=DO_DERIVED_RATES)
 
-    net = AmrexAstroCxxNetwork(libraries=[subch], symmetric_screening=True)
+    net = AmrexAstroCxxNetwork(libraries=[subch], symmetric_screening=False)
 
-    net.make_ap_pg_approx(intermediate_nuclei=["cl35", "k39", "sc43", "v47", "mn51", "co55"])
+    net.make_ap_pg_approx(intermediate_nuclei=["cl35", "k39", "sc43",
+                                               "v47", "mn51", "co55"])
     net.remove_nuclei(["cl35", "k39", "sc43", "v47", "mn51", "co55"])
 
-    print(f"number of nuclei: {len(net.unique_nuclei)}")
-    print(f"number of rates: {len(net.rates)}")
+    net.summary()
 
     comp = pyna.Composition(net.get_nuclei())
     comp.set_all(0.1)
@@ -41,13 +41,14 @@ def doit():
 
     fig = net.plot(rho, T, comp,
                    rotated=True, hide_xalpha=True, curved_edges=True,
-                   size=(1500, 450),
-                   node_size=500, node_font_size=11, node_color="#337dff", node_shape="s",
+                   size=(1500, 600),
+                   node_size=600, node_font_size=11,
                    Z_range=(1, 29))
 
     net.write_network()
 
     fig.savefig("cno-he-burn-33a.png", bbox_inches="tight")
+
 
 if __name__ == "__main__":
     doit()
