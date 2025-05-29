e9f0616502089c560048f6874dcdd8795d8d8209
Michael Zingale <michael.zingale@stonybrook.edu>
2025-05-29 10:13:32 -0400

    add a larger nova network this includes more CNO breakout as well as Li and Be links

---
+++ networks/nova-li/nova.py
@@ -0,0 +1,29 @@
+import pynucastro as pyna
+from pynucastro.networks import AmrexAstroCxxNetwork
+
+rl = pyna.ReacLibLibrary()
+
+all_nuclei = ["p", "h2", "he3", "he4",
+              "li6", "li7", "be7", "be8", "b8",
+              "c12", "c13", "n13", "n14", "n15",
+              "o14", "o15", "o16", "o17", "o18",
+              "f17", "f18", "f19",
+              "ne18", "ne19", "ne20", "ne21"]
+
+nova_library = rl.linking_nuclei(all_nuclei, with_reverse=True)
+
+tl = pyna.TabularLibrary(ordering=["ffn", "langanke"])
+#weak_library = tl.linking_nuclei(all_nuclei)
+
+
+net = AmrexAstroCxxNetwork(libraries=[nova_library])  # + weak_library])
+net.write_network()
+
+rc = pyna.RateCollection(libraries=[nova_library])
+
+rc.plot(rotated=False, outfile="nova.png",
+        hide_xalpha=True, hide_xp=True,
+        curved_edges=False,
+        node_size=400, node_font_size=10)
+
+net.summary()
