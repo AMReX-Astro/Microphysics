adb62c42c40d69adc9f04b2c7149268072443830
Michael Zingale <michael.zingale@stonybrook.edu>
2025-06-07 21:00:01 -0400

    regenerate all of the non-He burning nets (#1797)
    this updates them to the latest pynucastro
    for ignition_reaclib, it also fixes the neutron decay to use the
    tabular rate instead of the ReacLib rate.

--- networks/ignition_reaclib/C-burn-simple/table_rates_data.cpp
+++ networks/ignition_reaclib/C-burn-simple/table_rates_data.cpp
@@ -8,6 +8,11 @@ using namespace amrex;
 namespace rate_tables
 {
 
+    AMREX_GPU_MANAGED table_t j_n_p_meta;
+    AMREX_GPU_MANAGED amrex::Array3D<amrex::Real, 1, 13, 1, 11, 1, 6> j_n_p_data;
+    AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, 11> j_n_p_rhoy;
+    AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, 13> j_n_p_temp;
+
 
 }
 
@@ -18,6 +23,14 @@ void init_tabular()
     amrex::Print() << "reading in network electron-capture / beta-decay tables..." << std::endl;
 
     using namespace rate_tables;
+
+    j_n_p_meta.ntemp = 13;
+    j_n_p_meta.nrhoy = 11;
+    j_n_p_meta.nvars = 6;
+    j_n_p_meta.nheader = 5;
+
+    init_tab_info(j_n_p_meta, "langanke-n-p_betadecay.dat", j_n_p_rhoy, j_n_p_temp, j_n_p_data);
+
 
 
 }
