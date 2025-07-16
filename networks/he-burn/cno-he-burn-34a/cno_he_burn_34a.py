2fa6b2f5acec7e7e3d6959f8ebb50749a4e214d2
Michael Zingale <michael.zingale@stonybrook.edu>
2025-07-16 11:16:05 -0400

    sync up nets

--- networks/he-burn/cno-he-burn-34a/cno_he_burn_34a.py
+++ networks/he-burn/cno-he-burn-34a/cno_he_burn_34a.py
@@ -6,10 +6,7 @@ from pynucastro.networks import AmrexAstroCxxNetwork
 import he_burn_core
 
 
-DO_DERIVED_RATES = True
-
-
-def doit():
+def create_network():
 
     extra_reactants = ["c13", "n15",
                        "o14", "o15", "o17", "o18",
@@ -20,31 +17,30 @@ def doit():
     subch = he_burn_core.get_core_library(include_n14_sequence=True,
                                           include_zn=False,
                                           extra_nuclei=extra_reactants,
-                                          do_detailed_balance=DO_DERIVED_RATES)
+                                          do_detailed_balance=True)
 
-    net = AmrexAstroCxxNetwork(libraries=[subch], symmetric_screening=True)
+    net = AmrexAstroCxxNetwork(libraries=[subch])
 
     net.make_ap_pg_approx(intermediate_nuclei=["cl35", "k39", "sc43", "v47", "mn51", "co55"])
     net.remove_nuclei(["cl35", "k39", "sc43", "v47", "mn51", "co55"])
 
+    return net
+
+def doit():
+
+    net = create_network()
+
     net.summary()
-
-    comp = pyna.Composition(net.get_nuclei())
-    comp.set_all(0.1)
-    comp.set_nuc("he4", 0.95)
-    comp.normalize()
-
-    rho = 1.e6
-    T = 1.e9
 
     fig = net.plot(rotated=True, hide_xalpha=True, curved_edges=False,
                    size=(1500, 450),
                    node_size=500, node_font_size=10,
-                   Z_range=(1, 29), highlight_filter_function=lambda r : isinstance(r, pyna.rates.TabularRate))
+                   Z_range=(1, 29),
+                   highlight_filter_function=lambda r : isinstance(r, pyna.rates.TabularRate))
 
     net.write_network()
 
-    fig.savefig("cno-he-new.png", bbox_inches="tight")
+    fig.savefig("cno-he-burn-34a.png", bbox_inches="tight")
 
 if __name__ == "__main__":
     doit()
--- networks/he-burn/cno-he-burn-41a/cno_he_burn_41a.py
+++ networks/he-burn/cno-he-burn-41a/cno_he_burn_41a.py
@@ -11,9 +11,11 @@ def create_network():
     extra_reactants = ["c13", "n15",
                        "o14", "o15", "o17", "o18",
                        "f17", "f19",
-                       "ne18", "ne19",
-                       "na22", "na23",
-                       "mg22"]
+                       "ne18", "ne19", "ne22",
+                       "na21","na22", "na23",
+                       "mg22",'mg23', "mg25","mg26",
+                       "al25","al26",
+                       "si27","si28"]
 
     subch = he_burn_core.get_core_library(include_n14_sequence=True,
                                           include_zn=False,
@@ -27,22 +29,30 @@ def create_network():
 
     return net
 
-
 def doit():
 
     net = create_network()
 
     net.summary()
 
-    fig = net.plot(rotated=True, hide_xalpha=True, curved_edges=False,
+    fig = net.plot(rotated=True, hide_xalpha=True, # curved_edges=True,
                    size=(1500, 450),
                    node_size=500, node_font_size=10,
-                   Z_range=(1, 29),
-                   highlight_filter_function=lambda r : isinstance(r, pyna.rates.TabularRate))
+                   Z_range=(1, 29))
 
     net.write_network()
 
-    fig.savefig("cno-he-burn-33a.png", bbox_inches="tight")
+    fig.savefig("cno-he-burn-41a.png", bbox_inches="tight")
+
+    fig = net.plot(rotated=True, hide_xalpha=True, # curved_edges=True,
+                   size=(1500, 450),
+                   node_size=500, node_font_size=10,
+                   highlight_filter_function=lambda r: isinstance(r, pyna.rates.TabularRate),
+                   Z_range=(1, 29))
+
+    net.write_network()
+
+    fig.savefig("cno-he-burn-41a-highlight.png", bbox_inches="tight")
 
 if __name__ == "__main__":
     doit()
