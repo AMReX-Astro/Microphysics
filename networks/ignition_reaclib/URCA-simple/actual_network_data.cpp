a4628ba4e76dee57aec2d47d900b8e75842fbb72
Michael Zingale <michael.zingale@stonybrook.edu>
2025-12-08 19:05:40 -0500

    update more

--- networks/ignition_reaclib/URCA-simple/actual_network_data.cpp
+++ networks/ignition_reaclib/URCA-simple/actual_network_data.cpp
@@ -4,15 +4,15 @@
 #ifdef NSE_NET
 namespace NSE_INDEX
 {
-    AMREX_GPU_MANAGED amrex::Array2D<int, 1, Rates::NumRates, 1, 7, amrex::Order::C> rate_indices {
-        -1, 3, 3, -1, 2, 5, -1,
-        -1, 3, 3, -1, 0, 8, -1,
-        -1, 3, 3, -1, 1, 7, -1,
-        -1, 2, 3, -1, -1, 4, -1,
-        -1, -1, 7, -1, -1, 6, -1,
-        -1, -1, 6, -1, -1, 7, 5,
-        -1, -1, 0, -1, -1, 1, 8,
-        -1, -1, 1, -1, -1, 0, -1
+    AMREX_GPU_MANAGED amrex::Array2D<std::int8_t, 1, Rates::NumRates, 1, 7, amrex::Order::C> rate_indices {
+        -1, 3, 3, -1, 2, 5, -1,  // C12_C12__He4_Ne20
+        -1, 3, 3, -1, 0, 8, -1,  // C12_C12__n_Mg23
+        -1, 3, 3, -1, 1, 7, -1,  // C12_C12__p_Na23
+        -1, 2, 3, -1, -1, 4, -1,  // He4_C12__O16
+        -1, -1, 7, -1, -1, 6, -1,  // Na23__Ne23
+        -1, -1, 6, -1, -1, 7, 5,  // Ne23__Na23
+        -1, -1, 0, -1, -1, 1, 8,  // n__p
+        -1, -1, 1, -1, -1, 0, -1  // p__n
     };
 }
 #endif
