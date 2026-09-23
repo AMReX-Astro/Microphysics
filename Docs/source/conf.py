5764219821571b327fe9735345ed5cfe4db39a08
Michael Zingale <michael.zingale@stonybrook.edu>
2026-09-23 09:02:24 -0400

    split linkcheck off into its own action

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -240,6 +240,7 @@ linkcheck_request_headers = {
 }
 linkcheck_ignore = [r"https://doi.org/10.1103/[Pp]hys[Rr]ev*",
                     "https://www.sciencedirect.com/science/article/pii/S016892741200133X",
+                    r"https://ui.adsabs.harvard.edu/abs/*",
                     "https://doi.org/10.1145/641876.641877",
                     "https://doi.org/10.1137/S1064827597326651"]
 
